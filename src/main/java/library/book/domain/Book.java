a17eda4ace5fc04f22db14b158dc0b2c9e376880
EunChanNam <namhm23@gmail.com>
2023-09-23 17:02:12 +0900

    feat: Book 생성 메소드 구현

--- src/main/java/library/book/domain/Book.java
+++ src/main/java/library/book/domain/Book.java
@@ -12,6 +12,29 @@ public class Book {
 
 	private Status status;
 
+	//== Factory 메소드 ==//
+	private Book(
+		final long id,
+		final String title,
+		final String authorName,
+		final int pages
+	) {
+		this.id = id;
+		this.title = title;
+		this.authorName = authorName;
+		this.pages = pages;
+		this.status = new Status();
+	}
+
+	public static Book createBook(
+		final long id,
+		final String title,
+		final String authorName,
+		final int pages
+	) {
+		return new Book(id, title, authorName, pages);
+	}
+
 	//== Utility 메소드 ==//
 	public long getId() {
 		return id;
