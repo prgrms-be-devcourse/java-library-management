dc1ca9874d7faee4cd45a56514d1f4ee7573624e
EunChanNam <namhm23@gmail.com>
2023-09-23 21:12:05 +0900

    feat: OutputConsole 및 모드 선택창 출력 메소드 구현

---
+++ src/main/java/library/book/infra/console/output/OutputConsole.java
@@ -0,0 +1,18 @@
+package library.book.infra.console.output;
+
+import java.io.PrintStream;
+
+public class OutputConsole {
+
+	private static final PrintStream out = System.out;
+
+	public void showSelectMode() {
+		StringBuilder message = new StringBuilder();
+
+		message.append("Q. 모드를 선택해주세요.\n");
+		message.append("1. 일반 모드\n");
+		message.append("2. 테스트 모드\n");
+
+		out.println(message);
+	}
+}
---
+++ src/test/java/library/book/infra/console/output/OutputConsoleTest.java
@@ -0,0 +1,23 @@
+package library.book.infra.console.output;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+@DisplayName("[InputConsole Test] - Infra")
+public class OutputConsoleTest {
+
+	private final OutputConsole outputConsole = new OutputConsole();
+
+	@Test
+	@DisplayName("[showSelectMode 테스트]")
+	void showSelectModeTest() {
+		//when
+		Executable when = outputConsole::showSelectMode;
+
+		//then
+		assertDoesNotThrow(when);
+	}
+}
