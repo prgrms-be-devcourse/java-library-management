2f3fec09f9834391e3c38fc455a5bb39bbf43345
EunChanNam <namhm23@gmail.com>
2023-09-23 14:53:31 +0900

    feat: Book 모델 구현

---
+++ src/main/java/library/book/domain/Book.java
@@ -0,0 +1,14 @@
+package library.book.domain;
+
+public class Book {
+
+	private long id;
+
+	private String title;
+
+	private String authorName;
+
+	private int pages;
+
+	private Status status;
+}
