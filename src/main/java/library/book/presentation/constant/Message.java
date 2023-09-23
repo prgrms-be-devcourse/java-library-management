b5fd66659033c60926e8bc9d587f164f3b005b3b
EunChanNam <namhm23@gmail.com>
2023-09-24 00:52:38 +0900

    feat: 콘솔 메세지를 모아두는 Message enum 구현

---
+++ src/main/java/library/book/presentation/constant/Message.java
@@ -0,0 +1,21 @@
+package library.book.presentation.constant;
+
+public enum Message {
+
+	ENTRY_REGISTER_MENU("[System] 도서 등록 메뉴로 넘어갑니다.\n"),
+	INPUT_BOOK_NAME("Q. 등록할 도서 제목을 입력하세요.\n"),
+	INPUT_AUTHOR_NAME("Q. 작가 이름을 입력하세요.\n"),
+	INPUT_PAGES("Q. 페이지 수를 입력하세요.\n"),
+	COMPLETE_REGISTER("[System] 도서 등록이 완료되었습니다.\n"),
+	;
+
+	private final String value;
+
+	Message(String value) {
+		this.value = value;
+	}
+
+	public String getValue() {
+		return this.value;
+	}
+}
