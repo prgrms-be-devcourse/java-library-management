fc148fe69d82f168aeeb22a62325667522ea9216
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-09-23 10:54:33 +0900

    enum 생성

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.nio.Buffer;
+
+public class Main {
+    public static void main(String[] args) {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+        if(selectMode() == ModeType.NORMAL) {
+
+        };
+    }
+}
---
+++ src/ModeType.java
@@ -0,0 +1,3 @@
+public enum ModeType {
+    NORMAL, TEST;
+}
