959bc20f274e90a43fb4d6ac371083e6702c2f6b
EunChanNam <namhm23@gmail.com>
2023-09-24 02:01:39 +0900

    feat: 도서 정보 인풋을 RegisterBookRequest 로 변환하는 메소드 구현

--- src/main/java/library/book/exception/ErrorCode.java
+++ src/main/java/library/book/exception/ErrorCode.java
@@ -6,6 +6,7 @@ public enum ErrorCode {
 	FILE_READ_FAIL("File Read 오류입니다."),
 	FILE_WRITE_FAIL("File Write 오류입니다."),
 	TYPE_MISS_MATCH("주입된 타입이 틀립니다."),
+	INVALID_BOOK_INFO_FORMAT("도서 정보를 입력 할 때 '|||' 는 입력할 수 없습니다."),
 	;
 
 	private final String message;
--- src/main/java/library/book/presentation/converter/InputConverter.java
+++ src/main/java/library/book/presentation/converter/InputConverter.java
@@ -1,5 +1,10 @@
 package library.book.presentation.converter;
 
+import static library.book.exception.ErrorCode.*;
+
+import library.book.application.dto.request.RegisterBookRequest;
+import library.book.exception.LibraryException;
+
 public class InputConverter {
 
 	private static final String[] numberStrings
@@ -7,5 +12,24 @@ public class InputConverter {
 
 	public String convertNumberToString(final int number) {
 		return numberStrings[number];
+	}
+
+	public RegisterBookRequest convertStringToRegisterRequest(
+		final String bookInfo
+	) {
+		String[] splitBookInfo = bookInfo.split("\\|\\|\\|");
+		validateBookInfoFormat(splitBookInfo.length);
+
+		return new RegisterBookRequest(
+			splitBookInfo[0],
+			splitBookInfo[1],
+			Integer.parseInt(splitBookInfo[2])
+		);
+	}
+
+	private void validateBookInfoFormat(int size) {
+		if (size != 3) {
+			throw LibraryException.of(INVALID_BOOK_INFO_FORMAT);
+		}
 	}
 }
