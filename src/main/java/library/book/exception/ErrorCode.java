0546cf6be93c053d87c61c26058411aa4401c4e7
EunChanNam <namhm23@gmail.com>
2023-09-23 23:29:32 +0900

    feat: 도서 등록 기능을 처리하는 FunctionManger 구현

--- src/main/java/library/book/exception/ErrorCode.java
+++ src/main/java/library/book/exception/ErrorCode.java
@@ -4,7 +4,8 @@ public enum ErrorCode {
 
 	ONLY_NUMBER("페이지는 숫자만 입력해주세요."),
 	FILE_READ_FAIL("File Read 오류입니다."),
-	FILE_WRITE_FAIL("File Write 오류입니다.")
+	FILE_WRITE_FAIL("File Write 오류입니다."),
+	TYPE_MISS_MATCH("주입된 타입이 틀립니다."),
 	;
 
 	private final String message;
