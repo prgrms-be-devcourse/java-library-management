ea1c73d73c80d2d83dcf1b4055e59db2ead05ca6
EunChanNam <namhm23@gmail.com>
2023-09-23 23:29:23 +0900

    feat: 테스트용 MockBookManagerService 구현

---
+++ src/test/java/library/book/mock/MockBookManageService.java
@@ -0,0 +1,12 @@
+package library.book.mock;
+
+import library.book.application.BookManageService;
+import library.book.application.dto.request.RegisterBookRequest;
+
+public class MockBookManageService implements BookManageService {
+
+	@Override
+	public void registerBook(RegisterBookRequest request) {
+
+	}
+}
