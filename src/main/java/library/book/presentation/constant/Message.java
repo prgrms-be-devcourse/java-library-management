6bca1c45d1a643dbc02e32d37fbadf60db450f6f
EunChanNam <namhm23@gmail.com>
2023-09-24 01:17:02 +0900

    feat: 등록하는 도서 정보 콘솔입력 메소드 구현

--- src/main/java/library/book/presentation/constant/Message.java
+++ src/main/java/library/book/presentation/constant/Message.java
@@ -4,9 +4,9 @@ public enum Message {
 
 	ENTRY_REGISTER_MENU("[System] 도서 등록 메뉴로 넘어갑니다.\n"),
 	INPUT_BOOK_NAME("Q. 등록할 도서 제목을 입력하세요.\n"),
-	INPUT_AUTHOR_NAME("Q. 작가 이름을 입력하세요.\n"),
-	INPUT_PAGES("Q. 페이지 수를 입력하세요.\n"),
-	COMPLETE_REGISTER("[System] 도서 등록이 완료되었습니다.\n"),
+	INPUT_AUTHOR_NAME("\n\nQ. 작가 이름을 입력하세요.\n"),
+	INPUT_PAGES("\n\nQ. 페이지 수를 입력하세요.\n"),
+	COMPLETE_REGISTER("\n\n[System] 도서 등록이 완료되었습니다.\n"),
 	;
 
 	private final String value;
--- src/main/java/library/book/presentation/utils/ConsoleProcessor.java
+++ src/main/java/library/book/presentation/utils/ConsoleProcessor.java
@@ -1,5 +1,7 @@
 package library.book.presentation.utils;
 
+import static library.book.presentation.constant.Message.*;
+
 import library.book.infra.console.input.InputConsole;
 import library.book.infra.console.output.OutputConsole;
 import library.book.presentation.converter.NumberConverter;
@@ -24,5 +26,42 @@ public class ConsoleProcessor {
 		outputConsole.showSelectFunction();
 
 		return converter.convert(inputConsole.inputNumber());
+	}
+
+	public String inputBookInfo() {
+		StringBuilder stringBuilder = new StringBuilder();
+
+		outputConsole.showSystemMessage(ENTRY_REGISTER_MENU.getValue());
+
+		outputConsole.showSystemMessage(INPUT_BOOK_NAME.getValue());
+		outputConsole.showInputPrefix();
+		appendStringInput(stringBuilder);
+
+		outputConsole.showSystemMessage(INPUT_AUTHOR_NAME.getValue());
+		outputConsole.showInputPrefix();
+		appendStringInput(stringBuilder);
+
+		outputConsole.showSystemMessage(INPUT_PAGES.getValue());
+		outputConsole.showInputPrefix();
+		appendIntegerInput(stringBuilder);
+
+		outputConsole.showSystemMessage(COMPLETE_REGISTER.getValue());
+
+		String result = stringBuilder.toString();
+		return result.substring(0, result.length() - 1); //마지막 delimiter 제거
+	}
+
+	private void appendStringInput(StringBuilder stringBuilder) {
+		stringBuilder.append(inputConsole.inputString());
+		appendDelimiter(stringBuilder);
+	}
+
+	private void appendIntegerInput(StringBuilder stringBuilder) {
+		stringBuilder.append(converter.convert(inputConsole.inputNumber()));
+		appendDelimiter(stringBuilder);
+	}
+
+	private void appendDelimiter(StringBuilder stringBuilder) {
+		stringBuilder.append("/");
 	}
 }
