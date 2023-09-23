2e8b757a5570954ff2a0a3cadf0259f4b9a2804c
EunChanNam <namhm23@gmail.com>
2023-09-23 18:09:39 +0900

    feat: IoBookRepository 구현
    * implements BookRepository

--- src/main/java/library/book/exception/ErrorCode.java
+++ src/main/java/library/book/exception/ErrorCode.java
@@ -2,7 +2,8 @@ package library.book.exception;
 
 public enum ErrorCode {
 
-	ONLY_NUMBER("페이지는 숫자만 입력해주세요.")
+	ONLY_NUMBER("페이지는 숫자만 입력해주세요."),
+	FILE_READ("File Read 오류입니다.")
 	;
 
 	private final String message;
---
+++ src/test/java/library/book/infra/repository/IoBookRepositoryTest.java
@@ -0,0 +1,50 @@
+package library.book.infra.repository;
+
+import static library.book.exception.ErrorCode.*;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.api.ThrowableAssert.ThrowingCallable;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+import library.book.exception.LibraryException;
+
+@DisplayName("[IoBookRepository Test] - Infra")
+class IoBookRepositoryTest {
+
+	@Nested
+	@DisplayName("[생성자 테스트]")
+	class constructorTest {
+
+		@Test
+		@DisplayName("[Success]")
+		void success() {
+			//given
+			final String filePath = "src/test/resources/static/test.json";
+
+			//when
+			Executable when = () -> new IoBookRepository(filePath);
+
+			//then
+			assertDoesNotThrow(when);
+		}
+
+		@Test
+		@DisplayName("[Fail] filePath 에 파일이 존재하지 않아 실패한다")
+		void failWhenNotExistFile() {
+			//given
+			final String filePath = "no.json";
+
+			//when
+			ThrowingCallable when = () -> new IoBookRepository(filePath);
+
+			//then
+			assertThatThrownBy(when)
+				.isInstanceOf(LibraryException.class)
+				.hasMessageContaining(FILE_READ.getMessage());
+		}
+	}
+}
