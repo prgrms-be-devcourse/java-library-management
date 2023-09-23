e2c4da021fa5f952281886614a268f247bc276dd
EunChanNam <namhm23@gmail.com>
2023-09-23 18:33:40 +0900

    refactor: filePath 내부 변수로 추가 & try with resource 적용

--- src/main/java/library/book/infra/repository/IoBookRepository.java
+++ src/main/java/library/book/infra/repository/IoBookRepository.java
@@ -3,6 +3,7 @@ package library.book.infra.repository;
 import static library.book.exception.ErrorCode.*;
 
 import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -14,16 +15,19 @@ import library.book.exception.LibraryException;
 
 public class IoBookRepository implements BookRepository {
 
+	private final String filePath;
+
 	private final ObjectMapper objectMapper = new ObjectMapper();
 
-	private final Map<String, Book> bookStorage;
+	private final Map<Long, Book> bookStorage;
 
 	public IoBookRepository(final String filePath) {
-		try {
-			FileInputStream inputStream = new FileInputStream(filePath);
+		this.filePath = filePath;
+
+		try (FileInputStream inputStream = new FileInputStream(filePath)) {
 			this.bookStorage = objectMapper.readValue(inputStream, ConcurrentHashMap.class);
 		} catch (IOException e) {
-			throw LibraryException.of(FILE_READ);
+			throw LibraryException.of(FILE_READ_FAIL);
 		}
 	}
 
