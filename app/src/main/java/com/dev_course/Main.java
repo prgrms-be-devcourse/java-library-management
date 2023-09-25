2df1d5bc6592462307fa531f605d9ce2c2482a98
ASak1104 <khw56184@gmail.com>
2023-09-26 01:39:55 +0900

    feat : 도서관 애플리케이션 실행 기능 추가
    milestone #1

--- app/src/main/java/com/dev_course/Main.java
+++ app/src/main/java/com/dev_course/Main.java
@@ -1,7 +1,16 @@
 package com.dev_course;
 
+import com.dev_course.library.LibrarySystem;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        LibrarySystem library = new LibrarySystem(reader);
+
+        library.run();
     }
 }
