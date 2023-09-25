e585c5db1574528f97019b02ba8469659b77e7f4
EunChanNam <namhm23@gmail.com>
2023-09-25 10:50:14 +0900

    feat: 애플리케이션 커스텀 예외를 핸들링하는 BookExceptionHandler 구현

---
+++ src/test/java/library/book/presentation/proxy/BookExceptionHandlerTest.java
@@ -0,0 +1,55 @@
+package library.book.presentation.proxy;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+import library.book.infra.console.output.ConsoleOutputHandler;
+import library.book.infra.console.output.OutputHandler;
+import library.book.mock.MockBookController;
+import library.book.mock.MockExceptionBookController;
+
+@DisplayName("[BookExceptionHandler Test] - Presentation")
+class BookExceptionHandlerTest {
+
+	private final OutputHandler outputHandler = new ConsoleOutputHandler();
+	private BookExceptionHandler bookExceptionHandler;
+
+	@Nested
+	@DisplayName("[run 테스트]")
+	class runTest {
+
+		@Test
+		@DisplayName("[Success] 예외가 발생할 경우 예외를 잡아서 콘솔로 메세지를 출력한다.")
+		void successWhenException() {
+			//given
+			bookExceptionHandler = new BookExceptionHandler(
+				new MockExceptionBookController(), outputHandler
+			);
+
+			//when
+			Executable when = bookExceptionHandler::run;
+
+			//then
+			assertDoesNotThrow(when);
+		}
+
+		@Test
+		@DisplayName("[Success] 예외가 발생하지 않을 경우 통과한다.")
+		void successWhenNoException() {
+			//given
+			bookExceptionHandler = new BookExceptionHandler(
+				new MockBookController(), outputHandler
+			);
+
+			//when
+			Executable when = bookExceptionHandler::run;
+
+			//then
+			assertDoesNotThrow(when);
+		}
+	}
+}
