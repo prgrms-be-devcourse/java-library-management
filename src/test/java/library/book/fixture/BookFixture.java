8ab3bb29ec64333a8c6622e86cb861a4da467700
EunChanNam <namhm23@gmail.com>
2023-09-25 16:00:46 +0900

    feat: BookFixture 에 BookStatus 속성 및 BookSearchResponse 로 변환 메소드 추가

--- src/test/java/library/book/fixture/BookFixture.java
+++ src/test/java/library/book/fixture/BookFixture.java
@@ -1,19 +1,24 @@
 package library.book.fixture;
 
+import static library.book.domain.Status.BookStatus.*;
+
 import library.book.application.dto.request.RegisterBookRequest;
+import library.book.application.dto.response.BookSearchResponse;
 import library.book.domain.Book;
+import library.book.domain.Status.BookStatus;
 
 public enum BookFixture {
 
-	A(1L, "titleA", "authorA", 100),
-	B(2L, "titleB", "authorB", 200),
-	C(3L, "titleC", "authorC", 300),
+	A(1L, "titleA", "authorA", 100, AVAILABLE_RENT),
+	B(2L, "titleB", "authorB", 200, RENTED),
+	C(3L, "titleC", "authorC", 300, LOST),
 	;
 
 	private final long id;
 	private final String title;
 	private final String authorName;
 	private final int pages;
+	private final BookStatus bookStatus;
 
 	public long getId() {
 		return id;
@@ -31,16 +36,22 @@ public enum BookFixture {
 		return pages;
 	}
 
+	public BookStatus getBookStatus() {
+		return bookStatus;
+	}
+
 	BookFixture(
 		final long id,
 		final String title,
 		final String authorName,
-		final int pages
+		final int pages,
+		final BookStatus bookStatus
 	) {
 		this.id = id;
 		this.title = title;
 		this.authorName = authorName;
 		this.pages = pages;
+		this.bookStatus = bookStatus;
 	}
 
 	public Book toEntity() {
@@ -54,5 +65,9 @@ public enum BookFixture {
 
 	public RegisterBookRequest toRegisterRequest() {
 		return new RegisterBookRequest(title, authorName, pages);
+	}
+
+	public BookSearchResponse toSearchResponse() {
+		return new BookSearchResponse(id, title, authorName, pages, bookStatus.getDescription());
 	}
 }
