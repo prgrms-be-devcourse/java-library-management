3e0935ee6ee32a461b49f57f7ba68c6c09f7f4c1
EunChanNam <namhm23@gmail.com>
2023-09-25 10:16:52 +0900

    feat: 모드에 따라 필요한 BookRepository 를 제공하는 ModeManager 구현

---
+++ src/test/java/library/book/presentation/proxy/ModeManagerTest.java
@@ -0,0 +1,46 @@
+package library.book.presentation.proxy;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import library.book.domain.BookRepository;
+import library.book.infra.repository.IoBookRepository;
+import library.book.infra.repository.TestBookRepository;
+
+@DisplayName("[ModeManager Test] - Presentation")
+class ModeManagerTest {
+
+	@Nested
+	@DisplayName("[getRepository 테스트]")
+	class getRepositoryTest {
+
+		@Test
+		@DisplayName("[ONE]")
+		void ONE() {
+			//given
+			ModeManager one = ModeManager.ONE;
+
+			//when
+			BookRepository result = one.getRepository();
+
+			//then
+			assertThat(result).isInstanceOf(IoBookRepository.class);
+		}
+
+		@Test
+		@DisplayName("[TWO]")
+		void TWO() {
+			//given
+			ModeManager two = ModeManager.TWO;
+
+			//when
+			BookRepository result = two.getRepository();
+
+			//then
+			assertThat(result).isInstanceOf(TestBookRepository.class);
+		}
+	}
+}
