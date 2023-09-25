92649675e760dd8909d447d8c1e77bfe7515d9ab
EunChanNam <namhm23@gmail.com>
2023-09-25 10:50:14 +0900

    feat: 애플리케이션 커스텀 예외를 핸들링하는 BookExceptionHandler 구현

---
+++ src/main/java/library/book/presentation/proxy/BookExceptionHandler.java
@@ -0,0 +1,29 @@
+package library.book.presentation.proxy;
+
+import library.book.exception.LibraryException;
+import library.book.infra.console.output.OutputHandler;
+import library.book.presentation.BookController;
+
+public class BookExceptionHandler extends BookController {
+
+	private final BookController target;
+	private final OutputHandler outputHandler;
+
+	public BookExceptionHandler(
+		final BookController target,
+		final OutputHandler outputHandler
+	) {
+		super(null, null);
+		this.target = target;
+		this.outputHandler = outputHandler;
+	}
+
+	@Override
+	public void run() {
+		try {
+			target.run();
+		} catch (LibraryException e) {
+			outputHandler.showSystemMessage(e.getMessage());
+		}
+	}
+}
---
+++ src/test/java/library/book/mock/MockBookController.java
@@ -0,0 +1,15 @@
+package library.book.mock;
+
+import library.book.presentation.BookController;
+
+public class MockBookController extends BookController {
+
+	public MockBookController() {
+		super(null, null);
+	}
+
+	@Override
+	public void run() {
+
+	}
+}
