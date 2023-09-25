cd6dbbdde4c2a737d5123ed57e12987ddd370c79
EunChanNam <namhm23@gmail.com>
2023-09-25 13:21:24 +0900

    feat: BookClient 테스트 시 실제 데이터 파일에 데이터가 추가되지 않도록 타겟 클래스 모킹

--- src/test/java/library/book/presentation/proxy/BookClientTest.java
+++ src/test/java/library/book/presentation/proxy/BookClientTest.java
@@ -2,11 +2,14 @@ package library.book.presentation.proxy;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.lang.reflect.Field;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
 
 import library.book.infra.console.output.ConsoleOutputHandler;
+import library.book.mock.MockBookController;
 import library.book.mock.MockInputHandler;
 import library.book.presentation.converter.InputConverter;
 
@@ -19,7 +22,12 @@ class BookClientTest {
 
 	@Test
 	@DisplayName("[run 테스트]")
-	void runTest() {
+	void runTest() throws Exception {
+		//given
+		Field field = bookClient.getClass().getDeclaredField("target");
+		field.setAccessible(true);
+		field.set(bookClient, new MockBookController());
+
 		//when
 		Executable when = bookClient::run;
 
