c6a9858a0ca817a4c8e87e5d15a113193886dd29
EunChanNam <namhm23@gmail.com>
2023-09-25 13:04:26 +0900

    refactor: 사용되지 않는 ErrorCode 제거

--- src/main/java/library/book/exception/ErrorCode.java
+++ src/main/java/library/book/exception/ErrorCode.java
@@ -5,7 +5,6 @@ public enum ErrorCode {
 	ONLY_NUMBER("페이지는 숫자만 입력해주세요."),
 	FILE_READ_FAIL("File Read 오류입니다."),
 	FILE_WRITE_FAIL("File Write 오류입니다."),
-	TYPE_MISS_MATCH("주입된 타입이 틀립니다."),
 	INVALID_BOOK_INFO_FORMAT("도서 정보를 입력 할 때 '|||' 는 입력할 수 없습니다."),
 	;
 
