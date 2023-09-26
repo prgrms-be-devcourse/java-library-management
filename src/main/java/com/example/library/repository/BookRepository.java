7a55c03478af7ed51cac11cd5a307f27043be8bb
YongNyeo <won05121@gmail.com>
2023-09-26 20:05:50 +0900

    feat: Repository의 인터페이스를 구현한다.

---
+++ src/main/java/com/example/library/repository/BookRepository.java
@@ -0,0 +1,24 @@
+package com.example.library.repository;
+
+import com.example.library.domain.Book;
+
+public interface BookRepository {
+    void addBook(Book book);
+    void printAll();
+
+    void printByTitle(String bookName);
+
+    boolean borrowBook(int bookId);
+
+    boolean returnBook(int bookId);
+
+    boolean loseBook(int bookId);
+
+    boolean deleteBook(int bookId);
+
+    void arrangeBookStatus();
+
+    void saveBookList();
+
+    int size();
+}
