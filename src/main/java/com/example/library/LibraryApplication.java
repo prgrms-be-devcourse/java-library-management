93fe0d30f658e17fd107ff8ac4e4c45d53000a8d
YongNyeo <won05121@gmail.com>
2023-09-26 19:55:54 +0900

    chore: 환경설정을 변경한다.

--- src/main/java/com/example/library/LibraryApplication.java
+++ src/main/java/com/example/library/LibraryApplication.java
@@ -1,5 +1,6 @@
 package com.example.library;
 
+import com.example.library.service.LibraryService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,8 @@ public class LibraryApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LibraryApplication.class, args);
+        LibraryService libraryService = new LibraryService();
+
     }
 
 }
