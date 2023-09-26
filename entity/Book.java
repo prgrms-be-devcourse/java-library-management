f10a224621efe854ef34e33c436d8f5c0473efec
ZZAMBAs <rksidksrksi@naver.com>
2023-09-26 17:28:42 +0900

    Feat: Book 클래스에 생성자와 자신의 필드 상태를 출력하는 코드를 추가한다.

--- entity/Book.java
+++ entity/Book.java
@@ -1,17 +1,45 @@
 package entity;
 
+import java.lang.reflect.Field;
+import java.util.Arrays;
+
 public class Book {
-    String number;
+    static int numberCnt = 1;
+    static String[] fieldKoreanName = {"도서번호", "제목", "작가", "페이지 수", "상태"};
+    int number;
     String title;
     String author;
     int pageNum;
     State state;
 
-    public Book(String number, String title, String author, int pageNum, State state) {
-        this.number = number;
+    private Book(String title, String author, int pageNum) {
+        this.number = numberCnt++;
         this.title = title;
         this.author = author;
         this.pageNum = pageNum;
-        this.state = state;
+        this.state = State.AVAILABLE;
+    }
+
+    public static Book createBook(String title, String author, int pageNum){
+        return new Book(title, author, pageNum);
+    }
+
+    public static Book createBook(String[] bookInfo){
+        if (bookInfo.length != 3)
+            throw new IllegalArgumentException("입력 개수가 3개가 아닙니다.");
+        return createBook(bookInfo[0], bookInfo[1], Integer.parseInt(bookInfo[2]));
+    }
+
+    public String printInfo(){
+        Field[] fields = this.getClass().getFields();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < fields.length; i++) {
+            sb.append(fieldKoreanName[i]);
+            sb.append(" ");
+            sb.append(fields[i].getName());
+            sb.append("\n");
+        }
+
+        return sb.toString();
     }
 }
