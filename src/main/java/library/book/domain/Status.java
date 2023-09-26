742d953533bec2d06185d15280dccd20307004bd
EunChanNam <namhm23@gmail.com>
2023-09-26 13:48:34 +0900

    enhancement: 도서 대여 비즈니스 로직에 정리중인 경우에 대해서 정리가 끝났는지 확인하는 로직 추가

--- src/main/java/library/book/domain/Status.java
+++ src/main/java/library/book/domain/Status.java
@@ -11,21 +11,12 @@ public class Status {
 
 	private BookStatus bookStatus;
 
-	private LocalDateTime cleaningStartTime;
+	private LocalDateTime cleaningEndTime;
 
 	//== Factory 메소드 ==//
 	public Status() {
 		this.bookStatus = AVAILABLE_RENT;
-		this.cleaningStartTime = null;
-	}
-
-	//== Utility 메소드 ==//
-	public BookStatus getBookStatus() {
-		return bookStatus;
-	}
-
-	public LocalDateTime getCleaningStartTime() {
-		return cleaningStartTime;
+		this.cleaningEndTime = null;
 	}
 
 	public enum BookStatus {
@@ -33,8 +24,7 @@ public class Status {
 		AVAILABLE_RENT("대여 가능"),
 		RENTED("대여중"),
 		CLEANING("정리중"),
-		LOST("분실")
-		;
+		LOST("분실");
 
 		private final String description;
 
@@ -47,17 +37,47 @@ public class Status {
 		}
 	}
 
+	//== Utility 메소드 ==//
+	public BookStatus getBookStatus() {
+		return bookStatus;
+	}
+
+	public LocalDateTime getCleaningEndTime() {
+		return cleaningEndTime;
+	}
+
 	//== Business 메소드 ==//
 	public void updateBookStatusToRented() {
+		checkCleaningComplete();
 		validateIsAbleToRent();
 		this.bookStatus = RENTED;
 	}
 
 	private void validateIsAbleToRent() {
-		switch (this.bookStatus) {
+		switch (bookStatus) {
 			case RENTED -> throw BookException.of(ALREADY_RENTED);
 			case LOST -> throw BookException.of(NOW_LOST);
 			case CLEANING -> throw BookException.of(NOW_CLEANING);
+		}
+	}
+
+	private void checkCleaningComplete() {
+		if (bookStatus.equals(CLEANING)) {
+			validateCleaningEndTimeIsNotNull();
+			updateCleaningStatus();
+		}
+	}
+
+	private void validateCleaningEndTimeIsNotNull() {
+		if (cleaningEndTime == null) {
+			throw BookException.of(INVALID_CLEANING_END_TIME);
+		}
+	}
+
+	private void updateCleaningStatus() {
+		if (cleaningEndTime.isBefore(LocalDateTime.now())) {
+			this.cleaningEndTime = null;
+			this.bookStatus = AVAILABLE_RENT;
 		}
 	}
 }
