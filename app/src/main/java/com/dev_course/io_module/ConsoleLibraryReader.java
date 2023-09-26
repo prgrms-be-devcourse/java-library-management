987bf374d44fbc00004bdf698a820d8e274f985c
ASak1104 <khw56184@gmail.com>
2023-09-26 17:54:12 +0900

    edit : 도서관 리더의 read() 함수 수정
    read() 함수가 IOException을 던지도록 수정
    
    milestone #1

--- app/src/main/java/com/dev_course/io_module/ConsoleLibraryReader.java
+++ app/src/main/java/com/dev_course/io_module/ConsoleLibraryReader.java
@@ -3,28 +3,30 @@ package com.dev_course.io_module;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Objects;
 
 public class ConsoleLibraryReader implements LibraryReader {
     private final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private String str;
 
-    private void next() {
-        try {
-            str = br.readLine();
-        } catch (IOException e) {
-            str = "";
-        }
+    private void next() throws IOException {
+        str = br.readLine();
     }
 
     @Override
-    public String read() {
+    public String read() throws IOException {
         next();
         return str;
     }
 
     @Override
     public String readOrElse(String defaultValue) {
-        next();
-        return str.isEmpty() ? defaultValue : str;
+        try {
+            next();
+        } catch (IOException ignored) {
+            str = null;
+        }
+
+        return Objects.isNull(str) ? defaultValue : str;
     }
 }
--- app/src/main/java/com/dev_course/io_module/LibraryReader.java
+++ app/src/main/java/com/dev_course/io_module/LibraryReader.java
@@ -1,6 +1,8 @@
 package com.dev_course.io_module;
 
+import java.io.IOException;
+
 public interface LibraryReader {
-    String read();
+    String read() throws IOException;
     String readOrElse(String defaultValue);
 }
