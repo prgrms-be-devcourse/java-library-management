998c7e276d7f1340ec51fef39b04e8d4684ca192
ASak1104 <khw56184@gmail.com>
2023-09-27 00:00:10 +0900

    feat : Main 클래스에서 도서관 모듈에 의존성 주입
    milestone #1

--- app/src/main/java/com/dev_course/Main.java
+++ app/src/main/java/com/dev_course/Main.java
@@ -1,18 +1,25 @@
 package com.dev_course;
 
+import com.dev_course.book.Book;
 import com.dev_course.book.BookManager;
-import com.dev_course.book.BookManagerImpl;
+import com.dev_course.book.ListBookManager;
 import com.dev_course.io_module.ConsoleLibraryReader;
 import com.dev_course.io_module.ConsoleLibraryWriter;
 import com.dev_course.io_module.LibraryReader;
 import com.dev_course.io_module.LibraryWriter;
 import com.dev_course.library.LibrarySystem;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         LibraryReader libraryReader = new ConsoleLibraryReader();
         LibraryWriter libraryWriter = new ConsoleLibraryWriter();
-        BookManager bookManager = new BookManagerImpl();
+
+        List<Book> bookList = new ArrayList<>();
+        int seed = 0;
+        BookManager bookManager = new ListBookManager(bookList, seed);
 
         LibrarySystem library = new LibrarySystem(libraryReader, libraryWriter, bookManager);
 
