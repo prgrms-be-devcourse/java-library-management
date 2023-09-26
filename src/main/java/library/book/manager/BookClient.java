72e241c9cabbed8f0886ffeeb651e6215d7f90d3
EunChanNam <namhm23@gmail.com>
2023-09-27 00:47:17 +0900

    enhancement: 잘못된 모드, 기능 번호 입력시 예외처리 추가

--- src/main/java/library/book/manager/BookClient.java
+++ src/main/java/library/book/manager/BookClient.java
@@ -1,7 +1,10 @@
 package library.book.manager;
 
+import static library.book.exception.ErrorCode.*;
+
 import library.book.application.BookService;
 import library.book.domain.BookRepository;
+import library.book.exception.BookException;
 import library.book.infra.console.input.InputHandler;
 import library.book.infra.console.output.OutputHandler;
 import library.book.presentation.BookController;
@@ -22,13 +25,22 @@ public class BookClient extends BookController {
 		InputConverter converter = new InputConverter();
 
 		ConsoleProcessor consoleProcessor = new ConsoleProcessor(inputHandler, outputHandler, converter);
-		String mode = consoleProcessor.inputNumber(OutputHandler::showSelectMode);
+
+		String mode = inputMode(consoleProcessor);
 
 		BookRepository bookRepository = ModeManager.valueOf(mode).getRepository();
 		BookService bookService = ModeManager.valueOf(mode).getService(bookRepository);
 
 		FunctionExecutor executor = new FunctionExecutor(bookService, consoleProcessor);
 		this.target = new BookController(executor, consoleProcessor);
+	}
+
+	private String inputMode(final ConsoleProcessor consoleProcessor) {
+		try {
+			return consoleProcessor.inputNumber(OutputHandler::showSelectMode);
+		} catch (IllegalArgumentException e) {
+			throw BookException.of(NOT_SUPPORT_FUNCTION);
+		}
 	}
 
 	@Override
