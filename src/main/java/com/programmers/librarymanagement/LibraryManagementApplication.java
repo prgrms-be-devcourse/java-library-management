9b4004ad8ea6dc757fa92e3b7f726fc69284a9f6
letskuku <yujung9288@naver.com>
2023-09-26 19:37:14 +0900

    init: 초기 환경설정

---
+++ src/main/java/com/programmers/librarymanagement/LibraryManagementApplication.java
@@ -0,0 +1,10 @@
+package com.programmers.librarymanagement;
+
+public class LibraryManagementApplication {
+
+	public static void main(String[] args) {
+
+
+	}
+
+}
---
+++ src/test/java/com/programmers/librarymanagement/LibraryManagementApplicationTests.java
@@ -0,0 +1,5 @@
+package com.programmers.librarymanagement;
+
+class LibraryManagementApplicationTests {
+
+}
