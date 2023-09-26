78334cee5f3d80bd53fe235654c05b54cb72d925
ASak1104 <khw56184@gmail.com>
2023-09-27 00:27:18 +0900

    feat : 도서관 모듈 도서 삭제 기능 추가
    milestone #1

--- app/src/main/java/com/dev_course/library/LibraryMessage.java
+++ app/src/main/java/com/dev_course/library/LibraryMessage.java
@@ -17,13 +17,15 @@ public enum LibraryMessage {
             7. 도서 삭제"""),
     EXIT("[System] 프로그램을 종료합니다."),
     INVALID_FUNCTION("[System] 사용할 수 없는 기능입니다.\n"),
+    INVALID_INPUT("[System] 잘못된 입력 입니다.\n"),
     CREATE_BOOK("[System] 도서 등록 메뉴로 넘어갑니다.\n"),
+    DELETE_BOOK("[System] 도서 삭제 처리 메뉴로 넘어갑니다.\n"),
     LIST_BOOK("[System] 전체 도서 목록입니다.\n"),
     FIND_BOOK_BY_TITLE("[System] 제목으로 도서 검색 메뉴로 넘어갑니다.\n"),
-    INVALID_INPUT("[System] 잘못된 입력 입니다.\n"),
     READ_CREATE_BOOK_TITLE("Q. 등록할 도서 제목을 입력하세요."),
     READ_CREATE_BOOK_AUTHOR("Q. 작가 이름을 입력하세요."),
-    READ_CREATE_BOOK_PAGES("Q. 페이지 수를 입력하세요.");
+    READ_CREATE_BOOK_PAGES("Q. 페이지 수를 입력하세요."),
+    READ_DELETE_BOOK_ID("Q. 삭제 처리할 도서번호를 입력하세요");
 
 
     private final String msg;
