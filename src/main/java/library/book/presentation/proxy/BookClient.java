6568ec21f7ab3cd0c0478f982ac06362e98022a1
EunChanNam <namhm23@gmail.com>
2023-09-26 22:49:16 +0900

    feat: ModeManger 로 DataSaver 프록시를 연결해서 BookClient proxy 에 적용

--- src/main/java/library/book/presentation/proxy/BookClient.java
+++ src/main/java/library/book/presentation/proxy/BookClient.java
@@ -1,8 +1,6 @@
 package library.book.presentation.proxy;
 
 import library.book.application.BookService;
-import library.book.application.DefaultBookService;
-import library.book.application.proxy.DataSaver;
 import library.book.domain.BookRepository;
 import library.book.infra.console.input.InputHandler;
 import library.book.infra.console.output.OutputHandler;
@@ -21,13 +19,14 @@ public class BookClient extends BookController {
 		final InputConverter converter
 	) {
 		super(null, null);
+
 		ConsoleProcessor consoleProcessor = new ConsoleProcessor(inputHandler, outputHandler, converter);
 		String mode = consoleProcessor.inputNumber(OutputHandler::showSelectMode);
 
 		BookRepository bookRepository = ModeManager.valueOf(mode).getRepository();
-		BookService bookManageService = new DataSaver(new DefaultBookService(bookRepository), bookRepository);
+		BookService bookService = ModeManager.valueOf(mode).getService(bookRepository);
 
-		FunctionExecutor executor = new FunctionExecutor(bookManageService, consoleProcessor);
+		FunctionExecutor executor = new FunctionExecutor(bookService, consoleProcessor);
 		this.target = new BookController(executor, consoleProcessor);
 	}
 
