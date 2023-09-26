66b62b5c8e7b36b0ee3f4ada0036b07b56134d2a
ASak1104 <khw56184@gmail.com>
2023-09-27 00:28:12 +0900

    docs : 존재하는 도서를 생성하려고 할 때 출력하는 메시지 수정
    milestone #1

--- app/src/main/java/com/dev_course/book/BookManagerMessage.java
+++ app/src/main/java/com/dev_course/book/BookManagerMessage.java
@@ -1,7 +1,7 @@
 package com.dev_course.book;
 
 public enum BookManagerMessage {
-    ALREADY_EXIST_TITLE("[System] 존재하는 도서 입니다.\n"),
+    ALREADY_EXIST_TITLE("[System] 이미 존재하는 도서 입니다.\n"),
     NOT_EXIST_ID("[System] 존재하지 않는 도서번호 입니다.\n"),
     SUCCESS_CREATE_BOOK("[System] 도서 등록이 완료되었습니다.\n"),
     SUCCESS_DELETE_BOOK("[System] 도서가 삭제 처리 되었습니다.\n");
