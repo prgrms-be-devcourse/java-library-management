9ec2397ba99c07c202b95ceacfffb4a1e2a966ed
ZZAMBAs <rksidksrksi@naver.com>
2023-09-26 17:25:08 +0900

    Feat: 책을 관리하는 BookManager 인터페이스를 생성한다.

---
+++ manage/BookManager.java
@@ -0,0 +1,16 @@
+package manage;
+
+import entity.Book;
+import entity.State;
+
+import java.util.List;
+
+public interface BookManager {
+    Book register(Book book);
+    List<Book> searchAll();
+    List<Book> search(String text);
+    State rent(int bookNum);
+    State returnBook(int bookNum);
+    State lost(int bookNum);
+    State delete(int bookNum);
+}
