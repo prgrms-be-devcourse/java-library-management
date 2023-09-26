5b1693c059ed616ee16302a03812350b04ccbbea
voidmelody <sss9073@naver.com>
2023-09-26 15:28:09 +0900

    Refactor: "모드 열거형 객체 변수 추가" (Repository 인스턴스 추가)

--- src/main/java/org/library/entity/Mode.java
+++ src/main/java/org/library/entity/Mode.java
@@ -1,13 +1,20 @@
 package org.library.entity;
 
-public enum Mode {
-    APPLICATION(1,"일반 모드"),
-    TEST(2, "테스트 모드");
+import org.library.repository.ApplicationRepository;
+import org.library.repository.Repository;
+import org.library.repository.TestRepository;
 
-    Mode(int num, String name) {
+public enum Mode {
+    APPLICATION(1,"일반 모드", new ApplicationRepository()),
+    TEST(2, "테스트 모드", new TestRepository());
+
+    Mode(int num, String name, Repository repository) {
         this.num = num;
         this.name = name;
+        this.repository = repository;
     }
+
     private int num;
     private String name;
+    private Repository repository;
 }
