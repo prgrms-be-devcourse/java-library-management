411446965b6234c1b575f61cdf8839aabe08c858
ZZAMBAs <rksidksrksi@naver.com>
2023-09-26 17:29:10 +0900

    Feat: State에 삭제된 상태를 추가한다.

--- entity/State.java
+++ entity/State.java
@@ -1,8 +1,14 @@
 package entity;
 
 public enum State {
-    AVAILABLE,
-    RENTED,
-    PROCESSING,
-    LOST
+    AVAILABLE("대여 가능"),
+    RENTED("대여중"),
+    PROCESSING("정리중"),
+    LOST("분실됨"),
+    DELETED("삭제됨");
+
+    private String name;
+    private String koreanState;
+    State(String koreanState) {
+    }
 }
