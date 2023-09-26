885a3e21a8dbdddfd3fe1b091ebb9ec1f033b6ca
EunChanNam <namhm23@gmail.com>
2023-09-26 22:39:35 +0900

    feat: 서비스 로직 이후 json file 에 저장 로직을 호출해주는 DataSaver proxy 구현

---
+++ src/main/java/library/book/application/proxy/DataSaver.java
@@ -0,0 +1,60 @@
+package library.book.application.proxy;
+
+import java.util.List;
+
+import library.book.application.BookService;
+import library.book.application.dto.request.RegisterBookRequest;
+import library.book.application.dto.response.BookSearchResponse;
+import library.book.domain.BookRepository;
+
+public class DataSaver implements BookService {
+
+	private final BookService target;
+	private final BookRepository bookRepository;
+
+	public DataSaver(
+		final BookService target,
+		final BookRepository bookRepository
+	) {
+		this.target = target;
+		this.bookRepository = bookRepository;
+	}
+
+	@Override
+	public void registerBook(RegisterBookRequest request) {
+		target.registerBook(request);
+		bookRepository.updateData();
+	}
+
+	@Override
+	public List<BookSearchResponse> searchBooks() {
+		List<BookSearchResponse> result = target.searchBooks();
+		bookRepository.updateData();
+		return result;
+	}
+
+	@Override
+	public List<BookSearchResponse> searchBooks(String title) {
+		List<BookSearchResponse> result = target.searchBooks(title);
+		bookRepository.updateData();
+		return result;
+	}
+
+	@Override
+	public void rentBook(long id) {
+		target.rentBook(id);
+		bookRepository.updateData();
+	}
+
+	@Override
+	public void returnBook(long id) {
+		target.returnBook(id);
+		bookRepository.updateData();
+	}
+
+	@Override
+	public void registerAsLost(long id) {
+		target.registerAsLost(id);
+		bookRepository.updateData();
+	}
+}
