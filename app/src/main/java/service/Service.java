e15e9b55cfd524fd7393fc16f4c18695d7fb72a1
kimnamgyu <lethargic.hood@gmail.com>
2023-09-26 15:56:52 +0900

    feat : 요구사항을 수행하는 Service 인터페이스

---
+++ app/src/main/java/service/Service.java
@@ -0,0 +1,35 @@
+package service;
+
+public interface Service {
+
+    //csv에서 데이터를 가져오는 코드
+    public void load();
+
+    //main화면을 출력하는 코드
+    public void mainView();
+
+    //도서 등록 - 1
+    public void registration();
+
+    //전체 도서 목록을 조회하는 코드 - 2
+    public void viewAll();
+
+    //도서 검색 by title - 3
+    public void findByTitle();
+
+    //도서 대여 by id - 4
+    public void rentBook();
+
+    //도서 반납 by id - 5
+    public void returnBook();
+
+    //도서 분실 처리 by id - 6
+    public void lostBook();
+
+    //도서 삭제 by id - 7
+    public void deleteBook();
+
+    //종료하는 코드 - 8
+    public void exit();
+
+}
