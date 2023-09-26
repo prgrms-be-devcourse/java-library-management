cb8a067e161640a8d986a8752b11b589efb833dc
EunChanNam <namhm23@gmail.com>
2023-09-26 22:49:16 +0900

    feat: ModeManger 로 DataSaver 프록시를 연결해서 BookClient proxy 에 적용

--- src/main/java/library/book/presentation/proxy/ModeManager.java
+++ src/main/java/library/book/presentation/proxy/ModeManager.java
@@ -1,24 +1,42 @@
 package library.book.presentation.proxy;
 
+import java.util.function.Function;
 import java.util.function.Supplier;
 
+import library.book.application.BookService;
+import library.book.application.DefaultBookService;
+import library.book.application.proxy.DataSaver;
 import library.book.domain.BookRepository;
 import library.book.infra.repository.IoBookRepository;
 import library.book.infra.repository.TestBookRepository;
 
 public enum ModeManager {
 
-	ONE(() -> new IoBookRepository("src/main/resources/static/books.json")),
-	TWO(TestBookRepository::new)
-	;
+	ONE(
+		() -> new IoBookRepository("src/main/resources/static/books.json"),
+		bookRepository -> new DataSaver(new DefaultBookService(bookRepository), bookRepository)
+	),
+	TWO(
+		TestBookRepository::new,
+		DefaultBookService::new
+	);
 
 	private final Supplier<BookRepository> repositorySupplier;
+	private final Function<BookRepository, BookService> serviceFunction;
 
-	ModeManager(Supplier<BookRepository> repositorySupplier) {
+	ModeManager(
+		final Supplier<BookRepository> repositorySupplier,
+		final Function<BookRepository, BookService> serviceFunction
+	) {
 		this.repositorySupplier = repositorySupplier;
+		this.serviceFunction = serviceFunction;
 	}
 
 	public BookRepository getRepository() {
 		return this.repositorySupplier.get();
+	}
+
+	public BookService getService(final BookRepository bookRepository) {
+		return this.serviceFunction.apply(bookRepository);
 	}
 }
