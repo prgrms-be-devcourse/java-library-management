5e2d02b52ff3065daff3d918530318dabb95b365
YongNyeo <won05121@gmail.com>
2023-09-26 20:18:37 +0900

    test: 입력값에 대한 검증 테스트를 구현한다.

---
+++ src/test/java/com/example/library/io/InputTest.java
@@ -0,0 +1,53 @@
+package com.example.library.io;
+
+import com.example.library.validation.InputCheck;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class InputTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {"1,1","2,2"})
+    @DisplayName("모드 선택 입력값에 1,2만 성공한다.")
+    void successModeNumber(String input,int result) {
+
+        int modeNumber = InputCheck.isModeNumber(input);
+
+        assertThat(modeNumber).isEqualTo(result);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"4","124","abc"})
+    @DisplayName("모드 선택 입력값에 1,2를 제외하고 입력하면 실패한다.")
+    void failModeNumber(String input) {
+
+        assertThrows(NumberFormatException.class,
+                () -> InputCheck.isModeNumber(input));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"3,3","7,7"})
+    @DisplayName("메뉴 선택 입력값에 0~7만 성공한다.")
+    void successMenuNumber(String input,int result) {
+
+        int menuNumber = InputCheck.isMenuNumber(input);
+
+        assertThat(menuNumber).isEqualTo(result);
+    }
+    @ParameterizedTest
+    @ValueSource(strings = {"-1","12","a","214124"})
+    @DisplayName("모드 선택 입력값에 0~7를 제외하고 입력하면 실패한다.")
+    void failMenuNumber(String input) {
+
+        assertThrows(NumberFormatException.class,
+                () -> InputCheck.isMenuNumber(input));
+    }
+
+}
