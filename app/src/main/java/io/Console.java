a058b361e6cc6c7e105da4f22a0b9a3afd32c9d7
kimnamgyu <lethargic.hood@gmail.com>
2023-09-26 15:59:09 +0900

    feat : 모드 선택을 구현한 클래스(일반, 테스트)

---
+++ app/src/main/java/io/Console.java
@@ -0,0 +1,40 @@
+package io;
+
+import service.GeneralService;
+import service.Service;
+import service.TestService;
+
+import java.util.Scanner;
+
+public class Console {
+
+    Scanner scanner = new Scanner(System.in);
+
+    private Service service;
+
+    public void selectMode() {
+        System.out.println("Q. 모드를 선택해주세요.");
+        System.out.println("1. 일반 모드");
+        System.out.println("2. 테스트 모드");
+        System.out.println();
+
+        System.out.print("> ");
+
+        int mode = scanner.nextInt();
+        printMode(mode);
+    }
+
+    public void printMode(int mode) {
+        if (mode == 1) {
+            System.out.println("[System] 일반 모드로 애플리케이션을 실행합니다.");
+            service = new GeneralService();
+            service.mainView();
+        } else if (mode == 2) {
+            System.out.println("[System] 테스트 모드로 애플리케이션을 실행합니다.");
+            service = new TestService();
+            service.mainView();
+        } else {
+            throw new RuntimeException("[System] 잘못된 입력입니다.");
+        }
+    }
+}
