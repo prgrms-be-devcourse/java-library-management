92e5efb2b725d453b2167540fafd034174de4c84
uijin-j <uijin0531@gmail.com>
2023-09-28 03:18:29 +0900

    feat: 도서 등록 유효성 검사 추가

--- library-management/src/main/java/devcourse/backend/view/BookDto.java
+++ library-management/src/main/java/devcourse/backend/view/BookDto.java
@@ -10,6 +10,7 @@ public class BookDto {
     }
 
     public void setTitle(String title) {
+        if(title.equals("")) throw new IllegalArgumentException("제목은 빈칸일 수 없습니다.");
         this.title = title;
     }
 
@@ -18,6 +19,7 @@ public class BookDto {
     }
 
     public void setAuthor(String author) {
+        if(author.equals("")) throw new IllegalArgumentException("작가 이름은 빈칸일 수 없습니다.");
         this.author = author;
     }
 
@@ -26,6 +28,7 @@ public class BookDto {
     }
 
     public void setTotalPages(int totalPages) {
+        if (totalPages <= 0) throw new IllegalArgumentException("페이지 수는 0이상 입니다.");
         this.totalPages = totalPages;
     }
 }
