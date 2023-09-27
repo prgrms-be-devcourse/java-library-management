c8eac0c6cb921cd52dc4dda4082bbbee19612382
byulcode <bybit0408@gmail.com>
2023-09-27 13:37:43 +0900

    feat : 파일 읽기,쓰기를 위한 CsvFileUtil 생성

--- src/main/java/util/CsvFileUtil.java
+++ src/main/java/util/CsvFileUtil.java
@@ -1,38 +1,58 @@
 package util;
 
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import model.Book;
+import model.Status;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CsvFileUtil {
 
-    public static Long findLastId(String csvFile) {
-        File file = new File(csvFile);
+    private final String FILE_PATH = "book.csv";
+
+    public CsvFileUtil() throws IOException {
+        createFile();
+    }
+
+    private void createFile() throws IOException {
+        File file = new File(FILE_PATH);
         if (!file.exists()) {
-            return 0L;
+            file.createNewFile();
         }
+    }
 
-        try {
-            BufferedReader br = new BufferedReader(new FileReader(file));
-            String line;
-            String lastColumnValue = null;
-
-            while ((line = br.readLine()) != null) {
-                String[] columns = line.split(","); // CSV 열 구분 기호에 따라 변경
-
-                if (columns.length > 0) {
-                    lastColumnValue = columns[0]; // 첫 번째 열 값을 저장
+    public Map<Long, Book> readAllBooksFromCsv() {
+        Map<Long, Book> bookMemory = new HashMap<>();
+        File file = new File(FILE_PATH);
+        if (file.exists()) {
+            try (BufferedReader reader = new BufferedReader(new FileReader(FILE_PATH))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    String[] parts = line.split(",");
+                    bookMemory.put(Long.parseLong(parts[0]), new Book(Long.parseLong(parts[0]), parts[1], parts[2], Integer.parseInt(parts[3]), Status.findStatusByString(parts[4])));
                 }
+            } catch (IOException ignore) {
+                System.out.println("[System] 잘못된 파일 접근입니다.\n");
+            } catch (Exception e) {
+                System.out.println("[System] 손상된 파일입니다. 데이터 초기화 후 진행합니다.\n");
             }
+        }
+        return bookMemory;
+    }
 
-            if (lastColumnValue != null) {
-                return Long.parseLong(lastColumnValue);
+    public void writeFile(Map<Long, Book> bookMemory ) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_PATH))) {
+
+            for (Book book : bookMemory.values()) {
+                String csvLine = String.format("%d,%s,%s,%d,%s",
+                        book.getBookNo(), book.getTitle(), book.getAuthor(), book.getPageNum(), book.getStatus().getStatus());
+                writer.write(csvLine);
+                writer.newLine();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return 0L;
     }
 }
