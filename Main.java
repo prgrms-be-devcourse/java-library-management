0985121947b3fd432dd40d8cf33a7d083cc60e4f
ZZAMBAs <rksidksrksi@naver.com>
2023-09-27 23:46:28 +0900

    Refactor: 최초 콘솔 모드 선택 기능을 main 메서드로 이동한다.

--- Main.java
+++ Main.java
@@ -1,7 +1,33 @@
+import manage.FileManager;
+import manage.FileManagerImpl;
+import manage.ListBookManager;
 import manage.TotalManager;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        TotalManager.run();
+        int mode;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("모드를 선택합니다. 1. 운용 모드 2. 테스트 모드\n> ");
+        while (true) {
+            try {
+                mode = Integer.parseInt(sc.nextLine().trim());
+                if(mode < 1 || mode > 2) throw new NumberFormatException();
+                break;
+            }catch (NumberFormatException e){
+                System.out.println("잘못된 입력입니다.");
+            }
+        }
+
+        TotalManager totalManager;
+        if(mode == 1) {
+            FileManager fileManager = new FileManagerImpl();
+            totalManager = new TotalManager(new ListBookManager(fileManager.readCsv("res/temp.csv")), sc);
+        }else{
+            totalManager = new TotalManager(new ListBookManager(), sc);
+        }
+
+        totalManager.run();
     }
 }
--- manage/TotalManager.java
+++ manage/TotalManager.java
@@ -8,11 +8,16 @@ import java.util.List;
 import java.util.Scanner;
 
 public class TotalManager {
-    private static BookManager bookManager;
-    private static Scanner sc = new Scanner(System.in);
-    public static void run(){
+    private final BookManager bookManager;
+    private final Scanner sc;
+
+    public TotalManager(BookManager bookManager, Scanner sc) {
+        this.bookManager = bookManager;
+        this.sc = sc;
+    }
+
+    public void run(){
         int cmd = -1;
-        init();
         while (cmd != 8){ // enum이나 다른 방식으로 추후 리팩토링
             System.out.println("""
                     기능을 선택해 주세요.
@@ -49,37 +54,31 @@ public class TotalManager {
                 System.out.println("존재하지 않는 도서입니다. 이미 삭제된 도서일 수 있습니다. 최초 선택으로 이동합니다.\n");
             }
         }
+
     }
 
-    private static void init(){
-        System.out.print("모드를 선택합니다. 1. 운용 모드 2. 테스트 모드\n> ");
-        int mode = sc.nextInt();
-        String ignore = sc.nextLine();
+    private void register(){
+        System.out.print("등록 도서 정보를 입력합니다.\n도서 제목을 입력해 주세요.\n> ");
+        String[] bookInfo = new String[3];
+        bookInfo[0] = sc.nextLine().trim();
+        System.out.print("작가 이름을 입력해 주세요.\n> ");
+        bookInfo[1] = sc.nextLine().trim();
+        System.out.print("페이지 수를 입력해 주세요.(숫자만 입력해 주세요!)\n> ");
+        bookInfo[2] = sc.nextLine().trim();
 
-        if(mode == 1)
-            bookManager = new FileBookManager();
-        else if (mode == 2)
-            bookManager = new TestBookManager();
-    }
-
-    private static void register(){
-        System.out.println("등록 도서 정보를 입력합니다.\n도서 제목과 작가이름, 페이지 수를 공백으로 구분하여 입력해 주세요");
-        System.out.print("> ");
-        String[] bookInfo = sc.nextLine().split(" ");
-
-        Book createdBook = Book.createBook(bookInfo);
+        Book createdBook = new Book(bookInfo[0], bookInfo[1], Integer.parseInt(bookInfo[2]));
         bookManager.register(createdBook);
         System.out.println("정상 등록 되었습니다!\n");
     }
 
-    private static void searchAll(){
+    private void searchAll(){
         System.out.println("전체 도서 목록을 조회합니다.");
         List<Book> books = bookManager.searchAll();
 
         printingBookList(books);
     }
 
-    private static void search(){
+    private void search(){
         System.out.println("검색할 도서 제목 일부를 입력해주세요!");
         System.out.print("> ");
         String text = sc.nextLine().trim();
@@ -88,7 +87,7 @@ public class TotalManager {
         printingBookList(books);
     }
 
-    private static void printingBookList(List<Book> books){
+    private void printingBookList(List<Book> books){
         System.out.println();
 
         books.stream().forEach(book -> {
@@ -99,42 +98,42 @@ public class TotalManager {
         System.out.println("총 " + books.size() + "개의 도서가 조회되었습니다!\n");
     }
 
-    private static void rent(){
+    private void rent(){
         System.out.print("대여할 도서 번호를 입력해주세요.\n> ");
         int number = Integer.parseInt(sc.nextLine().trim());
         State initState = bookManager.rent(number);
 
         if (initState != State.AVAILABLE)
-            System.out.println("도서가 " + initState + " 상태이기 때문에 대여할 수 없습니다.\n");
+            System.out.println("도서가 " + initState.getKoreanState() + " 상태이기 때문에 대여할 수 없습니다.\n");
         else
             System.out.println("도서가 대여되었습니다!\n");
     }
 
-    private static void returnBook(){
+    private void returnBook(){
         System.out.print("반납할 도서 번호를 입력해 주세요\n> ");
         int number = Integer.parseInt(sc.nextLine().trim());
         State initState = bookManager.returnBook(number);
         if (initState == State.RENTED)
             System.out.println("정상 반납되었습니다.\n");
         else
-            System.out.println("반납 대상 도서가 아닙니다!\n해당 도서 상태:" + initState + "\n");
+            System.out.println("반납 대상 도서가 아닙니다!\n해당 도서 상태:" + initState.getKoreanState() + "\n");
     }
 
-    private static void lost(){
+    private void lost(){
+        System.out.print("분실 처리할 도서 번호를 입력해 주세요\n> ");
         int number = Integer.parseInt(sc.nextLine().trim());
         State initState = bookManager.lost(number);
-        if (initState != State.LOST && initState != State.DELETED)
+        if (initState != State.LOST)
             System.out.println("분실 처리되었습니다.\n");
         else
-            System.out.println("분실 대상 도서가 아닙니다! 이미 분실되었거나 삭제된 도서입니다.\n해당 도서 상태:" + initState + "\n");
+            System.out.println("이미 분실된 도서입니다.\n");
     }
 
-    private static void delete(){
+    private void delete(){
+        System.out.print("삭제할 도서 번호를 입력해 주세요\n> ");
         int number = Integer.parseInt(sc.nextLine().trim());
-        State initState = bookManager.delete(number);
-        if (initState == State.DELETED)
-            System.out.println("정상 삭제되었습니다.\n");
-        else
-            System.out.println("반납 대상 도서가 아닙니다!\n해당 도서 상태:" + initState + "\n");
+        bookManager.delete(number);
+
+        System.out.println("정상 삭제되었습니다.\n");
     }
 }
