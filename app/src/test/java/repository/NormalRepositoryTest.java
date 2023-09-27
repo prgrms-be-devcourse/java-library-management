c08502e471816d4bd69c5910d872b105771b9f6e
suzzingv <84590065+suzzingv@users.noreply.github.com>
2023-09-27 13:34:27 +0900

    NormalRepository 출력 테스트

--- app/src/test/java/repository/NormalRepositoryTest.java
+++ app/src/test/java/repository/NormalRepositoryTest.java
@@ -1,21 +1,34 @@
 package repository;
 
 import domain.Book;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class NormalRepositoryTest {
 
     Repository repository = new NormalRepository();
-
+    private static ByteArrayOutputStream outputMessage;
     NormalRepositoryTest() throws IOException {
+    }
+
+    @BeforeEach
+    void setUp() {
+        outputMessage = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputMessage));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.setOut(System.out);
     }
 
     @Test
@@ -48,8 +61,35 @@ class NormalRepositoryTest {
     }
 
     @Test
-    void printList() {
+    void printList() throws IOException {
+        File file = new File("C:/데브코스/java-library-management/app/src/main/resources/도서.csv");
+        List<Book> books = new ArrayList<>();
+        fileToList(books, file);
 
+        repository.printList();
+        StringBuilder tmp = new StringBuilder();
+        for(Book book : books) {
+            tmp.append("\n도서번호 : ").append(String.valueOf(book.getId())).append("\n제목 : ").append(book.getTitle()).append("\n작가 이름 : ").append(book.getWriter()).append("\n페이지 수: ").append(String.valueOf(book.getPage())).append("페이지").append("\n상태 : ").append(book.getState()).append("\n\n------------------------------\r\n");
+        }
+        Assertions.assertEquals(tmp.toString(), outputMessage.toString());
+    }
+
+    private void fileToList(List<Book> books, File file) throws IOException {
+        BufferedReader bf = new BufferedReader(new FileReader(file));
+        String line = "";
+
+        while((line = bf.readLine()) != null) {
+            String[] split = line.split(",");
+            Book tmpBook = new Book();
+
+            tmpBook.setId(Integer.parseInt(split[0]));
+            tmpBook.setTitle(split[1]);
+            tmpBook.setWriter(split[2]);
+            tmpBook.setPage(Integer.parseInt(split[3]));
+            tmpBook.setState(split[4]);
+
+            books.add(tmpBook);
+        }
     }
 
     @Test
