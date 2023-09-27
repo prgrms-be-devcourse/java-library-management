4533bd0747d1299ca4ad7fac8026dd8f5cacd32d
kkangh00n <kig2454@gmail.com>
2023-09-28 01:32:38 +0900

    feat: 일반모드 저장소 초기화 - 기존 파일에 데이터 읽어서 List 저장

--- src/repository/FileRepository.java
+++ src/repository/FileRepository.java
@@ -1,2 +1,59 @@
-package repository;public class FileRepository {
+package repository;
+
+import domain.Book;
+import domain.Status;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileRepository implements Repository{
+
+    private final String path = System.getProperty("user.dir") + "/src/resources/repository.csv";
+    private final List<Book> bookList = new ArrayList<>();
+
+    public FileRepository(){
+        File repository = new File(path);
+        loadFile();
+    }
+
+    private void loadFile(){
+        try{
+            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(path), StandardCharsets.UTF_8));
+            String data;
+            while((data=br.readLine())!=null){
+                String[] splitData = data.split(",");
+                Book book = new Book(Long.parseLong(splitData[0]), splitData[1], splitData[2], Integer.parseInt(splitData[3]), Status.valueOf(splitData[4]));
+                bookList.add(book);
+            }
+        }catch(IOException e){
+            System.out.println();
+        }
+    }
+
+    @Override
+    public void addBook(Book book) {
+
+    }
+
+    @Override
+    public List<Book> getAll() {
+        return null;
+    }
+
+    @Override
+    public List<Book> searchBook(String name) {
+        return null;
+    }
+
+    @Override
+    public Book getBook(Long bookNumber) {
+        return null;
+    }
+
+    @Override
+    public void deleteBook(Long bookNumber) {
+
+    }
 }
