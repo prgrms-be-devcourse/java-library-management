e00a305cc2e99ad43ca323951ffb69c9327dba78
ZZAMBAs <rksidksrksi@naver.com>
2023-09-28 15:25:13 +0900

    Feat: 테스트 모드를 위해 아무 행동도 하지 않는 TestFileManager를 추가한다.

---
+++ manage/TestFileManager.java
@@ -0,0 +1,18 @@
+package manage;
+
+import entity.Book;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestFileManager implements FileManager {
+    @Override
+    public List<Book> read() {
+        return new ArrayList<>();
+    }
+
+    @Override
+    public void write(List<Book> bookList) {
+        return;
+    }
+}
