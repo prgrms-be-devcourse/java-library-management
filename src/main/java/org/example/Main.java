6f4da3e93a6940a856d2128a76eb8acf91e56349
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-09-29 05:02:31 +0900

    refactor: Main 리팩토링

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,16 @@
 package org.example;
 
 import org.example.client.Client;
+import org.example.packet.Request;
 import org.example.server.Server;
 
 public class Main {
     public static void main(String[] args) {
         Server.requestMode(Client.scanMode()); // 입력값 클라이언트에서 확인, 서버는 Data 관련 예외 던진다.
         while (true) {
-            Client.printResponse(Server.requestMethod(Client.scanMenu())); // 입력값 클라이언트에서 확인, 서버는 Data 관련 예외 던진다.
+            Request request = Client.scanMenu();
+            String response = Server.requestMethod(request); // Response Class도 생각중
+            Client.printResponse(response); // 입력값 클라이언트에서 확인, 서버는 Data 관련 예외 던진다.
         }
     }
 }
