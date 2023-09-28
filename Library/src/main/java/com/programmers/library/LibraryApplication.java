e784a1b4caaa19454d54094ae193989539d823d7
park0jae <okvv26@naver.com>
2023-09-28 18:33:57 +0900

    feat : InitialConfig 구현 , 초기 설정 담당 클래스

--- Library/src/main/java/com/programmers/library/LibraryApplication.java
+++ Library/src/main/java/com/programmers/library/LibraryApplication.java
@@ -1,7 +1,11 @@
 package com.programmers.library;
 
+import com.programmers.library.business.LibraryExecutor;
+import com.programmers.library.config.InitialConfig;
+
 public class LibraryApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        LibraryExecutor libraryExecutor = InitialConfig.configLibrary();
+        libraryExecutor.run();
     }
 }
