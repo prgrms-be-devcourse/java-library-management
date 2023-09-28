314b2e95ec936f785b9b9111bbb94d8cb1c34cd3
voidmelody <sss9073@naver.com>
2023-09-28 23:42:17 +0900

    Refactor: "State 열거형 value 수정"

--- src/main/java/org/library/entity/State.java
+++ src/main/java/org/library/entity/State.java
@@ -1,10 +1,10 @@
 package org.library.entity;
 
 public enum State {
-    AVAILABLE("대여 가능합니다."),
-    RENT("이미 대여중인 도서입니다."),
-    ORGANIZING("도서 정리중입니다."),
-    LOST("분실된 도서입니다.");
+    AVAILABLE("대여 가능"),
+    RENT("대여중"),
+    ORGANIZING("도서 정리중"),
+    LOST("분실됨");
 
     private final String description;
 
