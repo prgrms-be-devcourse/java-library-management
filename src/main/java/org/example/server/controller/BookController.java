6b7142c1b7a5e6beeaf677f117e171d6bef41881
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-09-28 23:27:52 +0900

    style: 출력문 줄바꿈 스타일 변경

--- src/main/java/org/example/server/controller/BookController.java
+++ src/main/java/org/example/server/controller/BookController.java
@@ -14,7 +14,7 @@ public class BookController {
 
     public String registerBook(RequestBookDto requestBookDto) {
         bookService.register(requestBookDto);
-        return "[System] 도서 등록이 완료되었습니다.\n";
+        return "\n[System] 도서 등록이 완료되었습니다.\n";
     }
 
     public String readAllBook() {
@@ -27,21 +27,21 @@ public class BookController {
 
     public String borrow(int bookId) {
         bookService.borrow(bookId);
-        return "[System] 도서가 대여 처리 되었습니다.\n";
+        return "\n[System] 도서가 대여 처리 되었습니다.\n";
     }
 
     public String returnBook(int bookId) {
         bookService.returnBook(bookId);
-        return "[System] 도서가 반납 처리 되었습니다.\n";
+        return "\n[System] 도서가 반납 처리 되었습니다.\n";
     }
 
     public String lost(int bookId) {
         bookService.lost(bookId);
-        return "[System] 도서가 분실 처리 되었습니다.\n";
+        return "\n[System] 도서가 분실 처리 되었습니다.\n";
     }
 
     public String delete(int bookId) {
         bookService.delete(bookId);
-        return "[System] 도서가 삭제 처리 되었습니다.\n";
+        return "\n[System] 도서가 삭제 처리 되었습니다.\n";
     }
 }
