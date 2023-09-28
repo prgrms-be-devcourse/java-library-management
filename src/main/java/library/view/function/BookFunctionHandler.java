2196c83975642dc999f0eec5cfd3b99af2a32240
hyoguoo <hyoguoo@gmail.com>
2023-09-28 21:39:17 +0900

    feat: BookFunctionHandler 도서 추가를 처리하는 메서드 구현

--- src/main/java/library/view/function/BookFunctionHandler.java
+++ src/main/java/library/view/function/BookFunctionHandler.java
@@ -1,6 +1,7 @@
 package library.view.function;
 
 import library.controller.BookController;
+import library.dto.BookSaveRequest;
 import library.repository.BookRepository;
 import library.service.BookService;
 import library.view.console.ConsoleIOHandler;
@@ -13,6 +14,11 @@ public class BookFunctionHandler {
     public BookFunctionHandler(BookRepository bookRepository, ConsoleIOHandler consoleIOHandler) {
         this.bookController = new BookController(new BookService(bookRepository));
         this.consoleIOHandler = consoleIOHandler;
+    }
+
+    public void addBook() {
+        BookSaveRequest bookSaveRequest = consoleIOHandler.inputBookInfo();
+        bookController.addBook(bookSaveRequest);
     }
 }
 
