79d480ccd09ad460ec5c9d1c6735c96db1beb700
hyoguoo <hyoguoo@gmail.com>
2023-09-28 22:01:22 +0900

    feat: 도서 기능 관련 enum 클래스 구현
    - 요구 사항에 명시된 기능 7가지
    - 실행 코드 / 설명 / 시작 메시지 / 종료 메시지 / 실행 될 기능 포함

---
+++ src/main/java/library/view/function/BookFunction.java
@@ -0,0 +1,31 @@
+package library.view.function;
+
+import java.util.function.Consumer;
+
+public enum BookFunction {
+    ADD_BOOK("1", "도서 등록", "도서 등록 메뉴로 넘어갑니다.", "도서 등록이 완료되었습니다.", BookFunctionHandler::addBook),
+    LIST_ALL_BOOKS("2", "전체 도서 목록 조회", "전체 도서 목록입니다.", "도서 목록 끝", BookFunctionHandler::searchAll),
+    SEARCH_BY_TITLE("3", "제목으로 도서 검색", "제목으로 도서 검색 메뉴로 넘어갑니다.", "검색된 도서 끝", BookFunctionHandler::searchByTitle),
+    RENT_BOOK("4", "도서 대여", "도서 대여 메뉴로 넘어갑니다.", "도서가 대여 처리 되었습니다.", BookFunctionHandler::rentBook),
+    RETURN_BOOK("5", "도서 반납", "도서 반납 메뉴로 넘어갑니다.", "도서가 반납 처리 되었습니다.", BookFunctionHandler::returnBook),
+    LOST_BOOK("6", "도서 분실", "도서 분실 처리 메뉴로 넘어갑니다.", "도서가 분실 처리 되었습니다.", BookFunctionHandler::lostBook),
+    DELETE_BOOK("7", "도서 삭제", "도서 삭제 처리 메뉴로 넘어갑니다.", "도서가 삭제 처리 되었습니다.", BookFunctionHandler::deleteBook);
+
+    private final String code;
+    private final String description;
+    private final String menuEntranceMessage;
+    private final String menuSuccessMessage;
+    private final Consumer<BookFunctionHandler> bookConsumer;
+
+    BookFunction(String code,
+                 String description,
+                 String menuEntranceMessage,
+                 String menuSuccessMessage,
+                 Consumer<BookFunctionHandler> bookConsumer) {
+        this.code = code;
+        this.description = description;
+        this.menuEntranceMessage = menuEntranceMessage;
+        this.menuSuccessMessage = menuSuccessMessage;
+        this.bookConsumer = bookConsumer;
+    }
+}
