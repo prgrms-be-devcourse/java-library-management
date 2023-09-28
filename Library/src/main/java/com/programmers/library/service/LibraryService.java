90ba2ec57f88f66830fbef492ee403d878471126
park0jae <okvv26@naver.com>
2023-09-28 18:13:42 +0900

    feat : LibraryService 구현

---
+++ Library/src/main/java/com/programmers/library/service/LibraryService.java
@@ -0,0 +1,60 @@
+package com.programmers.library.service;
+
+import com.programmers.library.domain.Book;
+import com.programmers.library.domain.BookStatus;
+import com.programmers.library.repository.Repository;
+
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import static com.programmers.library.domain.BookStatus.RENTABLE;
+
+public class LibraryService {
+    private final Repository repository;
+    private static final int ORGANIZING_TIME = 5 * 60 * 1000;
+
+    public LibraryService(Repository repository) {
+        this.repository = repository;
+    }
+
+    public void registerBook(String title, String author, Integer page) {
+        Long lastId = repository.findLastId() + 1;
+
+        Book book = new Book(lastId, title, author, page);
+        repository.register(book);
+    }
+
+    public void updateStatus(Book book, BookStatus bookStatus){
+        repository.updateStatus(book, bookStatus);
+    }
+
+    public List<Book> findAllBooks() {
+        return repository.findAllBooks();
+    }
+
+    public List<Book> findBooksByTitle(String title) {
+        return repository.findBooksByTitle(title);
+    }
+
+    public Book findBookById(Long id) {
+        return repository.findBookById(id).get();
+    }
+
+    public void deleteBook(Long id) {
+        repository.findBookById(id).get();
+        repository.deleteBook(id);
+    }
+
+    public void completeOrganizing(Book book) {
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                // 5분 후에 실행되는 코드
+                repository.updateStatus(book, RENTABLE);
+            }
+        }, ORGANIZING_TIME); // 5분(밀리초 단위)
+    }
+
+}
