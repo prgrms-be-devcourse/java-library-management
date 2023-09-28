0c8365fd769231a7874fd2b41ace786a19140c19
hyoguoo <hyoguoo@gmail.com>
2023-09-28 20:58:52 +0900

    feat: ConsoleIOHandler를 통해 입출력하도록 의존성 주입

--- src/main/java/library/view/ConsoleEngine.java
+++ src/main/java/library/view/ConsoleEngine.java
@@ -1,8 +1,15 @@
 package library.view;
 
+import library.view.console.ConsoleIOHandler;
+
 public class ConsoleEngine implements Runnable {
 
+    private final ConsoleIOHandler consoleIOHandler;
     boolean isRunning = true;
+
+    public ConsoleEngine(ConsoleIOHandler consoleIOHandler) {
+        this.consoleIOHandler = consoleIOHandler;
+    }
 
     @Override
     public void run() {
