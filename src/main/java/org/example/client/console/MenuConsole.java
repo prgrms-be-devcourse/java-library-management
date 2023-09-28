f6c4a03b4996c73e4a4b59e7bd86ccf10e52f5b8
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-09-28 21:54:46 +0900

    style: 각 메뉴별 질문 문구 enum에 저장

--- src/main/java/org/example/client/console/MenuConsole.java
+++ src/main/java/org/example/client/console/MenuConsole.java
@@ -1,26 +1,14 @@
 package org.example.client.console;
 
 import org.example.client.connect.Request;
-import org.example.client.connect.RequestData;
 import org.example.type.MenuType;
 import org.example.server.entity.RequestBookDto;
-
-import java.util.Arrays;
-import java.util.LinkedList;
 
 // 메뉴를 입력 받아서 enum과 매핑해주는 역할
 // 메뉴를 입력받고 Controller로부터 받은 결과를 출력
 // "메뉴"만 아는 입출력 클래스
 public class MenuConsole implements Console {
     public static MenuType menuType;
-    public static LinkedList<String> scanBookInfoQuestions = new LinkedList<>(Arrays.asList(
-            ("Q. 등록할 도서 제목을 입력하세요.\n\n> "),
-            ("Q. 작가 이름을 입력하세요.\n\n> "),
-            ("Q. 페이지 수를 입력하세요.\n\n> ")
-    ));
-    public static String scanBookNameQuestions = "Q. 검색할 도서 제목 일부를 입력하세요.\n\n> ";
-
-    public String getScanBookIdQuestions = "Q. 대여할 도서번호를 입력하세요\n\n> ";
 
     @Override
     public void show() {
@@ -49,17 +37,17 @@ public class MenuConsole implements Console {
     }
 
     public int scanBookId() {
-        System.out.print(getScanBookIdQuestions);
+        System.out.print(menuType.getMenuQuestion().get(0));
         return Integer.parseInt(scanner.nextLine());
     }
 
     public String scanBookName() {
-        System.out.print(scanBookNameQuestions);
+        System.out.print(menuType.getMenuQuestion().get(0));
         return scanner.nextLine();
     }
 
     public RequestBookDto scanBookInfo() {
-        String[] bookInfo = scanBookInfoQuestions.stream().map(q -> {
+        String[] bookInfo = menuType.getMenuQuestion().stream().map(q -> {
             System.out.print(q);
             return scanner.nextLine();
         }).toArray(String[]::new);
--- src/main/java/org/example/type/MenuType.java
+++ src/main/java/org/example/type/MenuType.java
@@ -1,19 +1,23 @@
 package org.example.type;
 
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public enum MenuType {
-    REGISTER(1, "1. 도서 등록\n", "\n[System] 도서 등록 메뉴로 넘어갑니다.\n\n"),
+    REGISTER(1, "1. 도서 등록\n", "\n[System] 도서 등록 메뉴로 넘어갑니다.\n\n", new LinkedList<>(Arrays.asList(
+            ("Q. 등록할 도서 제목을 입력하세요.\n\n> "),
+            ("Q. 작가 이름을 입력하세요.\n\n> "),
+            ("Q. 페이지 수를 입력하세요.\n\n> ")
+    ))),
 
-    READ_ALL(2, "2. 전체 도서 목록 조회\n", "\n[System] 전체 도서 목록입니다.\n\n"),
-
-    SEARCH_BY_NAME(3, "3.제목으로 도서 검색\n", "\n[System] 제목으로 도서 검색 메뉴로 넘어갑니다.\n\n"),
-
-    BORROW(4, "4. 도서 대여\n", "\n[System] 도서 대여 메뉴로 넘어갑니다.\n\n");
-//    RETURN(4, "4. 도서 반납\n"),
+    READ_ALL(2, "2. 전체 도서 목록 조회\n", "\n[System] 전체 도서 목록입니다.\n\n", new LinkedList<>()),
+    SEARCH_BY_NAME(3, "3.제목으로 도서 검색\n", "\n[System] 제목으로 도서 검색 메뉴로 넘어갑니다.\n\n", new LinkedList<>(Arrays.asList("Q. 검색할 도서 제목 일부를 입력하세요.\n\n> "))),
+    BORROW(4, "4. 도서 대여\n", "\n[System] 도서 대여 메뉴로 넘어갑니다.\n\n", new LinkedList<>(Arrays.asList("Q. 대여할 도서번호를 입력하세요\n\n> "))),
+    RETURN(5, "5. 도서 반납\n", "\n[System] 도서 반납 메뉴로 넘어갑니다.\n", new LinkedList<String>(Arrays.asList("Q. 반납할 도서번호를 입력하세요\n\n> ")));
 //    LOST(5, "5. 도서 분실\n"),
 //    RETURN(6, "6. 도서 삭제\n");
 
@@ -21,10 +25,13 @@ public enum MenuType {
     private String menuName;
     private String menuStartMent;
 
-    private MenuType(int menuNum, String menuName, String menuStartMent) {
+    private LinkedList<String> menuQuestion;
+
+    private MenuType(int menuNum, String menuName, String menuStartMent, LinkedList<String> menuQuestion) {
         this.menuNum = menuNum;
         this.menuName = menuName;
         this.menuStartMent = menuStartMent;
+        this.menuQuestion = menuQuestion;
     }
 
     public int getMenuNum() {
@@ -42,6 +49,10 @@ public enum MenuType {
     //    public static String getMenuNames() {
 //        return
 //    }
+    public LinkedList<String> getMenuQuestion() {
+        return menuQuestion;
+    }
+
     private static final Map<Integer, MenuType> BY_NUMBER =
             Stream.of(values()).collect(Collectors.toMap(MenuType::getMenuNum, Function.identity()));
 
