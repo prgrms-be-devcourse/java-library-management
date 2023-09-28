d67781dc62fc11f1a10b1c86d92781c4009453ba
uijin-j <uijin0531@gmail.com>
2023-09-28 16:25:48 +0900

    refactor: Mode.enum에 description 필드 추가, toString() 메서드 오버라이드

--- library-management/src/main/java/devcourse/backend/Mode.java
+++ library-management/src/main/java/devcourse/backend/Mode.java
@@ -9,15 +9,22 @@ import java.util.Arrays;
 import java.util.Optional;
 
 public enum Mode {
-    NORMAL(1, new FileRepository("src/main/resources/도서 목록.csv")),
-    TEST(2, new MemoryRepository());
+    NORMAL(1, "일반 모드", new FileRepository("src/main/resources/도서 목록.csv")),
+    TEST(2, "테스트 모드", new MemoryRepository());
 
     private final int num;
+    private final String description;
     private Repository repository;
 
-    Mode(int num, Repository repository) {
+    Mode(int num, String description, Repository repository) {
         this.num = num;
+        this.description = description;
         this.repository = repository;
+    }
+
+    @Override
+    public String toString() {
+        return num + ". " + description;
     }
 
     public static Repository getRepository(int num) {
