76a2d3583b55ec8a5e5b3c18ae4d7ac0a346b148
park0jae <okvv26@naver.com>
2023-09-28 18:38:20 +0900

    feat : 숫자 입력에 대한 검증을 진행하는 ValidateInput 구현 및 예외처리 추가

---
+++ Library/src/main/java/com/programmers/library/validation/ValidateInput.java
@@ -0,0 +1,21 @@
+package com.programmers.library.validation;
+
+import com.programmers.library.exception.ErrorCode;
+import com.programmers.library.exception.ExceptionHandler;
+
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class ValidateInput {
+
+    public static Long checkNumberValidate(Scanner scanner){
+        try{
+            return Long.valueOf(scanner.nextLine());
+        }catch (InputMismatchException e){
+            throw ExceptionHandler.err(ErrorCode.INVALID_INPUT_EXCEPTION);
+        }catch (NoSuchElementException e){
+            throw ExceptionHandler.err(ErrorCode.NO_INPUT_EXCEPTION);
+        }
+    }
+}
