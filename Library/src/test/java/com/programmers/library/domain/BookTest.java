ed442dc49bcf24c738448ec8ab2b5d1940a81b9d
park0jae <okvv26@naver.com>
2023-09-28 14:13:22 +0900

    test : Book Entity 테스트 코드 구현

---
+++ Library/src/test/java/com/programmers/library/domain/BookTest.java
@@ -0,0 +1,43 @@
+package com.programmers.library.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BookTest {
+
+    @Test
+    @DisplayName("책 엔티티 생성 테스트")
+    public void bookEntityTest(){
+        // Given
+        Long bookId = 1L;
+        String title = "제목1";
+        String author = "작가1";
+        Integer page = 100;
+
+        // When
+        Book book = new Book(bookId, title, author, page);
+
+        // Then
+        assertEquals(book.getTitle(), title);
+    }
+
+    @Test
+    @DisplayName("책 엔티티 상태 확인 테스트")
+    public void bookStatusTest(){
+        // Given
+        Long bookId = 1L;
+        String title = "제목1";
+        String author = "작가1";
+        Integer page = 100;
+        BookStatus status = BookStatus.LOST;
+
+        // When
+        Book book = new Book(bookId, title, author, page, status);
+
+        // Then
+        BookStatus bookStatus = book.getBookStatus();
+        assertEquals(bookStatus.getStatusDescription(), "분실");
+    }
+}
