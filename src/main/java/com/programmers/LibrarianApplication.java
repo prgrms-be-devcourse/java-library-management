be535184bc6d6867bbceda482311e63ef6bada8e
Jay <injaesong0@gmail.com>
2023-09-29 02:00:06 +0900

    feat: add app.run feature

--- src/main/java/com/programmers/LibrarianApplication.java
+++ src/main/java/com/programmers/LibrarianApplication.java
@@ -1,7 +1,14 @@
 package com.programmers;
 
+import com.programmers.app.App;
+import com.programmers.app.instances.GeneralInstances;
+
 public class LibrarianApplication {
     public static void main(String[] args) {
 
+        GeneralInstances generalInstances = new GeneralInstances();
+        App app = new App(generalInstances);
+
+        app.run();
     }
 }
--- src/main/java/com/programmers/app/App.java
+++ src/main/java/com/programmers/app/App.java
@@ -1,6 +1,27 @@
 package com.programmers.app;
 
-public interface App {
+import com.programmers.app.instances.GeneralInstances;
+import com.programmers.app.instances.SelectiveInstances;
+import com.programmers.app.menu.Menu;
+import com.programmers.app.mode.Mode;
 
-    void run();
+public class App {
+
+    private final GeneralInstances generalInstances;
+    private final Mode mode;
+    private final SelectiveInstances selectiveInstances;
+
+    public App(GeneralInstances generalInstances) {
+        this.generalInstances = generalInstances;
+        mode = generalInstances.getModeSelector().select();
+        selectiveInstances = new SelectiveInstances(generalInstances, mode);
+    }
+
+    public void run() {
+        Menu menu;
+        while (true) {
+            menu = selectiveInstances.getMenuSelector().select();
+            selectiveInstances.getMenuExecuter().execute(menu);
+        }
+    }
 }
