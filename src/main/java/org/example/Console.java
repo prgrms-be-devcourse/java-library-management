33e4dca0962040e5b4c2982a965658b18960974f
eugene <81062608+eugene225@users.noreply.github.com>
2023-09-29 20:39:19 +0900

    [FIX] 소스파일 이동 및 정리

---
+++ src/main/java/org/example/Console.java
@@ -0,0 +1,194 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public enum Console {
+
+    START {
+        @Override
+        public void consolePrint() {
+            System.out.println("0. 사용할 기능을 선택해 주세요.");
+            System.out.println("1. 도서 등록");
+            System.out.println("2. 전체 도서 목록 조회");
+            System.out.println("3. 제목으로 도서 검색");
+            System.out.println("4. 도서 대여");
+            System.out.println("5. 도서 반납");
+            System.out.println("6. 도서 분실");
+            System.out.println("7. 도서 삭제");
+
+            System.out.print("\n> ");
+        }
+
+        @Override
+        public String[] getConsolePrint() { return null; }
+    },
+    CREATE_BOOK {
+        @Override
+        public void consolePrint() throws IOException {
+        }
+
+        @Override
+        String[] getConsolePrint() throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+            String[] ar = new String[3];
+            System.out.println("[System] 도서 등록 메뉴로 넘어갑니다.");
+            System.out.println("Q. 등록할 도서 제목을 입력하세요.\n");
+            System.out.print("> ");
+            String title = br.readLine();
+
+            System.out.println("Q. 작가 이름을 입력하세요.\n");
+            System.out.print("> ");
+            String author = br.readLine();
+            System.out.println();
+
+            System.out.println("Q. 페이지 수를 입력하세요.\n");
+            System.out.print("> ");
+            int pageNum = Integer.parseInt(br.readLine());
+            System.out.println();
+
+            ar[0] = title; ar[1] = author; ar[2] = String.valueOf(pageNum);
+
+            return ar;
+        }
+    },
+    GET_ALL_BOOKS {
+        void consolePrint() throws IOException {
+            System.out.println("[System] 전체 도서 목록입니다.\n");
+        }
+
+        @Override
+        String[] getConsolePrint() throws IOException {
+            return new String[0];
+        }
+
+    },
+    GET_BY_TITLE {
+        @Override
+        void consolePrint() throws IOException {
+
+        }
+
+        @Override
+        String[] getConsolePrint() throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String[] word = new String[3];
+
+            System.out.println("[System] 제목으로 도서 검색 메뉴로 넘어갑니다.");
+            System.out.println("Q. 검색할 도서 제목 일부를 입력하세요.\n");
+            System.out.print("> ");
+            word[0] = br.readLine();
+            System.out.println();
+
+            return word;
+        }
+    },
+    RENT_BOOK {
+        @Override
+        void consolePrint() throws IOException {
+
+        }
+
+        @Override
+        String[] getConsolePrint() throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String[] word = new String[3];
+
+            System.out.println("[System] 도서 대여 메뉴로 넘어갑니다.");
+            System.out.println("Q. 대여할 도서번호를 입력하세요\n");
+            System.out.print("> ");
+            word[0] = br.readLine();
+            System.out.println();
+
+            return word;
+        }
+    },
+    RETURN_BOOK {
+        @Override
+        void consolePrint() throws IOException {
+
+        }
+
+        @Override
+        String[] getConsolePrint() throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String[] word = new String[3];
+
+            System.out.println("[System] 도서 반납 메뉴로 넘어갑니다.");
+            System.out.println("Q. 반납할 도서번호를 입력하세요\n");
+            System.out.print("> ");
+            word[0] = br.readLine();
+            System.out.println();
+
+            return word;
+        }
+    },
+    LOST_BOOK {
+        @Override
+        void consolePrint() throws IOException {
+
+        }
+
+        @Override
+        String[] getConsolePrint() throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String[] word = new String[3];
+
+            System.out.println("[System] 도서 분실 처리 메뉴로 넘어갑니다.");
+            System.out.println("Q. 분실 처리할 도서번호를 입력하세요\n");
+            System.out.print("> ");
+            word[0] = br.readLine();
+            System.out.println();
+
+            return word;
+        }
+    },
+    DELETE_BOOK {
+        @Override
+        void consolePrint() throws IOException {
+
+        }
+
+        @Override
+        String[] getConsolePrint() throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String[] word = new String[3];
+
+            System.out.println("[System] 도서 삭제 처리 메뉴로 넘어갑니다.");
+            System.out.println("Q. 삭제할 도서번호를 입력하세요\n");
+            System.out.print("> ");
+            word[0] = br.readLine();
+            System.out.println();
+
+            return word;
+        }
+    };
+
+    public static Console getByNumber(int number) {
+        switch (number) {
+            case 1:
+                return CREATE_BOOK;
+            case 2:
+                return GET_ALL_BOOKS;
+            case 3:
+                return GET_BY_TITLE;
+            case 4:
+                return RENT_BOOK;
+            case 5:
+                return RETURN_BOOK;
+            case 6:
+                return LOST_BOOK;
+            case 7:
+                return DELETE_BOOK;
+            default:
+                throw new IllegalArgumentException("1부터 7까지의 숫자를 입력하세요.");
+        }
+    }
+
+
+    abstract void consolePrint() throws IOException;
+    abstract String[] getConsolePrint() throws IOException;
+
+}
---
+++ src/main/java/org/example/Mode.java
@@ -0,0 +1,91 @@
+package org.example;
+
+import org.example.domain.Book;
+import org.example.service.BookService;
+import org.example.service.FileService;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+
+import static org.example.Console.*;
+
+public class Mode {
+
+    private static final BookService bookService = new BookService();
+    private static final FileService fileService;
+
+    static {
+        try {
+            fileService = new FileService();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void normalMode(int i) throws IOException {
+        if(i==1) System.out.println("[System] 일반 모드로 애플리케이션을 실행합니다.\n");
+        else System.out.println("[System] 테스트 모드로 애플리케이션을 실행합니다.\n");
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 파일에 있는 책 목록 불러오기
+        if(i==1) bookService.updateBooks(fileService.readFile());
+
+        while(true) {
+            Console.START.consolePrint();
+
+            int fun, bookId;
+            String[] id;
+            List<Book> bookList;
+            fun = Integer.parseInt(br.readLine());
+            Console console = Console.getByNumber(fun);
+            switch (console) {
+                case CREATE_BOOK:
+                    String[] ar = CREATE_BOOK.getConsolePrint();
+                    bookService.createBook(ar[0], ar[1], Integer.parseInt(ar[2]));
+                    break;
+                case GET_ALL_BOOKS:
+                    GET_ALL_BOOKS.consolePrint();
+                    bookList = bookService.getAllBooks();
+                    bookService.printAllBooks(bookList);
+                    break;
+                case GET_BY_TITLE:
+                    id = GET_BY_TITLE.getConsolePrint();
+                    bookList = bookService.findByTitle(id[0]);
+                    bookService.printAllBooks(bookList);
+                    break;
+                case RENT_BOOK:
+                    id = RENT_BOOK.getConsolePrint();
+                    bookService.rentBook(Integer.parseInt(id[0]));
+                    break;
+                case RETURN_BOOK:
+                    id = RETURN_BOOK.getConsolePrint();
+                    bookService.returnBook(Integer.parseInt(id[0]));
+                    break;
+                case LOST_BOOK:
+                    id = LOST_BOOK.getConsolePrint();
+                    bookService.lostBook(Integer.parseInt(id[0]));
+                    break;
+                case DELETE_BOOK:
+                    id = DELETE_BOOK.getConsolePrint();
+                    bookService.deleteBook(Integer.parseInt(id[0]));
+                    break;
+                default:
+                    if(i==1) {
+                        fileService.deleteFile();
+
+                        List<Book> books = bookService.getAllBooks();
+                        fileService.writeFiles(books);
+                    }
+
+                    System.out.println("[System] 시스템이 종료됩니다.");
+                    break;
+            }
+
+            if(fun < 1 || fun > 7) break;
+        }
+    }
+}
