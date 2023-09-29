6582d1edcf9ab017ee23e8a8c0370471735e0fae
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-09-29 12:40:37 +0900

    style: Main 클래스 Application으로 이름 변경

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -4,7 +4,7 @@ import org.example.client.Client;
 import org.example.packet.Request;
 import org.example.server.Server;
 
-public class Main {
+public class Application {
     public static void main(String[] args) {
         Server.requestMode(Client.scanMode()); // 입력값 클라이언트에서 확인, 서버는 Data 관련 예외 던진다.
         while (true) {
