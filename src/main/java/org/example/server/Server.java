aa9afb76f259c3d3e8e1cbf304a8d0fe4f2b7293
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-09-29 14:13:15 +0900

    refactor: 서버 모드 enum Server로 이동

--- src/main/java/org/example/server/Server.java
+++ src/main/java/org/example/server/Server.java
@@ -1,12 +1,32 @@
 package org.example.server;
 
 import org.example.packet.Request;
+import org.example.server.repository.BookRepository;
+import org.example.server.repository.FileBookRepository;
+import org.example.server.repository.TestBookRepository;
+
+import java.util.function.Supplier;
 
 // 싱글톤으로 생성
 // 콘솔에서 입력 받은 모드, 메뉴, 각 메뉴에 대한 입력 값들을 세팅.
 // 모드 = 레포 세팅
 // 메뉴 = 컨트롤러 뭐할지 세팅
 public class Server {
+    private enum ServerMode {
+        COMMON(FileBookRepository::new),
+        TEST(TestBookRepository::new);
+
+        private final Supplier<BookRepository> bookRepositorySupplier;
+
+        ServerMode(Supplier<BookRepository> bookRepositorySupplier) {
+            this.bookRepositorySupplier = bookRepositorySupplier;
+        }
+
+        public BookRepository getRepository() {
+            return bookRepositorySupplier.get();
+        }
+    }
+
     private Server() {
     }
 
