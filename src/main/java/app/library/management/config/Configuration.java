63177f20d239691e08d8d3c8bf5f61f98b4a1f0b
이수진 <isujin@isujin-ui-MacBookAir.local>
2023-09-30 02:28:28 +0900

    [feat]: Configuration 추가

---
+++ src/main/java/app/library/management/config/Configuration.java
@@ -0,0 +1,59 @@
+package app.library.management.config;
+
+import app.library.management.infra.port.Request;
+import app.library.management.infra.console.Input;
+import app.library.management.infra.console.Output;
+import app.library.management.core.controller.BookController;
+import app.library.management.core.domain.util.FileStatusManager;
+import app.library.management.infra.port.Response;
+import app.library.management.core.repository.file.FileStorage;
+import app.library.management.core.repository.BookRepository;
+import app.library.management.core.repository.file.FileStorageAdaptor;
+import app.library.management.core.repository.memory.BookMemoryRepository;
+import app.library.management.core.service.BookService;
+import app.library.management.infra.mode.ExecutionMode;
+
+public class Configuration {
+
+    private final ExecutionMode executionMode;
+
+    public Configuration(ExecutionMode executionMode) {
+        this.executionMode = executionMode;
+    }
+
+    private BookRepository bookRepository() {
+        BookRepository bookRepository = null;
+        switch (executionMode) {
+            case GENERAL:
+                bookRepository = new FileStorageAdaptor(fileStorage());
+                break;
+            case TEST:
+                bookRepository = new BookMemoryRepository(fileStorage());
+                break;
+        }
+        return bookRepository;
+    }
+
+    private BookService bookService() {
+        return new BookService(bookRepository(), statusManager());
+    }
+
+    public BookController bookController() {
+        return new BookController(bookService(), request(), response());
+    }
+
+    private FileStatusManager statusManager() {
+        return new FileStatusManager(bookRepository());
+    }
+
+    private FileStorage fileStorage() {
+        return new FileStorage();
+    }
+    private Request request() {
+        return new Input();
+    }
+
+    private Response response() {
+        return new Output();
+    }
+}
