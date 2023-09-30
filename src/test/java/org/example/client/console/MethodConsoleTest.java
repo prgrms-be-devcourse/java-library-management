8664c5af0e64df6270533b796b26f7d473cb86b6
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-09-30 19:17:31 +0900

    test: 테스트할 클래스 추가

---
+++ src/test/java/org/example/client/console/MethodConsoleTest.java
@@ -0,0 +1,28 @@
+package org.example.client.console;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MethodConsoleTest {
+
+    @Test
+    void scanTypeAndInfo() {
+    }
+
+    @Test
+    void setClientMethod() {
+    }
+
+    @Test
+    void scanAndSetBookInfo() {
+    }
+
+    @Test
+    void scanAndSetBookName() {
+    }
+
+    @Test
+    void scanAndSetBookId() {
+    }
+}
---
+++ src/test/java/org/example/server/service/BookServiceTest.java
@@ -0,0 +1,36 @@
+package org.example.server.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookServiceTest {
+
+    @Test
+    void register() {
+    }
+
+    @Test
+    void readAll() {
+    }
+
+    @Test
+    void searchByName() {
+    }
+
+    @Test
+    void borrow() {
+    }
+
+    @Test
+    void restore() {
+    }
+
+    @Test
+    void lost() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
