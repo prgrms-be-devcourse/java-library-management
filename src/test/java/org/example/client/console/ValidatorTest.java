0bceb4e28039fffeef75a4746aaaab409a86c948
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-09-30 22:15:54 +0900

    test: 콘솔 테스트보다는 유효성 테스트를 하는 것이 적합할 것 같아 테스트 파일 변경

---
+++ src/test/java/org/example/client/console/ValidatorTest.java
@@ -0,0 +1,21 @@
+package org.example.client.console;
+
+import org.junit.jupiter.api.Test;
+
+class ValidatorTest {
+    @Test
+    void validateBook() {
+    }
+
+    @Test
+    void validateNameAndAuthor() {
+    }
+
+    @Test
+    void validateIdAndPages() {
+    }
+
+    @Test
+    void validateSelectNum() {
+    }
+}
