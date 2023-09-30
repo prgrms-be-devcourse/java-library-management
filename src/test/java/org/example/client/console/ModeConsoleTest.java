7ef2b2a5ae23f34a58f3cc1b50162bca526caa04
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-09-30 21:53:27 +0900

    test: 모드 입력 시 발생하는 예외 테스트

--- src/test/java/org/example/client/console/ModeConsoleTest.java
+++ src/test/java/org/example/client/console/ModeConsoleTest.java
@@ -1,12 +1,24 @@
 package org.example.client.console;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class ModeConsoleTest {
 
     @Test
-    void scanType() {
+    @DisplayName("모드 설정 입력에서 숫자 외 입력값 예외처리")
+    void checkNumberValidationExcaption() {
+        Assertions.assertThrows(ValidateException.class, () -> {
+            Validator.validateSelectNum(ModeConsole.ModeType.values().length, "%%");
+        });
+    }
+
+    @Test
+    @DisplayName("모드 설정 입력에서 모드 번호 외 입력값 예외처리")
+    void checkModeNumberValidationExcaption() {
+        Assertions.assertThrows(ValidateException.class, () -> {
+            Validator.validateSelectNum(ModeConsole.ModeType.values().length, "100");
+        });
     }
 }
