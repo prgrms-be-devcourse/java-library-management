27bbaeb7fbfa0616de1eacd7bf76dff1b9c26451
EomYerim <djadpfa1002@naver.com>
2023-10-01 02:28:07 +0900

    feat: 도서 repository 인터페이스 생성

---
+++ src/main/java/com/programmers/library/repository/LibraryRepository.java
@@ -0,0 +1,14 @@
+package com.programmers.library.repository;
+
+import com.programmers.library.domain.Book;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface LibraryRepository {
+    Book save(Book book);   // 도서 등록
+    List<Book> findAll();   // 전체 도서 목록 조회
+    List<Book> findByTitle(String title);   // 제목으로 도서 검색
+    Optional<Book> findById(int bookId);    // 도서 번호로 도서 검색
+    void delete(int bookId);    // 도서 삭제
+}
