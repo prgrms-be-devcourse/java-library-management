9d77439804ed532b707a6fe97840b3c6362aa14e
EomYerim <djadpfa1002@naver.com>
2023-10-01 02:26:34 +0900

    feat: 도서 domain 생성

---
+++ src/main/java/com/programmers/library/domain/Book.java
@@ -0,0 +1,75 @@
+package com.programmers.library.domain;
+
+import com.programmers.library.utils.BookStatus;
+
+public class Book {
+    private int bookId;
+    private String title;
+    private String author;
+    private int pages;
+    private BookStatus status;
+
+    public Book(String title, String author, int pages) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.status = BookStatus.AVAILABLE; // 기본 도서 상태 - AVAILABLE(대여 가능)
+    }
+
+    public Book(int bookId, String title, String author, int pages, BookStatus status) {
+        this.bookId = bookId;
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "도서번호 : " + bookId +
+                "\n제목 : " + title +
+                "\n작가 이름 : " + author +
+                "\n페이지 수 : " + pages +
+                "\n상태 : " + status.getDescription();
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public BookStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(BookStatus status) {
+        this.status = status;
+    }
+}
