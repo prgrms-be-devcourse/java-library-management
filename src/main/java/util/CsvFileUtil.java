3369eeb84786a6471f2f8a5cf868673d1f7bc8c0
byulcode <bybit0408@gmail.com>
2023-09-30 20:38:40 +0900

    refactor : 메서드 분리

--- src/main/java/util/CsvFileUtil.java
+++ src/main/java/util/CsvFileUtil.java
@@ -16,38 +16,36 @@ public class CsvFileUtil {
         this.FILE_PATH = FILE_PATH;
     }
 
-    private void createFile() throws IOException {
-        File file = new File(FILE_PATH);
-        if (!file.exists()) {
-            file.createNewFile();
-        }
-    }
-
     public Map<Long, Book> readAllBooksFromCsv() {
         Map<Long, Book> bookMemory = new HashMap<>();
         File file = new File(FILE_PATH);
         if (file.exists()) {
-            try (BufferedReader reader = new BufferedReader(new FileReader(FILE_PATH))) {
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    String[] parts = line.split(",");
-                    Book book = new Book(Long.parseLong(parts[0]), parts[1], parts[2], Integer.parseInt(parts[3]), Status.findStatusByString(parts[4]));
-                    // 정리 상태 도서가 있으면 대여 가능으로 변경
-                    if (book.getStatus().equals(Status.ORGANIZING)) {
-                        book.toAvailable();
-                    }
-                    bookMemory.put(Long.parseLong(parts[0]), book);
-                }
-            } catch (IOException ignore) {
-                System.out.println("[System] 잘못된 파일 접근입니다.\n");
-            } catch (Exception e) {
-                System.out.println("[System] 손상된 파일입니다. 데이터 초기화 후 진행합니다.\n");
-            }
+            return readAllBooksFromCsv(bookMemory);
         }
         return bookMemory;
     }
 
-    public void writeFile(Map<Long, Book> bookMemory ) {
+    private Map<Long, Book> readAllBooksFromCsv(Map<Long, Book> bookMemory) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(FILE_PATH))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(",");
+                Book book = new Book(Long.parseLong(parts[0]), parts[1], parts[2], Integer.parseInt(parts[3]), Status.findStatusByString(parts[4]));
+                // 정리 상태 도서가 있으면 대여 가능으로 변경
+                if (book.getStatus().equals(Status.ORGANIZING)) {
+                    book.toAvailable();
+                }
+                bookMemory.put(Long.parseLong(parts[0]), book);
+            }
+        } catch (IOException ignore) {
+            System.out.println("[System] 잘못된 파일 접근입니다." + System.lineSeparator());
+        } catch (Exception e) {
+            System.out.println("[System] 손상된 파일입니다. 데이터 초기화 후 진행합니다." + System.lineSeparator());
+        }
+        return bookMemory;
+    }
+
+    public void writeFile(Map<Long, Book> bookMemory) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_PATH))) {
 
             for (Book book : bookMemory.values()) {
