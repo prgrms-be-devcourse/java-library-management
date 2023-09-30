e90fb5e8c3b02b9f3d41cddd9eca98b90521b94c
ZZAMBAs <rksidksrksi@naver.com>
2023-09-30 22:34:10 +0900

    Refactor: 도서 상태를 잘 나타낼 수 있는 이름으로 변경하고 두번째 값 이름을 displayName으로 수정한다.

---
+++ src/main/java/entity/BookState.java
@@ -0,0 +1,18 @@
+package entity;
+
+public enum BookState {
+    AVAILABLE("대여 가능" + System.lineSeparator()),
+    RENTED("대여중" + System.lineSeparator()),
+    PROCESSING("정리중" + System.lineSeparator()),
+    LOST("분실됨" + System.lineSeparator()),
+    DELETED("삭제됨" + System.lineSeparator());
+
+    private final String displayName;
+    BookState(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+}
