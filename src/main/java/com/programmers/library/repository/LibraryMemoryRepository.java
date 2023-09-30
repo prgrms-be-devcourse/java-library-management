9aaf1abed3387a89f9d9b232deb3fc18dc7cea4c
EomYerim <djadpfa1002@naver.com>
2023-10-01 02:29:53 +0900

    feat: 인메모리 repository 생성

---
+++ src/main/java/com/programmers/library/repository/LibraryMemoryRepository.java
@@ -0,0 +1,45 @@
+package com.programmers.library.repository;
+
+import com.programmers.library.domain.Book;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class LibraryMemoryRepository implements LibraryRepository{
+    private static List<Book> books = new ArrayList<>();
+    private static int sequence = 0;
+
+    @Override
+    public Book save(Book book) {
+        book.setBookId(++sequence);
+        books.add(book);
+        return book;
+    }
+
+    @Override
+    public List<Book> findAll() {
+        return new ArrayList<>(books);
+    }
+
+    @Override
+    public List<Book> findByTitle(String title) {
+        return books.stream()
+                .filter(book -> book.getTitle().contains(title))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Optional<Book> findById(int bookId) {
+        return books.stream()
+                .filter(book -> book.getBookId() == bookId)
+                .findFirst();
+    }
+
+
+    @Override
+    public void delete(int bookId) {
+        books.removeIf(book -> book.getBookId() == bookId);
+    }
+}
