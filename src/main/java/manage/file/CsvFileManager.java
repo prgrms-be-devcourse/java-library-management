6e1d80f3bc353c1a2bfe3504a584576135b42550
ZZAMBAs <rksidksrksi@naver.com>
2023-09-30 22:45:58 +0900

    Refactor: HashMap으로 자료구조를 변경한다.

--- src/main/java/manage/file/CsvFileManager.java
+++ src/main/java/manage/file/CsvFileManager.java
@@ -1,14 +1,13 @@
 package manage.file;
 
 import entity.Book;
-import entity.State;
+import entity.BookState;
 import exception.BookNumberAlreadyExistException;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.stream.IntStream;
 
 public class CsvFileManager implements FileManager {
@@ -20,8 +19,8 @@ public class CsvFileManager implements FileManager {
     }
 
     @Override
-    public List<Book> read() {
-        List<Book> bookList = new ArrayList<>();
+    public HashMap<Integer, Book> read() {
+        HashMap<Integer, Book> books = new HashMap<>();
         HashSet<Integer> bookNumSet = new HashSet<>();
 
         File file = new File(filePath);
@@ -45,25 +44,25 @@ public class CsvFileManager implements FileManager {
                     throw new BookNumberAlreadyExistException("파일에 도서번호가 중복됩니다. 다시 확인해 주세요.");
 
                 bookNumSet.add(bookNum);
-                bookList.add(
-                        new Book(bookNum, split[1], split[2], Integer.parseInt(split[3]), State.valueOf(split[4]), Long.parseLong(split[5]))
+                books.put(bookNum,
+                        new Book(bookNum, split[1], split[2], Integer.parseInt(split[3]), BookState.valueOf(split[4]), Long.parseLong(split[5]))
                 );
             }
         } catch (IOException e){
             throw new RuntimeException("파일 읽기 중 문제 발생");
         }
 
-        return bookList;
+        return books;
     }
 
     @Override
-    public void write(List<Book> bookList) {
+    public void write(HashMap<Integer, Book> books) {
         try (BufferedWriter bw = new BufferedWriter(
                 new OutputStreamWriter(
                         new FileOutputStream(this.filePath), StandardCharsets.UTF_8))){
             bw.write("도서번호,도서 제목,작가,페이지 수,상태,마지막으로 반납한 시간");
             bw.newLine();
-            for(Book book : bookList)
+            for(Book book : books.values())
                 writeBook(bw, book);
 
         }catch (FileNotFoundException e){
@@ -79,7 +78,7 @@ public class CsvFileManager implements FileManager {
                 book.getTitle(),
                 book.getAuthor(),
                 String.valueOf(book.getPageNum()),
-                String.valueOf(book.getState()),
+                String.valueOf(book.getBookState()),
                 String.valueOf(book.getLastReturn())
         };
         IntStream.rangeClosed(1, 2).forEach(i -> {
