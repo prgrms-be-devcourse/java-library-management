bc16279c2342fecc3a8eb9deddb0a45a0df2e1e0
YongNyeo <won05121@gmail.com>
2023-09-30 20:46:26 +0900

    refactor: 스프링 의존성을 제거한다.

--- src/main/java/com/example/library/LibraryApplication.java
+++ src/main/java/com/example/library/LibraryApplication.java
@@ -1,14 +1,10 @@
 package com.example.library;
 
 import com.example.library.service.LibraryService;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class LibraryApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LibraryApplication.class, args);
         LibraryService libraryService = new LibraryService();
 
     }
