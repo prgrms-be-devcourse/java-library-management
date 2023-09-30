daa0fb042b8190d089cf46d9f04a4543f799e09f
EomYerim <djadpfa1002@naver.com>
2023-10-01 02:43:06 +0900

    fix: main 실행 모드별 의존성 주입

--- src/main/java/com/programmers/library/LibraryApplication.java
+++ src/main/java/com/programmers/library/LibraryApplication.java
@@ -1,7 +1,32 @@
 package com.programmers.library;
 
+import com.programmers.library.controller.LibraryController;
+import com.programmers.library.repository.LibraryFileRepository;
+import com.programmers.library.repository.LibraryMemoryRepository;
+import com.programmers.library.repository.LibraryRepository;
+import com.programmers.library.service.LibraryService;
+import com.programmers.library.utils.Mode;
+
 public class LibraryApplication {
     public static void main(String[] args) {
-        System.out.println("도서관리프로그램");
+        // 모드 선택
+        Mode modeType = Mode.selectMode();
+
+        // 모드별 Repository 의존 주입
+        LibraryRepository libraryRepository;
+        if(modeType == Mode.NORMAL) {
+            libraryRepository = new LibraryFileRepository();
+        }
+        else {
+            libraryRepository = new LibraryMemoryRepository();
+        }
+
+        // Service, Controller 의존 주입
+        LibraryService libraryService = new LibraryService(libraryRepository);
+        LibraryController libraryController = new LibraryController(libraryService);
+
+        // 메뉴 시작
+        libraryController.run();
     }
+
 }
