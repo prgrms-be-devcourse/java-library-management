57a04f4fe086d2358d327b4a08ea7d68767b392e
YongNyeo <won05121@gmail.com>
2023-09-30 20:59:25 +0900

    refactor: 입력값 검증시 예외 처리 메시지를 추가한다.

--- src/main/java/com/example/library/validation/InputValidator.java
+++ src/main/java/com/example/library/validation/InputValidator.java
@@ -1,12 +1,12 @@
 package com.example.library.validation;
 
-public class InputCheck {
+public class InputValidator {
 
     public static int isNumber(String input) {
         try {
             return Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            throw new NumberFormatException();
+            throw new NumberFormatException("숫자가 아닌 문자를 입력하셨습니다. 다시 입력해주세요. ");
         }
 
     }
@@ -18,7 +18,7 @@ public class InputCheck {
         if (modeNumber == 1 || modeNumber == 2) {
             return modeNumber;
         }
-        throw new NumberFormatException();
+        throw new NumberFormatException("모드 선택은 1번 혹은 2번만 가능합니다.");
     }
 
     public static int isMenuNumber(String input) {
@@ -28,7 +28,7 @@ public class InputCheck {
         if (menuNumber < 8 && menuNumber > -1) {
             return menuNumber;
         }
-        throw new NumberFormatException();
+        throw new NumberFormatException("메뉴 선택은 0번부터 7번까지 가능합니다.");
     }
 
 }
