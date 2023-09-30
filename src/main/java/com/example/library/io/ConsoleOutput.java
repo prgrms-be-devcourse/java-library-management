8f10bb9ed836b57768cb8e0b4c7b2ee7db012678
YongNyeo <won05121@gmail.com>
2023-09-30 21:47:26 +0900

    refactor: jdk17의 텍스트블록 출력으로 변경한다.

--- src/main/java/com/example/library/io/ConsoleOutput.java
+++ src/main/java/com/example/library/io/ConsoleOutput.java
@@ -3,10 +3,19 @@ package com.example.library.io;
 public class ConsoleOutput {
 
     public ConsoleOutput() {
-        System.out.println("Q. 모드를 선택해주세요.\n" +
-                "1. 일반 모드\n" +
-                "2. 테스트 모드\n");
+
+        String printMode= """
+                
+                Q. 모드를 선택해주세요.
+                
+                1. 일반 모드
+                2. 테스트 모드
+                
+                """;
+
+        System.out.println(printMode);
     }
+
 
     public void printTitleQuestion() {
         System.out.println("Q. 등록할 도서 제목을 입력하세요.");
@@ -33,9 +42,15 @@ public class ConsoleOutput {
     }
 
     public void startPrintByTitle() {
-        System.out.println("[System] 제목으로 도서 검색 메뉴로 넘어갑니다.\n" + "\n" +
-                "Q. 검색할 도서 제목 일부를 입력하세요.\n");
+        String printByTitleMenu = """
+               
+                [System] 제목으로 도서 검색 메뉴로 넘어갑니다.
+                Q. 검색할 도서 제목 일부를 입력하세요.
+                
+                """;
+        System.out.println(printByTitleMenu);
     }
+
     public void printBookNotExist() {
         System.out.println("[System] 존재하지 않는 도서번호 입니다.\n");
     }
@@ -53,37 +68,62 @@ public class ConsoleOutput {
     }
 
     public void printMenu() {
-        System.out.println("Q. 사용할 기능을 선택해주세요.\n" +
-                "1. 도서 등록\n" +
-                "2. 전체 도서 목록 조회\n" +
-                "3. 제목으로 도서 검색\n" +
-                "4. 도서 대여\n" +
-                "5. 도서 반납\n" +
-                "6. 도서 분실\n" +
-                "7. 도서 삭제\n");
+
+        String menu = """
+                Q. 사용할 기능을 선택해주세요.
+                1. 도서 등록
+                2. 전체 도서 목록 조회
+                3. 제목으로 도서 검색
+                4. 도서 대여
+                5. 도서 반납
+                6. 도서 분실
+                7. 도서 삭제
+                """;
+        System.out.println(menu);
     }
 
     public void startBorrowBook() {
-        System.out.println("[System] 도서 대여 메뉴로 넘어갑니다.\n" +
-                "\n" +
-                "Q. 대여할 도서번호를 입력하세요\n");
+        String borrowMenu = """
+                
+                [System] 도서 대여 메뉴로 넘어갑니다.
+
+                Q. 대여할 도서번호를 입력하세요
+                
+                """;
+        System.out.println(borrowMenu);
     }
 
     public void startReturnBook() {
-        System.out.println("[System] 도서 반납 메뉴로 넘어갑니다.\n" +
-                "\n" +
-                "Q. 반납할 도서번호를 입력하세요\n");
+        String returnMenu = """
+                
+                [System] 도서 반납 메뉴로 넘어갑니다.
+              
+                Q. 반납할 도서번호를 입력하세요.
+                
+                """;
+        System.out.println(returnMenu);
     }
 
     public void startLoseBook() {
-        System.out.println("[System] 도서 분실 처리 메뉴로 넘어갑니다.\n" +
-                "\n" +
-                "Q. 분실 처리할 도서번호를 입력하세요\n");
+        String loseMenu = """
+                
+                [System] 도서 분실 처리 메뉴로 넘어갑니다.
+              
+                Q. 분실 처리할 도서번호를 입력하세요
+                
+                """;
+        System.out.println(loseMenu);
     }
 
     public void startDeleteBook() {
-        System.out.println("[System] 도서 삭제 처리 메뉴로 넘어갑니다.\n" +
-                "\n" +
-                "Q. 삭제 처리할 도서번호를 입력하세요\n");
+        String deleteMenu = """
+                
+                [System] 도서 삭제 처리 메뉴로 넘어갑니다.
+              
+                Q. 삭제 처리할 도서번호를 입력하세요.
+                
+                """;
+        System.out.println(deleteMenu);
+
     }
 }
