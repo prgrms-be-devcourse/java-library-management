40f0c86c07840ad30d7a1c5b1886d7fe28c454dc
shoeone96 <dlwnddnjs96@gmail.com>
2023-10-01 17:42:24 +0900

    [feat] library Interface에 맞춰 인메모리 db 작성

---
+++ src/main/java/com/programmers/repository/TestBookRepository.java
@@ -0,0 +1,40 @@
+package com.programmers.repository;
+
+import com.programmers.domain.Book;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+public class TestBookRepository implements BookRepository{
+
+    private final List<Book> bookList = new ArrayList<>();
+    private static long sequence = 0L; //static
+
+    @Override
+    public List<Book> findByBookTitle(String title) {
+        return bookList.stream()
+                .filter((book) -> book.searchByTitle(title))
+                .toList();
+    }
+
+    @Override
+    public Optional<Book> findByBookId(Long bookId) {
+        return bookList.stream()
+                .filter((book) -> Objects.equals(book.getBookId(), bookId))
+                .findAny();
+    }
+
+    @Override
+    public List<Book> findAll() {
+        return bookList;
+    }
+
+    @Override
+    public void saveBook(Book book) {
+        book.settingId(++sequence);
+        bookList.add(book);
+    }
+
+}
