cdea90ac1df2d03d81eace902a40d094e550a15d
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2023-10-02 01:21:10 +0900

    refactor: 도서 기능 선택 시, enum으로 커맨드 관리하도록 수정

---
+++ src/main/java/org/example/LibraryFunctionType.java
@@ -0,0 +1,33 @@
+package org.example;
+
+public enum LibraryFunctionType {
+    REGISTER_BOOK(1, "Register Book"),
+    SEARCH_ALL_BOOKS(2, "Search All Books"),
+    SEARCH_BOOKS_BY_TITLE(3, "Search Books by Title"),
+    BORROW_BOOK(4, "Borrow Book"),
+    RETURN_BOOK(5, "Return Book"),
+    LOST_BOOK(6, "Lost Book"),
+    DELETE_BOOK(7, "Delete Book");
+
+    private Integer number;
+    private String name;
+
+    LibraryFunctionType(Integer number, String name) {
+        this.number = number;
+        this.name = name;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public static LibraryFunctionType getValueByNumber(Integer number) {
+        for (LibraryFunctionType libraryFunctionType : LibraryFunctionType.values()) {
+            if (libraryFunctionType.getNumber().equals(number)) {
+                return libraryFunctionType;
+            }
+        }
+
+        throw new IllegalArgumentException("1부터 7까지의 정수 중 하나를 입력해주세요.");
+    }
+}
