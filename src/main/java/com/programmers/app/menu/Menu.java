a159018dddec47d73648ae7e64a9d0fac98c2695
Jay <injaesong0@gmail.com>
2023-10-01 12:02:55 +0900

    refactor: clarify the responsibility of Menu Executer

--- src/main/java/com/programmers/app/menu/Menu.java
+++ src/main/java/com/programmers/app/menu/Menu.java
@@ -1,32 +1,22 @@
 package com.programmers.app.menu;
 
-import java.util.function.Consumer;
-
-import com.programmers.app.book.controller.BookController;
-
 public enum Menu {
-    EXIT(0, BookController::exit),
-    REGISTER(1, BookController::register),
-    FIND_ALL_BOOKS(2, BookController::findAllBooks),
-    SEARCH_TITLE(3, BookController::searchBookByTitle),
-    BORROW_BOOK(4, BookController::borrowBook),
-    RETURN_BOOK(5, BookController::returnBook),
-    REPORT_LOST(6, BookController::reportLostBook),
-    DELETE_BOOK(7, BookController::deleteBook);
+    EXIT(0),
+    REGISTER(1),
+    FIND_ALL_BOOKS(2),
+    SEARCH_TITLE(3),
+    BORROW_BOOK(4),
+    RETURN_BOOK(5),
+    REPORT_LOST(6),
+    DELETE_BOOK(7);
 
     private final int menuCode;
-    private final Consumer<BookController> functionToExecute;
 
-    Menu(int menuCode, Consumer<BookController> functionToExecute) {
+    Menu(int menuCode) {
         this.menuCode = menuCode;
-        this.functionToExecute = functionToExecute;
     }
 
     public boolean isSelected(int menuCode) {
         return this.menuCode == menuCode;
-    }
-
-    public void executeMenu(BookController bookController) {
-        this.functionToExecute.accept(bookController);
     }
 }
--- src/main/java/com/programmers/app/menu/MenuExecuter.java
+++ src/main/java/com/programmers/app/menu/MenuExecuter.java
@@ -1,6 +1,7 @@
 package com.programmers.app.menu;
 
 import com.programmers.app.book.controller.BookController;
+import com.programmers.app.exception.InvalidInputException;
 
 public class MenuExecuter {
     private final BookController bookController;
@@ -10,6 +11,33 @@ public class MenuExecuter {
     }
 
     public void execute(Menu menu) {
-        menu.executeMenu(bookController);
+        switch (menu) {
+            case EXIT:
+                bookController.exit();
+                break;
+            case REGISTER:
+                bookController.register();
+                break;
+            case FIND_ALL_BOOKS:
+                bookController.findAllBooks();
+                break;
+            case SEARCH_TITLE:
+                bookController.searchBookByTitle();
+                break;
+            case BORROW_BOOK:
+                bookController.borrowBook();
+                break;
+            case RETURN_BOOK:
+                bookController.returnBook();
+                break;
+            case REPORT_LOST:
+                bookController.reportLostBook();
+                break;
+            case DELETE_BOOK:
+                bookController.deleteBook();
+                break;
+            default:
+                throw new InvalidInputException();
+        }
     }
 }
