1a00393d1bfd6a0d9793c08a48543827742360f3
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2023-10-02 17:45:26 +0900

    style: 텍스트 블럭 코드 포맷 수정

--- src/main/java/org/example/Client.java
+++ src/main/java/org/example/Client.java
@@ -22,7 +22,8 @@ public class Client {
         System.out.println("""
                 Q. 모드를 선택해주세요.
                 1. 일반 모드
-                2. 테스트 모드""");
+                2. 테스트 모드"""
+        );
         int mode = scan.nextInt();
         if (mode == 1) {
             System.out.println("일반 모드를 입력하셨습니다.");
@@ -43,7 +44,8 @@ public class Client {
                 4. 도서 대여
                 5. 도서 반납
                 6. 도서 분실
-                7. 도서 삭제""");
+                7. 도서 삭제"""
+        );
 
         int functionNumber = scan.nextInt();
         scan.nextLine();
@@ -68,7 +70,8 @@ public class Client {
     private void registerBook() {
         System.out.println("""
                 [System] 도서 등록 메뉴로 넘어갑니다.
-                Q. 등록할 도서 제목을 입력하세요.""");
+                Q. 등록할 도서 제목을 입력하세요."""
+        );
         String title = scan.nextLine();
         System.out.println("Q. 작가 이름을 입력하세요.");
         String author = scan.nextLine();
@@ -91,7 +94,8 @@ public class Client {
     private void searchBooksByTitle() {
         System.out.println("""
                 [System] 제목으로 도서 검색 메뉴로 넘어갑니다.
-                Q. 검색할 도서 제목 일부를 입력하세요.-""");
+                Q. 검색할 도서 제목 일부를 입력하세요.-"""
+        );
         String title = scan.nextLine();
         libraryManagementService.searchBookBy(title)
                 .stream()
@@ -104,7 +108,8 @@ public class Client {
     private void borrowBook() {
         System.out.println("""
                 [System] 제목으로 도서 대여 메뉴로 넘어갑니다.
-                Q. 대여할 도서번호를 입력하세요.""");
+                Q. 대여할 도서번호를 입력하세요."""
+        );
         Integer bookId = scan.nextInt();
         try {
             libraryManagementService.borrowBook(bookId);
@@ -117,7 +122,8 @@ public class Client {
     private void returnBook() {
         System.out.println("""
                 [System] 도서 반납 메뉴로 넘어갑니다.
-                Q. 반납할 도서번호를 입력하세요.""");
+                Q. 반납할 도서번호를 입력하세요."""
+        );
         Integer bookId = scan.nextInt();
         try {
             libraryManagementService.returnBook(bookId);
@@ -130,7 +136,8 @@ public class Client {
     private void lostBook() {
         System.out.println("""
                 [System] 도서 분실 처리 메뉴로 넘어갑니다.
-                Q. 분실 처리할 도서번호를 입력하세요.""");
+                Q. 분실 처리할 도서번호를 입력하세요."""
+        );
         Integer bookId = scan.nextInt();
         try {
             libraryManagementService.lostBook(bookId);
@@ -143,7 +150,8 @@ public class Client {
     private void deleteBook() {
         System.out.println("""
                 [System] 도서 삭제 처리 메뉴로 넘어갑니다.
-                Q. 삭제 처리할 도서번호를 입력하세요""");
+                Q. 삭제 처리할 도서번호를 입력하세요"""
+        );
         Integer bookId = scan.nextInt();
         try {
             libraryManagementService.deleteBook(bookId);
