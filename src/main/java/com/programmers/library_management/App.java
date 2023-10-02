4604eb714c7fa59100e1b627a9b86eb86969bf43
KyungMin Lee <hello.tidavid@gmail.com>
2023-10-02 19:54:05 +0900

    LibraryManagement 멤버 변수 final 선언을 위한 App selectManageMode 메소드 추가

--- src/main/java/com/programmers/library_management/App.java
+++ src/main/java/com/programmers/library_management/App.java
@@ -3,11 +3,37 @@ package com.programmers.library_management;
 import com.programmers.library_management.business.LibraryManagement;
 import com.programmers.library_management.utils.ConsoleIOManager;
 
+import java.io.IOException;
+
 public class App {
     public static void main(String[] args) {
-        LibraryManagement libraryManagement = new LibraryManagement(new ConsoleIOManager());
-        if (libraryManagement.selectManagementMode()) {
+        ConsoleIOManager consoleIOManager = new ConsoleIOManager();
+        try {
+            LibraryManagement libraryManagement = new LibraryManagement(consoleIOManager, selectManagementMode(consoleIOManager));
             libraryManagement.run();
+        } catch (RuntimeException e) {
+            consoleIOManager.printSystemMsg(e.getMessage());
+        } catch (IOException e) {
+            consoleIOManager.printIOExceptionMsg();
+        }
+    }
+
+    private static boolean selectManagementMode(ConsoleIOManager consoleIOManager) throws RuntimeException, IOException {
+        consoleIOManager.printModeMenu();
+        String input = consoleIOManager.getInput();
+        switch (input) {
+            case "1" -> {
+                consoleIOManager.printSystemMsg("일반 모드로 애플리케이션을 실행합니다.");
+                return false;
+            }
+            case "2" -> {
+                consoleIOManager.printSystemMsg("테스트 모드로 애플리케이션을 실행합니다.");
+                return true;
+            }
+            default -> {
+                consoleIOManager.printSystemMsg("잘못된 모드 선택입니다.");
+                throw new RuntimeException("잘못된 모드 선택입니다.");
+            }
         }
     }
 }
--- src/main/java/com/programmers/library_management/business/LibraryManagement.java
+++ src/main/java/com/programmers/library_management/business/LibraryManagement.java
@@ -1,5 +1,6 @@
 package com.programmers.library_management.business;
 
+import com.programmers.library_management.repository.BookRepository;
 import com.programmers.library_management.repository.ProductBookRepository;
 import com.programmers.library_management.repository.TestBookRepository;
 import com.programmers.library_management.service.LibraryManagementService;
@@ -10,41 +11,24 @@ import java.io.IOException;
 
 public class LibraryManagement {
     private final ConsoleIOManager consoleIOManager;
-    private LibraryManagementService libraryManagementService;
+    private final LibraryManagementService libraryManagementService;
     private final UpdateScheduler updateScheduler;
 
-    public LibraryManagement(ConsoleIOManager consoleIOManager) {
+    public LibraryManagement(ConsoleIOManager consoleIOManager, boolean isTest) {
+        BookRepository bookRepository;
         this.consoleIOManager = consoleIOManager;
         this.updateScheduler = new UpdateScheduler();
+        if (isTest) {
+            bookRepository = new TestBookRepository();
+        } else {
+            bookRepository = new ProductBookRepository();
+        }
+        this.libraryManagementService = new LibraryManagementService(bookRepository);
     }
 
-    public boolean selectManagementMode() {
-        consoleIOManager.printModeMenu();
-        try {
-            String input = consoleIOManager.getInput();
-            switch (input) {
-                case "1" -> {
-                    libraryManagementService = new LibraryManagementService(new ProductBookRepository());
-                    consoleIOManager.printSystemMsg("일반 모드로 애플리케이션을 실행합니다.");
-                }
-                case "2" -> {
-                    libraryManagementService = new LibraryManagementService(new TestBookRepository());
-                    consoleIOManager.printSystemMsg("테스트 모드로 애플리케이션을 실행합니다.");
-                }
-                default -> {
-                    consoleIOManager.printSystemMsg("잘못된 모드 선택입니다.");
-                    return false;
-                }
-            }
-        } catch (IOException e) {
-            consoleIOManager.printIOExceptionMsg();
-            return false;
-        }
-        updateScheduler.execute(() -> libraryManagementService.updateBookStatus());
-        return true;
-    }
 
     public void run() {
+        updateScheduler.execute(libraryManagementService::updateBookStatus);
         String input = "";
         do {
             consoleIOManager.printFuncMenu();
