70550ca253278614f5dd87ca9960dd2d7a917dfb
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2023-10-02 18:43:21 +0900

    refactor: Book 인스턴스를 담는 List를 Map으로 수정

--- src/main/java/org/example/repository/InMemoryRepository.java
+++ src/main/java/org/example/repository/InMemoryRepository.java
@@ -4,66 +4,52 @@ import org.example.domain.Book;
 import org.example.domain.BookStatusType;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class InMemoryRepository implements Repository {
-    List<Book> books = new ArrayList<>();
+    private Map<Integer, Book> books = new HashMap<>();
 
     @Override
     public void saveBook(Book book) {
-        books.add(book);
+        books.put(book.getId(), book);
     }
 
     @Override
     public List<Book> findAllBooks() {
-        return books;
+        return new ArrayList<>(books.values());
     }
 
     @Override
     public List<Book> findBookByTitle(String title) {
-        return books.stream()
+        return books.values().stream()
                 .filter(book -> book.getTitle().contains(title))
                 .collect(Collectors.toList());
     }
 
     @Override
     public Optional<Book> findBookById(Integer bookId) {
-        return books.stream()
-                .filter(book -> book.getId().equals(bookId))
-                .findFirst();
+        return Optional.ofNullable(books.get(bookId));
     }
 
     @Override
     public void updateBookStatus(Integer bookId, BookStatusType status) {
-        Optional<Book> bookToUpdate = books.stream()
-                .filter(book -> book.getId().equals(bookId))
-                .findFirst();
-        bookToUpdate.ifPresent(
-                book -> {
-                    book.setStatus(status);
-                    if (status == BookStatusType.ORGANIZING)
-                        book.setReturnTime(LocalDateTime.now());
-                    else
-                        book.setReturnTime(null);
-                }
-        );
+        Book bookToUpdate = books.get(bookId);
+        bookToUpdate.setStatus(status);
+        if (BookStatusType.ORGANIZING == status) {
+            bookToUpdate.setReturnTime(LocalDateTime.now());
+        } else {
+            bookToUpdate.setReturnTime(null);
+        }
     }
 
     @Override
     public void deleteBookById(Integer bookId) {
-        books.removeIf(book -> book.getId().equals(bookId));
+        books.remove(bookId);
     }
 
     @Override
     public Integer getNextBookId() {
-        Integer lastBookId = 0;
-        if (books.size() > 0) {
-            lastBookId = books.get(books.size() - 1).getId();
-        }
-
-        return lastBookId + 1;
+        return books.keySet().stream().max(Integer::compareTo).orElse(1);
     }
 }
