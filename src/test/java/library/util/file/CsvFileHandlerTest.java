6d576c788b8420acb67cf4bb73c7ff145e92566b
hyoguoo <hyoguoo@gmail.com>
2023-10-02 19:26:03 +0900

    refactor: CsvFileHandler를 테스트 시작 전에 초기화하도록 리팩터링하여 중복 코드 제거

--- src/test/java/library/util/file/CsvFileHandlerTest.java
+++ src/test/java/library/util/file/CsvFileHandlerTest.java
@@ -2,6 +2,7 @@ package library.util.file;
 
 import library.domain.Book;
 import library.domain.BookStatus;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -15,13 +16,16 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 class CsvFileHandlerTest {
 
     static private final String testFilePath = "src/main/resources/books.csv";
+    private CsvFileHandler fileHandler;
+
+    @BeforeEach
+    void setUp() {
+        fileHandler = new CsvFileHandler(testFilePath);
+    }
 
     @Test
     @DisplayName("도서 목록을 파일에서 불러올 수 있어야 합니다.")
     void testLoadBooksFromFile() {
-        // given
-        CsvFileHandler fileHandler = new CsvFileHandler(testFilePath);
-
         // when
         List<Book> loadedBooks = fileHandler.loadBooksFromFile();
 
@@ -33,7 +37,6 @@ class CsvFileHandlerTest {
     @DisplayName("도서 목록을 파일에 저장할 수 있어야 합니다.")
     void testSaveBooksToFile() {
         // given
-        CsvFileHandler fileHandler = new CsvFileHandler(testFilePath);
         List<Book> savedBooks = new ArrayList<>();
         savedBooks.add(Book.createBook(1L, "Title 1", "Author 1", 100, LocalDateTime.now(), BookStatus.AVAILABLE));
         savedBooks.add(Book.createBook(2L, "Title 2", "Author 2", 200, LocalDateTime.now(), BookStatus.RENTED));
