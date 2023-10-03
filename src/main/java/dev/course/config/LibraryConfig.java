6fd87f899ddff2cb5b9b29611194e4d13f3a6fcb
JIN-076 <andy230@naver.com>
2023-10-03 23:29:50 +0900

    update : 사용하지 않는 필드 제거

--- src/main/java/dev/course/config/LibraryConfig.java
+++ src/main/java/dev/course/config/LibraryConfig.java
@@ -1,22 +1,14 @@
 package dev.course.config;
 
-import dev.course.manager.ConsoleManager;
-import dev.course.manager.JSONFileManager;
 import lombok.Builder;
 
 public class LibraryConfig {
 
     private final BookRepositoryConfig bookRepositoryConfig;
-    private final ConsoleManager consoleManager;
 
     @Builder
-    public LibraryConfig(BookRepositoryConfig bookRepositoryConfig, ConsoleManager consoleManager) {
+    public LibraryConfig(BookRepositoryConfig bookRepositoryConfig) {
         this.bookRepositoryConfig = bookRepositoryConfig;
-        this.consoleManager = consoleManager;
-    }
-
-    public ConsoleManager getConsoleManager() {
-        return this.consoleManager;
     }
 
     public BookRepositoryConfig getBookRepositoryConfig() {
