0cf2ead87248b8c9845cce36de1f3123b3248f9c
kkangh00n <kig2454@gmail.com>
2023-10-03 22:11:48 +0900

    refactor: 주석 제거

--- src/main/java/domain/Book.java
+++ src/main/java/domain/Book.java
@@ -2,15 +2,14 @@ package domain;
 
 public class Book {
 
-    //도서 번호
     private Long id;
-    //제목
+
     private String name;
-    //작가 이름
+
     private String author;
-    //페이지 수
+
     private int page;
-    //상태
+
     private Status status;
 
     public Book(String name, String author, int page) {
@@ -48,7 +47,6 @@ public class Book {
         this.id = id;
     }
 
-    //대여
     public void rentalBook(){
         if(this.status == Status.IMPOSSIBLE) System.out.println("[System] 이미 대여중인 도서입니다.");
         else if (this.status == Status.ORGANIZE) System.out.println("[System] 도서 정리중입니다.");
@@ -59,7 +57,6 @@ public class Book {
         }
     }
 
-    //반납
     public void organizeBook(){
         if(this.status == Status.IMPOSSIBLE || this.status == Status.LOST) {
             System.out.println("[System] 도서가 반납 처리 되었습니다.");
@@ -73,7 +70,6 @@ public class Book {
         this.status = Status.POSSIBLE;
     }
 
-    //분실
     public void lostBook(){
         if(this.status != Status.LOST){
             System.out.println("[System] 도서가 분실 처리 되었습니다.");
--- src/main/java/repository/Repository.java
+++ src/main/java/repository/Repository.java
@@ -6,14 +6,13 @@ import java.util.List;
 
 public interface Repository {
 
-    //도서 추가
     void addBook(Book book);
-    //전체 목록 조회
+
     List<Book> getAll();
-    //제목 검색
+
     List<Book> searchBook(String name);
-    //도서 조회
+
     Book getBook(Long bookNumber);
-    //도서 삭제
+
     void deleteBook(Long bookNumber);
 }
