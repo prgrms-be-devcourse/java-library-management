b15634c1b5fd67f83ed8590936bed290dd939d33
KarmaPol <kkhkr98@gmail.com>
2023-10-03 23:44:20 +0900

    test : test

---
+++ src/test/java/service/BookServiceTest.java
@@ -0,0 +1,24 @@
+package service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class BookServiceTest {
+//    private final Repository repository = new TestRepository();
+//    private BookService bookService = new BookService(repository);
+
+    @DisplayName("도서 등록 테스트")
+    @Test
+    void addTest(){
+        // given
+
+
+        // when
+
+        // then
+
+
+    }
+
+
+}
