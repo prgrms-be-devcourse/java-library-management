f147e8f3bc8e870d4ea03633b7182ab5d3b18d4b
shoeone96 <dlwnddnjs96@gmail.com>
2023-10-03 16:38:59 +0900

    [refactor] 11. 메서드 분리 실행
    각 기능 단위로 해당 클래스 내부에서만 사용가능한 메서드로 분리

--- src/main/java/com/programmers/controller/NumberMappingHandler.java
+++ src/main/java/com/programmers/controller/NumberMappingHandler.java
@@ -12,31 +12,44 @@ public class NumberMappingHandler {
         this.bookController = bookController;
     }
 
-    public boolean numberMapping(String num){
+    public boolean numberMapping(String num) {
         try {
-                boolean keep = true;
-                if(!ErrorChecking.checkNumber(num)) throw new LibraryException(ErrorCode.NOT_NUMBER);
-                int menu = changeToNum(num);
-                if(!ErrorChecking.optionChecking(menu)) throw new LibraryException(ErrorCode.NUMBER_OVER_BOUNDARY);;
-
-                switch (menu) {
-                    case 1 -> bookController.enrollBook();
-                    case 2 -> bookController.findAllBooks();
-                    case 3 -> bookController.findBookByTitle();
-                    case 4 -> bookController.rentBook();
-                    case 5 -> bookController.returnBook();
-                    case 6 -> bookController.loseBook();
-                    case 7 -> bookController.deleteBook();
-                    case 8 -> keep = false;
-                }
-                return keep;
-        } catch (LibraryException e){
-            System.out.println(e.getErrorCode().getMessage());
-            System.out.println("[System] 시스템을 다시 시작합니다.");
-            LibraryEngine libraryEngine = new LibraryEngine();
-            libraryEngine.init(LibraryEngine.getSetting());
+            boolean keep = true;
+            int menu = checkingMenu(num);
+            keep = executeOption(keep, menu);
+            return keep;
+        } catch (LibraryException e) {
+            retryLibraryEngine(e);
         }
         return true;
+    }
+
+    private static void retryLibraryEngine(LibraryException e) {
+        System.out.println(e.getErrorCode().getMessage());
+        System.out.println("[System] 시스템을 다시 시작합니다.");
+        LibraryEngine libraryEngine = new LibraryEngine();
+        libraryEngine.init(LibraryEngine.getSetting());
+    }
+
+    private int checkingMenu(String num) {
+        if (!ErrorChecking.checkNumber(num)) throw new LibraryException(ErrorCode.NOT_NUMBER);
+        int menu = changeToNum(num);
+        if (!ErrorChecking.optionChecking(menu)) throw new LibraryException(ErrorCode.NUMBER_OVER_BOUNDARY);
+        return menu;
+    }
+
+    private boolean executeOption(boolean keep, int menu) {
+        switch (menu) {
+            case 1 -> bookController.enrollBook();
+            case 2 -> bookController.findAllBooks();
+            case 3 -> bookController.findBookByTitle();
+            case 4 -> bookController.rentBook();
+            case 5 -> bookController.returnBook();
+            case 6 -> bookController.loseBook();
+            case 7 -> bookController.deleteBook();
+            case 8 -> keep = false;
+        }
+        return keep;
     }
 
     private int changeToNum(String input) {
