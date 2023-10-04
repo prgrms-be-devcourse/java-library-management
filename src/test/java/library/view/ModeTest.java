6173dc24174fb42a06a7936190afbb8dddd10aef
hyoguoo <hyoguoo@gmail.com>
2023-10-04 19:50:50 +0900

    refactor: 모드 선택 테스트에 ParameterizedTest 적용

--- src/test/java/library/view/ModeTest.java
+++ src/test/java/library/view/ModeTest.java
@@ -4,34 +4,62 @@ import library.repository.BookRepository;
 import library.repository.FileBookRepository;
 import library.repository.InMemoryBookRepository;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Optional;
+import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class ModeTest {
 
-    @Test
-    @DisplayName("모드 코드로 모드를 찾을 수 있어야 합니다.")
-    void testFindByCode() {
-        Optional<Mode> normalMode = Mode.findByCode("1");
-        assertEquals(Mode.NORMAL, normalMode.get());
-
-        Optional<Mode> testMode = Mode.findByCode("2");
-        assertEquals(Mode.TEST, testMode.get());
-
-        Optional<Mode> invalidMode = Mode.findByCode("3");
-        assertFalse(invalidMode.isPresent());
+    private static Stream<Arguments> modeCode() {
+        return Stream.of(
+                Arguments.of(Mode.NORMAL, "1"),
+                Arguments.of(Mode.TEST, "2")
+        );
     }
 
-    @Test
-    @DisplayName("모드에 따라 도서 저장소를 가져올 수 있어야 합니다.")
-    void testGetBookRepository() {
-        BookRepository normalRepository = Mode.NORMAL.getBookRepository();
-        assertTrue(normalRepository instanceof FileBookRepository);
+    private static Stream<Arguments> modeStrategy() {
+        return Stream.of(
+                Arguments.of(Mode.NORMAL, FileBookRepository.class),
+                Arguments.of(Mode.TEST, InMemoryBookRepository.class)
+        );
+    }
 
-        BookRepository testRepository = Mode.TEST.getBookRepository();
-        assertTrue(testRepository instanceof InMemoryBookRepository);
+    @DisplayName("모드 코드로 모드를 찾을 수 있어야 합니다.")
+    @MethodSource("modeCode")
+    @ParameterizedTest(name = "모드 {0}의 코드는 {1}입니다.")
+    void testFindByCode(Mode mode, String code) {
+        Optional<Mode> actual = Mode.findByCode(code);
+        assertThat(actual).contains(mode);
+    }
+
+    @DisplayName("모드 코드로 모드를 찾을 수 없으면 Optional.empty()를 반환합니다.")
+    @ValueSource(strings = {"3", "4"})
+    @ParameterizedTest(name = "모드 코드 {0}에 해당하는 모드가 없으면 Optional.empty()를 반환합니다.")
+    void failFindByCodeTest(String code) {
+        Optional<Mode> actual = Mode.findByCode(code);
+        assertThat(actual).isEmpty();
+    }
+
+    @DisplayName("빈 값이나 null로 모드를 조회 시 Optional.empty()를 반환합니다.")
+    @NullAndEmptySource
+    @ParameterizedTest(name = "모드 코드가 {0}이면 Optional.empty()를 반환합니다.")
+    void failFindByNullAndEmptyCodeTest(String code) {
+        Optional<Mode> actual = Mode.findByCode(code);
+        assertThat(actual).isEmpty();
+    }
+
+    @DisplayName("모드에 따라 도서 저장소를 가져올 수 있어야 합니다.")
+    @MethodSource("modeStrategy")
+    @ParameterizedTest(name = "모드 {0}에 따라 도서 저장소를 가져올 수 있어야 합니다.")
+    void testGetBookRepository(Mode mode, Class<?> expected) {
+        BookRepository actual = mode.getBookRepository();
+        assertThat(actual).isInstanceOf(expected);
     }
 }
