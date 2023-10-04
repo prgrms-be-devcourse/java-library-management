dccbe55883ae57803d31d13584960566228ea698
letskuku <yujung9288@naver.com>
2023-10-05 02:52:18 +0900

    feat: controller에서 출력 로직 분리

--- src/main/java/com/programmers/librarymanagement/presentation/LibraryManagementController.java
+++ src/main/java/com/programmers/librarymanagement/presentation/LibraryManagementController.java
@@ -3,6 +3,7 @@ package com.programmers.librarymanagement.presentation;
 import com.programmers.librarymanagement.domain.Book;
 import com.programmers.librarymanagement.domain.ReturnResult;
 import com.programmers.librarymanagement.domain.Status;
+import com.programmers.librarymanagement.dto.BookRequestDto;
 import com.programmers.librarymanagement.utils.ConsoleIo;
 import com.programmers.librarymanagement.repository.NormalBookRepository;
 import com.programmers.librarymanagement.repository.TestBookRepository;
@@ -28,24 +29,24 @@ public class LibraryManagementController {
         try {
             switch (consoleIo.getInputInt()) {
                 case 1 -> {
-                    System.out.println("[System] 일반 모드로 애플리케이션을 실행합니다. \n");
+                    consoleIo.printNormalMode();
                     libraryManagementService = new LibraryManagementService(new NormalBookRepository());
                     run();
                 }
 
                 case 2 -> {
-                    System.out.println("[System] 테스트 모드로 애플리케이션을 실행합니다. \n");
+                    consoleIo.printTestMode();
                     libraryManagementService = new LibraryManagementService(new TestBookRepository());
                     run();
                 }
 
                 default -> {
-                    System.out.println("[System] 존재하지 않는 선택지입니다. 다시 입력해주세요. \n");
+                    consoleIo.printWrongSelect();
                     this.start();
                 }
             }
         } catch (IOException e) {
-            System.out.println("[System] 올바른 입력 형식이 아닙니다. 다시 입력해주세요. \n");
+            consoleIo.printIOException();
             this.start();
         }
     }
@@ -71,10 +72,10 @@ public class LibraryManagementController {
 
                 case 7 -> deleteBook();
 
-                default -> System.out.println("[System] 존재하지 않는 선택지입니다. 다시 입력해주세요. \n");
+                default -> consoleIo.printWrongSelect();
             }
         } catch (IOException e) {
-            System.out.println("[System] 올바른 입력 형식이 아닙니다. 다시 입력해주세요. \n");
+            consoleIo.printIOException();
         }
 
         run();
@@ -82,86 +83,64 @@ public class LibraryManagementController {
 
     private void addBook() throws IOException {
 
-        System.out.println("[System] 도서 등록 메뉴로 넘어갑니다. \n");
+        BookRequestDto bookRequestDto = consoleIo.addBookRequest();
 
-        System.out.println("Q. 등록할 도서 제목을 입력하세요. \n");
-        String title = consoleIo.getInputString();
+        libraryManagementService.addBook(bookRequestDto.getTitle(), bookRequestDto.getAuthor(), bookRequestDto.getPage());
 
-        System.out.println("Q. 작가 이름을 입력하세요. \n");
-        String author = consoleIo.getInputString();
-
-        System.out.println("Q. 페이지 수를 입력하세요. \n");
-        int page = consoleIo.getInputInt();
-
-        libraryManagementService.addBook(title, author, page);
-
-        System.out.println("[System] 도서 등록이 완료되었습니다. \n");
+        consoleIo.addBookResponse();
     }
 
     private void getAllBooks() {
 
-        System.out.println("[System] 전체 도서 목록입니다. \n");
+        consoleIo.getAllBooksRequest();
 
         List<Book> bookList = libraryManagementService.getAllBooks();
-        consoleIo.printBookInfo(bookList);
 
-        System.out.println("[System] 도서 목록 끝 \n");
+        consoleIo.getAllBooksResponse(bookList);
     }
 
     private void getBookByTitle() throws IOException {
 
-        System.out.println("[System] 제목으로 도서 검색 메뉴로 넘어갑니다. \n");
-
-        System.out.println("Q. 검색할 도서 제목 일부를 입력하세요. \n");
-        String title = consoleIo.getInputString();
+        String title = consoleIo.getBookByTitleRequest();
 
         List<Book> bookList = libraryManagementService.getBookByTitle(title);
-        consoleIo.printBookInfo(bookList);
 
-        System.out.println("[System] 검색된 도서 끝 \n");
+        consoleIo.getBookByTitleResponse(bookList);
     }
 
     private void rentBook() throws IOException {
 
-        System.out.println("[System] 도서 대여 메뉴로 넘어갑니다. \n");
-
-        System.out.println("Q. 대여할 도서번호를 입력하세요. \n");
-        Long id = consoleIo.getInputLong();
+        Long id = consoleIo.rentBookRequest();
 
         Status result = libraryManagementService.rentBook(id);
+
         consoleIo.rentBookResponse(result);
     }
 
     private void returnBook() throws IOException {
 
-        System.out.println("[System] 도서 반납 메뉴로 넘어갑니다. \n");
-
-        System.out.println("Q. 반납할 도서번호를 입력하세요. \n");
-        Long id = consoleIo.getInputLong();
+        Long id = consoleIo.returnBookRequest();
 
         ReturnResult result = libraryManagementService.returnBook(id);
+
         consoleIo.returnBookResponse(result);
     }
 
     private void lostBook() throws IOException {
 
-        System.out.println("[System] 도서 분실 처리 메뉴로 넘어갑니다. \n");
-
-        System.out.println("Q. 분실 처리할 도서번호를 입력하세요. \n");
-        Long id = consoleIo.getInputLong();
+        Long id = consoleIo.lostBookRequest();
 
         Boolean result = libraryManagementService.lostBook(id);
+
         consoleIo.lostBookResponse(result);
     }
 
     private void deleteBook() throws IOException {
 
-        System.out.println("[System] 도서 삭제 처리 메뉴로 넘어갑니다. \n");
-
-        System.out.println("Q. 삭제 처리할 도서번호를 입력하세요. \n");
-        Long id = consoleIo.getInputLong();
+        Long id = consoleIo.deleteBookRequest();
 
         Boolean result = libraryManagementService.deleteBook(id);
+
         consoleIo.deleteBookResponse(result);
     }
 }
--- src/main/java/com/programmers/librarymanagement/utils/ConsoleIo.java
+++ src/main/java/com/programmers/librarymanagement/utils/ConsoleIo.java
@@ -3,6 +3,7 @@ package com.programmers.librarymanagement.utils;
 import com.programmers.librarymanagement.domain.Book;
 import com.programmers.librarymanagement.domain.ReturnResult;
 import com.programmers.librarymanagement.domain.Status;
+import com.programmers.librarymanagement.dto.BookRequestDto;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -23,6 +24,14 @@ public class ConsoleIo {
                 "2. 테스트 모드 \n");
     }
 
+    public void printNormalMode() {
+        System.out.println("[System] 일반 모드로 애플리케이션을 실행합니다. \n");
+    }
+
+    public void printTestMode() {
+        System.out.println("[System] 테스트 모드로 애플리케이션을 실행합니다. \n");
+    }
+
     public void printSelectFun() {
         System.out.println("Q. 사용할 기능을 선택해주세요. \n" +
                 "1. 도서 등록 \n" +
@@ -32,6 +41,70 @@ public class ConsoleIo {
                 "5. 도서 반납 \n" +
                 "6. 도서 분실 \n" +
                 "7. 도서 삭제 \n");
+    }
+
+    public void printWrongSelect() {
+        System.out.println("[System] 존재하지 않는 선택지입니다. 다시 입력해주세요. \n");
+    }
+
+    public void printIOException() {
+        System.out.println("[System] 올바른 입력 형식이 아닙니다. 다시 입력해주세요. \n");
+    }
+
+    public BookRequestDto addBookRequest() throws IOException {
+
+        System.out.println("[System] 도서 등록 메뉴로 넘어갑니다. \n");
+
+        System.out.println("Q. 등록할 도서 제목을 입력하세요. \n");
+        String title = this.getInputString();
+
+        System.out.println("Q. 작가 이름을 입력하세요. \n");
+        String author = this.getInputString();
+
+        System.out.println("Q. 페이지 수를 입력하세요. \n");
+        int page = this.getInputInt();
+
+        return new BookRequestDto(title, author, page);
+    }
+
+    public void addBookResponse() {
+        System.out.println("[System] 도서 등록이 완료되었습니다. \n");
+    }
+
+    public void getAllBooksRequest() {
+        System.out.println("[System] 전체 도서 목록입니다. \n");
+    }
+
+    public void getAllBooksResponse(List<Book> bookList) {
+
+        this.printBookInfo(bookList);
+
+        System.out.println("[System] 도서 목록 끝 \n");
+    }
+
+    public String getBookByTitleRequest() throws IOException {
+
+        System.out.println("[System] 제목으로 도서 검색 메뉴로 넘어갑니다. \n");
+
+        System.out.println("Q. 검색할 도서 제목 일부를 입력하세요. \n");
+
+        return this.getInputString();
+    }
+
+    public void getBookByTitleResponse(List<Book> bookList) {
+
+        this.printBookInfo(bookList);
+
+        System.out.println("[System] 검색된 도서 끝 \n");
+    }
+
+    public Long rentBookRequest() throws IOException {
+
+        System.out.println("[System] 도서 대여 메뉴로 넘어갑니다. \n");
+
+        System.out.println("Q. 대여할 도서번호를 입력하세요. \n");
+
+        return this.getInputLong();
     }
 
     public void rentBookResponse(Status result) {
@@ -47,6 +120,15 @@ public class ConsoleIo {
         }
     }
 
+    public Long returnBookRequest() throws IOException {
+
+        System.out.println("[System] 도서 반납 메뉴로 넘어갑니다. \n");
+
+        System.out.println("Q. 반납할 도서번호를 입력하세요. \n");
+
+        return this.getInputLong();
+    }
+
     public void returnBookResponse(ReturnResult result) {
 
         switch (result) {
@@ -58,6 +140,15 @@ public class ConsoleIo {
         }
     }
 
+    public Long lostBookRequest() throws IOException {
+
+        System.out.println("[System] 도서 분실 처리 메뉴로 넘어갑니다. \n");
+
+        System.out.println("Q. 분실 처리할 도서번호를 입력하세요. \n");
+
+        return this.getInputLong();
+    }
+
     public void lostBookResponse(Boolean result) {
 
         if (result) {
@@ -65,6 +156,15 @@ public class ConsoleIo {
         } else {
             System.out.println("[System] 이미 분실 처리된 도서입니다. \n");
         }
+    }
+
+    public Long deleteBookRequest() throws IOException {
+
+        System.out.println("[System] 도서 삭제 처리 메뉴로 넘어갑니다. \n");
+
+        System.out.println("Q. 삭제 처리할 도서번호를 입력하세요. \n");
+
+        return this.getInputLong();
     }
 
     public void deleteBookResponse(Boolean result) {
@@ -76,7 +176,7 @@ public class ConsoleIo {
         }
     }
 
-    public void printBookInfo(List<Book> bookList) {
+    private void printBookInfo(List<Book> bookList) {
 
         for (Book book : bookList) {
             System.out.println("도서번호 : " + book.getId() + "\n"
@@ -89,7 +189,7 @@ public class ConsoleIo {
         }
     }
 
-    public String getInputString() throws IOException {
+    private String getInputString() throws IOException {
 
         System.out.print("> ");
 
@@ -137,7 +237,7 @@ public class ConsoleIo {
         return Integer.parseInt(input);
     }
 
-    public Long getInputLong() throws IOException {
+    private Long getInputLong() throws IOException {
 
         System.out.print("> ");
 
