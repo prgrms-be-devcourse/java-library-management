8473d2de29b3331ec1a8d496434766dcf5fa6ac9
hyoguoo <hyoguoo@gmail.com>
2023-10-04 23:19:43 +0900

    refactor: parameterized test와 MethodSource를 이용하여 중복 코드 제거

--- src/test/java/library/domain/BookTest.java
+++ src/test/java/library/domain/BookTest.java
@@ -1,10 +1,15 @@
 package library.domain;
 
+import library.exception.BookErrorMessage;
 import library.exception.BookException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.time.LocalDateTime;
+import java.util.stream.Stream;
 
 import static library.domain.BookStatus.*;
 import static library.exception.BookErrorMessage.*;
@@ -12,6 +17,36 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class BookTest {
+
+    private static Stream<Arguments> provideBookStatusForRentTest() {
+        return Stream.of(
+                Arguments.of(RENTED, null, BOOK_ALREADY_RENTED),
+                Arguments.of(IN_CLEANUP, LocalDateTime.now(), BOOK_IN_CLEANUP),
+                Arguments.of(LOST, null, BOOK_LOST)
+        );
+    }
+
+    private static Stream<Arguments> provideBookStatusForReturnTest() {
+        return Stream.of(
+                Arguments.of(AVAILABLE, null, BOOK_ALREADY_AVAILABLE),
+                Arguments.of(IN_CLEANUP, LocalDateTime.now(), BOOK_IN_CLEANUP)
+        );
+    }
+
+    private static Stream<Arguments> provideReturnTimeForRentTest() {
+        return Stream.of(
+                Arguments.of(LocalDateTime.now().minusMinutes(5), AVAILABLE),
+                Arguments.of(LocalDateTime.now().minusMinutes(3), IN_CLEANUP)
+        );
+    }
+
+    private static Stream<Arguments> provideBookStatusForLostTest() {
+        return Stream.of(
+                Arguments.of(AVAILABLE),
+                Arguments.of(IN_CLEANUP),
+                Arguments.of(RENTED)
+        );
+    }
 
     @Test
     @DisplayName("도서가 '대여 가능' 상태 일 때만 도서를 대여할 수 있고, 도서가 대여되면 상태를 '대여중'으로 바꿔야합니다.")
@@ -26,21 +61,16 @@ class BookTest {
         assertThat(availableBook.getStatus()).isEqualTo(RENTED);
     }
 
-    @Test
     @DisplayName("도서가 '대여 가능' 상태가 아닌 경우 해당 도서를 대여할 수 없는 이유에 대해 알려줘야합니다.")
-    void test_givenBookNotAvailable_whenRent_thenThrowsException() {
+    @ParameterizedTest(name = "도서가 {0} 상태 일 때는 도서를 대여할 수 없습니다. 이때 도서를 대여할 수 없는 이유는 {1}입니다.")
+    @MethodSource("provideBookStatusForRentTest")
+    void test_givenBookStatus_whenRent_thenThrowsException(BookStatus bookStatus, LocalDateTime returnDueDate, BookErrorMessage bookErrorMessage) {
         // Given
-        Book rentedBook = Book.createBook(1L, "Title1", "Author1", 59, null, RENTED);
-        Book inCleanUpBook = Book.createBook(2L, "Title2", "Author2", 59, LocalDateTime.now(), IN_CLEANUP);
-        Book lostBook = Book.createBook(3L, "Title3", "Author3", 59, null, LOST);
+        Book book = Book.createBook(1L, "Title", "Author", 1, returnDueDate, bookStatus);
 
         // When, Then
-        assertThat(assertThrows(BookException.class, rentedBook::toRent).getMessage())
-                .isEqualTo(BOOK_ALREADY_RENTED.getMessage());
-        assertThat(assertThrows(BookException.class, inCleanUpBook::toRent).getMessage())
-                .isEqualTo(BOOK_IN_CLEANUP.getMessage());
-        assertThat(assertThrows(BookException.class, lostBook::toRent).getMessage())
-                .isEqualTo(BOOK_LOST.getMessage());
+        assertThat(assertThrows(BookException.class, book::toRent).getMessage())
+                .isEqualTo(bookErrorMessage.getMessage());
     }
 
     @Test
@@ -56,47 +86,39 @@ class BookTest {
         assertThat(book.getStatus()).isEqualTo(IN_CLEANUP);
     }
 
-    @Test
     @DisplayName("도서가 대여가능/정리 중 상태인 경우 반납할 수 없는 이유에 대해 알려줘야합니다.")
-    void test_givenBookNotRented_whenReturn_thenThrowsException() {
+    @ParameterizedTest(name = "도서가 {0} 상태 일 때는 도서를 반납할 수 없습니다. 이때 도서를 반납할 수 없는 이유는 {1}입니다.")
+    @MethodSource("provideBookStatusForReturnTest")
+    void test_givenBookStatus_whenReturn_thenThrowsException(BookStatus bookStatus, LocalDateTime returnDueDate, BookErrorMessage bookErrorMessage) {
         // Given
-        Book availableBook = Book.createBook(1L, "Title2", "Author2", 59, null, AVAILABLE);
-        Book inCleanUpBook = Book.createBook(2L, "Title2", "Author2", 59, LocalDateTime.now(), IN_CLEANUP);
+        Book book = Book.createBook(1L, "Title", "Author", 1, returnDueDate, bookStatus);
 
         // When, Then
-        assertThat(assertThrows(BookException.class, availableBook::toReturn).getMessage())
-                .isEqualTo(BOOK_ALREADY_AVAILABLE.getMessage());
-        assertThat(assertThrows(BookException.class, inCleanUpBook::toReturn).getMessage())
-                .isEqualTo(BOOK_IN_CLEANUP.getMessage());
+        assertThat(assertThrows(BookException.class, book::toReturn).getMessage())
+                .isEqualTo(bookErrorMessage.getMessage());
     }
 
-    @Test
     @DisplayName("'도서 정리중' 상태에서 5분이 지난 도서는 '대여 가능'으로 바뀌어야합니다.")
-    void test_givenBookInCleanUp_when5MinutesPassed_thenGoesForAvailable() {
+    @ParameterizedTest(name = "도서가 {0} 상태이고, 반납 예정일이 {1}일 때, 5분이 지나면 도서의 상태는 {2}이어야 합니다.")
+    @MethodSource("provideReturnTimeForRentTest")
+    void test_givenBookInCleanUp_when5MinutesPassed_thenGoesForAvailable(LocalDateTime returnDateTime, BookStatus expectedBookStatus) {
         // Given
-        Book afterThan5Minutes = Book.createBook(1L, "Title1", "Author1", 59, LocalDateTime.now().minusMinutes(5).minusSeconds(1), IN_CLEANUP);
-        Book afterThan3Minutes = Book.createBook(2L, "Title2", "Author2", 59, LocalDateTime.now().minusMinutes(3), IN_CLEANUP);
+        Book book = Book.createBook(1L, "Title1", "Author1", 59, returnDateTime, IN_CLEANUP);
 
         // When, Then
-        assertThat(afterThan5Minutes.getStatus()).isEqualTo(AVAILABLE);
-        assertThat(afterThan3Minutes.getStatus()).isEqualTo(IN_CLEANUP);
+        assertThat(book.getStatus()).isEqualTo(expectedBookStatus);
     }
 
-    @Test
     @DisplayName("도서를 분실 처리하면 도서는 '분실됨' 상태가 되어야합니다.")
-    void test_givenBook_whenLost_thenGoesForLost() {
+    @ParameterizedTest(name = "도서가 {0} 상태 일 때는 도서를 분실 처리할 수 있습니다.")
+    @MethodSource("provideBookStatusForLostTest")
+    void test_givenBook_whenLost_thenGoesForLost(BookStatus bookStatus) {
         // Given
-        Book availableBook = Book.createBook(1L, "Title1", "Author1", 59, null, AVAILABLE);
-        Book inCleanUpBook = Book.createBook(2L, "Title2", "Author2", 59, null, IN_CLEANUP);
-        Book rentedBook = Book.createBook(3L, "Title3", "Author3", 59, null, RENTED);
+        Book book = Book.createBook(1L, "Title1", "Author1", 59, null, bookStatus);
 
         // When, Then
-        availableBook.toLost();
-        assertThat(availableBook.getStatus()).isEqualTo(LOST);
-        inCleanUpBook.toLost();
-        assertThat(inCleanUpBook.getStatus()).isEqualTo(LOST);
-        rentedBook.toLost();
-        assertThat(rentedBook.getStatus()).isEqualTo(LOST);
+        book.toLost();
+        assertThat(book.getStatus()).isEqualTo(LOST);
     }
 
     @Test
