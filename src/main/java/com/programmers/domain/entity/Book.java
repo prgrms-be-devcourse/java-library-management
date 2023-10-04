084ecdede10d435e694b6e6813c5f005aad629c9
happyjamy <78072370+happyjamy@users.noreply.github.com>
2023-10-04 15:26:13 +0900

    [Feat] Book 도메인 추가

--- src/main/java/com/programmers/domain/entity/Book.java
+++ src/main/java/com/programmers/domain/entity/Book.java
@@ -1,16 +1,27 @@
 package com.programmers.domain.entity;
 
+import com.programmers.config.DependencyInjector;
 import com.programmers.domain.enums.BookStatus;
-import com.programmers.util.UUIDGenerator;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class Book {
-    private final String id = UUIDGenerator.generateUUID();
+
+    private Long id;
     private String title;
     private String author;
     private BookStatus status;
     private Integer pages;
-    private boolean available;
-    private boolean lost;
+
+    @Builder
+    private Book(Long id,String title, String author, BookStatus status, Integer pages) {
+        this.id = DependencyInjector.getInstance().getIdGenerator().generateId();
+        this.title = title;
+        this.author = author;
+        this.status = status;
+        this.pages = pages;
+    }
 }
--- src/main/java/com/programmers/domain/enums/BookStatus.java
+++ src/main/java/com/programmers/domain/enums/BookStatus.java
@@ -1,8 +1,18 @@
 package com.programmers.domain.enums;
 
 public enum BookStatus {
-    AVAILABLE,
-    RENTED,
-    ORGANIZING,
-    LOST
+    AVAILABLE("대여 가능"),
+    RENTED("대여 중"),
+    ORGANIZING("정리 중"),
+    LOST("분실");
+
+    private final String name;
+
+    BookStatus(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
