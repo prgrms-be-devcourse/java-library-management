8fcf28ab71b8b971795467ba10acecc6f3182939
happyjamy <78072370+happyjamy@users.noreply.github.com>
2023-10-04 15:53:58 +0900

    [Del] 사용하지 않는 파일 정리

--- src/main/java/com/programmers/presentation/UserInteraction.java
+++ src/main/java/com/programmers/presentation/UserInteraction.java
@@ -3,4 +3,6 @@ package com.programmers.presentation;
 public interface UserInteraction{
     void displayMessage(String message);
     String collectUserInput();
+    Long collectUserLongInput();
+    Integer collectUserIntegerInput();
 }
--- src/main/java/com/programmers/util/UUIDGenerator.java
+++
@@ -1,7 +0,0 @@
-package com.programmers.util;
-
-public class UUIDGenerator {
-    public static String generateUUID() {
-        return java.util.UUID.randomUUID().toString();
-    }
-}
