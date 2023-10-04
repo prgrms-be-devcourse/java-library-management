062cd982c63786d4dc940e5dbc8015b8609bb979
Hanna <hanna@trdst.com>
2023-10-04 19:28:46 +0900

    chore: JsonBookDto 필요없는 생성자, setter 제거

--- app/src/main/java/com/programmers/library/dto/JsonBookDto.java
+++ app/src/main/java/com/programmers/library/dto/JsonBookDto.java
@@ -12,54 +12,27 @@ public class JsonBookDto {
     private BookStatus status;
     private LocalDateTime returnedAt;
 
-    public JsonBookDto() {
-    }
-
     public int getId() {
         return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getAuthor() {
         return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
     }
 
     public int getPageCount() {
         return pageCount;
     }
 
-    public void setPageCount(int pageCount) {
-        this.pageCount = pageCount;
-    }
-
     public BookStatus getStatus() {
         return status;
     }
 
-    public void setStatus(BookStatus status) {
-        this.status = status;
-    }
-
     public LocalDateTime getReturnedAt() {
         return returnedAt;
-    }
-
-    public void setReturnedAt(LocalDateTime returnedAt) {
-        this.returnedAt = returnedAt;
     }
 }
