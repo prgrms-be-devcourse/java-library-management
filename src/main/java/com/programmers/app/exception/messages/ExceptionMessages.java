d8e368b45d0570c48daa5f1ad1354a710a794e5f
Jay <injaesong0@gmail.com>
2023-10-04 11:48:08 +0900

    refactor: edit custom exception messages to be clear

--- src/main/java/com/programmers/app/exception/messages/ExceptionMessages.java
+++ src/main/java/com/programmers/app/exception/messages/ExceptionMessages.java
@@ -3,7 +3,7 @@ package com.programmers.app.exception.messages;
 public enum ExceptionMessages {
 
     //book not found
-    NO_BOOK_LOADED("[System] 보관된 책이 없습니다."),
+    NO_BOOK_STORED("[System] 보관된 책이 없습니다."),
     TITLE_NONEXISTENT("[System] 존재하지 않는 제목입니다."),
     BOOK_NUMBER_NONEXISTENT("[System] 존재하지 않는 도서번호 입니다."),
 
@@ -11,6 +11,7 @@ public enum ExceptionMessages {
     ALREADY_BORROWED("[System] 이미 대여중인 도서입니다."),
     BOOK_LOSS_REPORTED("[System] 분실 처리된 도서입니다."),
     BOOK_ON_ARRANGEMENT("[System] 정리중인 도서입니다."),
+    BOOK_IN_PLACE("[System] 대여 가능한 도서입니다."),
     ALREADY_RETURNED("[System] 이미 반납 처리된 도서입니다."),
     ALREADY_REPORTED_LOST("[System] 이미 분실 처리된 도서입니다."),
 
