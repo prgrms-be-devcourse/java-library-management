2cf058871a02efc8c9b5087badce16656d877683
voidmelody <sss9073@naver.com>
2023-10-04 09:30:02 +0900

    Refactor: "Executor 이름 수정"

--- src/main/java/org/library/entity/Func.java
+++ src/main/java/org/library/entity/Func.java
@@ -1,35 +1,35 @@
 package org.library.entity;
 
 import org.library.error.InvalidFuncError;
-import org.library.utils.Executor;
+import org.library.controller.Controller;
 
 import java.util.Arrays;
 import java.util.function.Consumer;
 
 public enum Func {
-    ONE(1, "도서 등록", Executor::register),
-    TWO(2, "전체 도서 목록 조회", Executor::showAll),
-    THREE(3, "제목으로 도서 검색", Executor::findBookByTitle),
-    FOUR(4, "도서 대여", Executor::rent),
-    FIVE(5, "도서 반납", Executor::returns),
-    SIX(6, "도서 분실", Executor::reportLost),
-    SEVEN(7, "도서 삭제", Executor::delete),
-    EIGHT(8, "프로그램 종료", Executor::exit);
+    ONE(1, "도서 등록", Controller::register),
+    TWO(2, "전체 도서 목록 조회", Controller::showAll),
+    THREE(3, "제목으로 도서 검색", Controller::findBookByTitle),
+    FOUR(4, "도서 대여", Controller::rent),
+    FIVE(5, "도서 반납", Controller::returns),
+    SIX(6, "도서 분실", Controller::reportLost),
+    SEVEN(7, "도서 삭제", Controller::delete),
+    EIGHT(8, "프로그램 종료", Controller::exit);
 
 
     private final int value;
     private final String name;
-    private final Consumer<Executor> func;
+    private final Consumer<Controller> func;
 
 
-    Func(int value, String name, Consumer<Executor> func) {
+    Func(int value, String name, Consumer<Controller> func) {
         this.value = value;
         this.name = name;
         this.func = func;
     }
 
-    public void call(Executor executor){
-        this.func.accept(executor);
+    public void call(Controller controller){
+        this.func.accept(controller);
     }
 
     public boolean isValueEqual(int value){
