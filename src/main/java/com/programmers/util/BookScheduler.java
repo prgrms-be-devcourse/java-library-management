582ed79da4afb56734198fab1980b74eb861fc79
happyjamy <78072370+happyjamy@users.noreply.github.com>
2023-10-04 15:48:34 +0900

    [Feat] 5분 정리를 위한 스케쥴러

---
+++ src/main/java/com/programmers/util/BookScheduler.java
@@ -0,0 +1,22 @@
+package com.programmers.util;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class BookScheduler {
+
+    private final ScheduledExecutorService scheduler;
+    private final int BOOK_ORGANIZING_TIME = 1000 * 60 * 5;
+
+    public BookScheduler() {
+        this(Executors.newScheduledThreadPool(100));
+    }
+
+    public void scheduleBookAvailableTask(Runnable task) {
+        scheduler.schedule(task, BOOK_ORGANIZING_TIME, TimeUnit.MILLISECONDS);
+    }
+}
+
