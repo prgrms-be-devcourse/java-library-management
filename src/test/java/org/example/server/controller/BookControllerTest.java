497a6d39da224f2449ede15e424a8712c044e635
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-10-05 02:09:53 +0900

    chore: mockito 추가

---
+++ src/test/java/org/example/server/controller/BookControllerTest.java
@@ -0,0 +1,11 @@
+package org.example.server.controller;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith()
+class BookControllerTest {
+
+}
