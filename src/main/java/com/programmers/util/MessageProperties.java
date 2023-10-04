a6b5e7c5d212c264e3b4dfc8dace7dd31a3fa6bf
happyjamy <78072370+happyjamy@users.noreply.github.com>
2023-10-04 15:49:04 +0900

    [Feat] 메세지 상수

--- src/main/java/com/programmers/util/MessageProperties.java
+++ src/main/java/com/programmers/util/MessageProperties.java
@@ -1,5 +1,7 @@
 package com.programmers.util;
 
+import com.programmers.exception.unchecked.SystemErrorException;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -7,40 +9,47 @@ import java.nio.charset.StandardCharsets;
 import java.util.Properties;
 
 public class MessageProperties {
+
     private static final Properties errorProperties = new Properties();
     private static final Properties properties = new Properties();
 
     //TODO: 파일이름 환경변수 설정
     static {
-        try (InputStream input = MessageProperties.class.getResourceAsStream("/messages.properties")) {
+        try (InputStream input = MessageProperties.class.getResourceAsStream(
+            "/messages.properties")) {
             if (input != null) {
                 properties.load(new InputStreamReader(input, StandardCharsets.UTF_8));
             } else {
-                System.err.println("messages.properties 파일이 존재하지 않습니다.");
-                System.exit(-1);
+                throw new SystemErrorException();
             }
         } catch (IOException e) {
-            //TODO: 예외처리
+            throw new SystemErrorException();
         }
 
-        try (InputStream input = MessageProperties.class.getResourceAsStream("/error_messages.properties")) {
+        try (InputStream input = MessageProperties.class.getResourceAsStream(
+            "/error_messages.properties")) {
             if (input != null) {
                 errorProperties.load(new InputStreamReader(input, StandardCharsets.UTF_8));
             } else {
-                System.err.println("error_messages.properties 파일이 존재하지 않습니다.");
-                System.exit(-1);
+                throw new SystemErrorException();
             }
         } catch (IOException e) {
-            //TODO: 예외처리
+            throw new SystemErrorException();
         }
     }
 
+    private MessageProperties() {
+    }
+
+    // TODO: 해당 class 로 메세지를 가져오기 때문에 여기서는 상수 사용 불가.
     public static String get(String key) {
-        return properties.getProperty(key, "해당 키가 존재하지 않습니다: " + key);
+        String value = properties.getProperty(key, "메세지 가져오기에 실패 하였습니다.");
+        return value.replace("\n", System.lineSeparator());
     }
 
     public static String getError(String key) {
-        return errorProperties.getProperty(key, "해당 키가 존재하지 않습니다: " + key);
+        String value = errorProperties.getProperty(key, "메세지 가져오기에 실패 하였습니다.");
+        return value.replace("\n", System.lineSeparator());
     }
 }
 
