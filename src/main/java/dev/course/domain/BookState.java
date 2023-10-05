ee4b57e695a104fdaf0c6629e6589a5c57c7a1c2
JIN-076 <andy230@naver.com>
2023-10-05 23:38:56 +0900

    update : 핸들러 로직에서 BookRepository 의존성 제거. 핸들러 로직에서 예외 발생만을 담당하도록 메서드 수정

--- src/main/java/dev/course/domain/BookState.java
+++ src/main/java/dev/course/domain/BookState.java
@@ -9,67 +9,53 @@ public enum BookState {
 
     RENTAL_AVAILABLE("대여 가능") {
         @Override
-        public void handleBorrow(BookRepository bookRepository, Book book) {
-            bookRepository.update(book, RENTING);
-        }
+        public void handleBorrow(Book book) {}
 
         @Override
-        public Book handleReturn(BookRepository bookRepository, Book book) {
+        public void handleReturn(Book book) {
             throw new FuncFailureException("[System] 대여 가능한 도서로 반납이 불가합니다.\n");
         }
 
         @Override
-        public void handleLost(BookRepository bookRepository, Book book) {
-            bookRepository.update(book, LOST);
-        }
+        public void handleLost(Book book) {}
     },
     RENTING("대여중") {
         @Override
-        public void handleBorrow(BookRepository bookRepository, Book book) {
+        public void handleBorrow(Book book) {
             throw new FuncFailureException("[System] 대여중인 도서로 대여가 불가합니다.\n");
         }
 
         @Override
-        public Book handleReturn(BookRepository bookRepository, Book book) {
-            bookRepository.update(book, ARRANGEMENT);
-            return book;
-        }
+        public void handleReturn(Book book) {}
 
         @Override
-        public void handleLost(BookRepository bookRepository, Book book) {
-            bookRepository.update(book, LOST);
-        }
+        public void handleLost(Book book) {}
     },
     ARRANGEMENT("도서 정리중") {
         @Override
-        public void handleBorrow(BookRepository bookRepository, Book book) {
+        public void handleBorrow(Book book) {
             throw new FuncFailureException("[System] 정리중인 도서로 대여가 불가합니다.\n");
         }
 
         @Override
-        public Book handleReturn(BookRepository bookRepository, Book book) {
+        public void handleReturn(Book book) {
             throw new FuncFailureException("[System] 정리중인 도서로 반납이 불가합니다.\n");
         }
 
         @Override
-        public void handleLost(BookRepository bookRepository, Book book) {
-            bookRepository.update(book, LOST);
-        }
+        public void handleLost(Book book) {}
     },
     LOST("분실됨") {
         @Override
-        public void handleBorrow(BookRepository bookRepository, Book book) {
+        public void handleBorrow(Book book) {
             throw new FuncFailureException("[System] 분실된 도서로 대여가 불가합니다.\n");
         }
 
         @Override
-        public Book handleReturn(BookRepository bookRepository, Book book) {
-            bookRepository.update(book, ARRANGEMENT);
-            return book;
-        }
+        public void handleReturn(Book book) {}
 
         @Override
-        public void handleLost(BookRepository bookRepository, Book book) {
+        public void handleLost(Book book) {
             throw new FuncFailureException("[System] 이미 분실된 도서입니다.\n");
         }
     };
@@ -91,8 +77,8 @@ public enum BookState {
                 .orElse(null);
     }
 
-    public abstract void handleBorrow(BookRepository bookRepository, Book book);
-    public abstract Book handleReturn(BookRepository bookRepository, Book book);
-    public abstract void handleLost(BookRepository bookRepository, Book book);
+    public abstract void handleBorrow(Book book);
+    public abstract void handleReturn(Book book);
+    public abstract void handleLost(Book book);
 }
 
