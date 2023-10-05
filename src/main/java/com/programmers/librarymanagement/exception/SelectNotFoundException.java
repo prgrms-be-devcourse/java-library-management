157f4e249d9f3806bea6c75e417864828a48995e
letskuku <yujung9288@naver.com>
2023-10-06 04:05:30 +0900

    refactor: 필요없는 파일 삭제

--- src/main/java/com/programmers/librarymanagement/exception/SelectNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.programmers.librarymanagement.exception;
-
-public class SelectNotFoundException extends RuntimeException {
-
-    public SelectNotFoundException() {
-        super("존재하지 않는 선택지입니다.");
-    }
-}
