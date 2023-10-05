80602eff464a5d2f76331d0fd0c17744f1ec6d1a
uijin-j <uijin0531@gmail.com>
2023-10-05 12:45:12 +0900

    refactor: 도서 상태 변경 가능 검사 로직 수정

--- library-management/src/main/java/devcourse/backend/model/BookStatus.java
+++ library-management/src/main/java/devcourse/backend/model/BookStatus.java
@@ -2,14 +2,20 @@ package devcourse.backend.model;
 
 import java.util.Arrays;
 import java.util.Optional;
+import java.util.function.Predicate;
 
 public enum BookStatus {
-    AVAILABLE("대여 가능"), BORROWED("대여 중"), ARRANGING("도서 정리 중"), LOST("분실됨");
+    AVAILABLE("대여 가능", (after) -> after == "BORROWED" || after == "LOST"),
+    BORROWED("대여 중", (after) -> after == "ARRANGING" || after == "LOST"),
+    ARRANGING("도서 정리 중", (after) -> after == "AVAILABLE" || after == "LOST"),
+    LOST("분실됨", (after) -> after == "ARRANGING");
 
     private final String description;
+    private final Predicate<String> canSwitchTo;
 
-    BookStatus(String description) {
+    BookStatus(String description, Predicate<String> canSwitchTo) {
         this.description = description;
+        this.canSwitchTo = canSwitchTo;
     }
 
     @Override
@@ -23,23 +29,7 @@ public enum BookStatus {
                 .findAny().orElseThrow(() -> new IllegalArgumentException(description + "은 존재하지 않은 도서 상태입니다."));
     }
 
-    public final boolean canSwitch(BookStatus after) {
-        if (after == BORROWED) {
-            if (this == BookStatus.AVAILABLE) return true;
-        }
-
-        if (after == ARRANGING) {
-            if (this == BORROWED || this == LOST) return true;
-        }
-
-        if (after == AVAILABLE) {
-            if (this == ARRANGING) return true;
-        }
-
-        if (after == LOST) {
-            return (this == LOST) ? false : true;
-        }
-
-        return false;
+    public boolean canSwitch(BookStatus after) {
+        return this.canSwitchTo.test(after.name());
     }
 }
