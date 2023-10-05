18e4475bbbf2affc05b7fcd43191ae4af79369ef
uijin-j <uijin0531@gmail.com>
2023-10-05 15:14:02 +0900

    refactor: 도서 정리 스케쥴러 구현에 Timer 대신 ScheduledExecutorService 사용

--- library-management/src/main/java/devcourse/backend/business/BookOrganizingScheduler.java
+++ library-management/src/main/java/devcourse/backend/business/BookOrganizingScheduler.java
@@ -6,35 +6,41 @@ import devcourse.backend.repository.Repository;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 import static devcourse.backend.model.BookStatus.*;
 
 public class BookOrganizingScheduler {
     private final Repository repository;
-    private Timer timer;
     private boolean isRunning;
+    private ScheduledExecutorService scheduler;
 
     public BookOrganizingScheduler(Repository repository) {
-        timer = new Timer(true);
         this.repository = repository;
         isRunning = false;
+    }
+
+    public void startScheduler() {
+        if(!isRunning) {
+            isRunning = true;
+            scheduler = Executors.newSingleThreadScheduledExecutor(runnable -> {
+                Thread thread = new Thread(runnable);
+                thread.setDaemon(true);
+                return thread;
+            });
+
+            scheduler.scheduleAtFixedRate(new BookOrganizingTask(), 0, 60, TimeUnit.SECONDS);
+        }
     }
 
     public boolean isRunning() {
         return isRunning;
     }
 
-    public void startScheduler() {
-        if(!isRunning) {
-            isRunning = true;
-            timer.schedule(new BookOrganizingTask(), 0, 60 * 1000);
-        }
-    }
-
-    private class BookOrganizingTask extends TimerTask {
+    private class BookOrganizingTask implements Runnable {
         @Override
         public void run() {
             List<Book> booksToBeAvailable = repository.findAll()
@@ -45,12 +51,12 @@ public class BookOrganizingScheduler {
             long count = booksToBeAvailable.size();
             if (count == 0) {
                 isRunning = false;
-                return;
+                scheduler.shutdown();
             }
 
             LocalDateTime now = LocalDateTime.now(ZoneId.of("Asia/Seoul"));
             for (Book book : booksToBeAvailable) {
-                if (now.isAfter(book.getUpdateAt().plusMinutes(5L))) {
+                if (now.isAfter(book.getUpdateAt().plusMinutes(1L))) {
                     book.changeStatus(AVAILABLE);
                 }
             }
