2ef382ec1e165f398286d39d9d804c715622a6cf
EomYerim <djadpfa1002@naver.com>
2023-10-05 13:45:43 +0900

    feat: MessageType enum 대신 MessageProvider 클래스 생성

---
+++ src/main/java/com/programmers/library/utils/MessageProvider.java
@@ -0,0 +1,19 @@
+package com.programmers.library.utils;
+
+public class MessageProvider {
+    public static final String BOOK_ADDED = "\n[System] 도서 등록이 완료되었습니다.\n";
+    public static final String NO_BOOKS = "[System] 현재 등록된 도서가 없습니다.\n";
+    public static final String BOOK_LIST_START = "[System] 전체 도서 목록입니다.\n";
+    public static final String BOOK_LIST_END = "[System] 도서 목록 끝\n";
+    public static final String NO_SEARCH_BOOKS = "[System] 검색된 도서가 없습니다.\n";
+    public static final String SEARCH_RESULTS_END = "\n[System] 검색된 도서 끝\n";
+    public static final String INVALID_BOOK_ID = "\n[System] 존재하지 않는 도서번호 입니다.\n";
+    public static final String BOOK_RENTED = "\n[System] 도서가 대여 처리 되었습니다.\n";
+    public static final String ALREADY_RENTED = "\n[System] 이미 대여중인 도서입니다.\n";
+    public static final String UNAVAILABLE_FOR_RENT = "\n[System] 해당 도서는 대여가 불가능합니다.";
+    public static final String BOOK_RETURNED = "\n[System] 도서가 반납 처리 되었습니다.\n";
+    public static final String UNRETURNABLE = "\n[System] 해당 도서는 반납할 수 없습니다.";
+    public static final String BOOK_LOST = "\n[System] 도서가 분실 처리 되었습니다.\n";
+    public static final String ALREADY_LOST = "\n[System] 이미 분실 처리된 도서입니다.\n";
+    public static final String BOOK_DELETED = "\n[System] 도서가 삭제 처리 되었습니다.\n";
+}
