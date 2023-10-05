e3aaad3a654e95b56fdcccc8d47c9a6799b9ec4f
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-10-05 13:47:39 +0900

    test: 서비스 테스트 작성

---
+++ src/test/java/org/example/server/service/BasicBookServiceTest.java
@@ -0,0 +1,80 @@
+package org.example.server.service;
+
+import org.example.packet.BookDto;
+import org.example.server.entity.Book;
+import org.example.server.repository.Repository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class BasicBookServiceTest {
+    private Repository repository;
+    private BookService service;
+    private LinkedList<Book> emptyList;
+    private LinkedList<Book> notEmptyList;
+
+    @BeforeEach
+    @DisplayName("서비스 초기 설정")
+    void initService() {
+        repository = mock(Repository.class);
+        service = new BookService(repository);
+
+        emptyList = new LinkedList<>();
+        notEmptyList = new LinkedList<>();
+        for (int i = 0; i < 5; i++) {
+            notEmptyList.add(new Book(new BookDto("테스트책" + i, "이세희", i)));
+        }
+    }
+
+    @Test
+    @DisplayName("도서 등록: 성공")
+    void borrowAllIfExist() {
+        BookDto newBookDto = new BookDto("새로 나온 책", "이세희", 100);
+
+        service.register(newBookDto);
+    }
+
+    /* 도서 전체 조회 테스트 */
+    @Test
+    @DisplayName("책이 있을 때 전체 조회")
+    void readAllIfExist() {
+        when(repository.getAll()).thenReturn(notEmptyList);
+        LinkedList<BookDto> books = service.readAll();
+
+        assertEquals(books.size(), 5);
+    }
+
+    @Test
+    @DisplayName("책이 전혀 없을 때 전체 조회")
+    void readAllIfAllNotExist() {
+        when(repository.getAll()).thenReturn(emptyList);
+        LinkedList<BookDto> books = service.readAll();
+
+        assertEquals(books.size(), 0);
+    }
+
+    /* 도서 이름 검색 테스트 */
+    @Test
+    @DisplayName("해당 문자열을 가진 책이 있을 때 이름으로 검색")
+    void searchByNameIfContain() {
+        when(repository.getByName("테스트책")).thenReturn(notEmptyList);
+        LinkedList<BookDto> books = service.searchByName("테스트책");
+
+        assertEquals(books.size(), 5);
+    }
+
+    @Test
+    @DisplayName("해당 문자열을 가진 책이 없을 때 이름으로 검색")
+    void searchByNameIfNotContain() {
+        when(repository.getByName("없는 이름")).thenReturn(emptyList);
+        LinkedList<BookDto> books = service.searchByName("없는 이름");
+
+        assertEquals(books.size(), 0);
+    }
+}
---
+++ src/test/java/org/example/server/service/BorrowedBookServiceTest.java
@@ -0,0 +1,89 @@
+package org.example.server.service;
+
+import org.example.server.entity.Book;
+import org.example.server.entity.bookStatus.BookStatusType;
+import org.example.server.entity.bookStatus.BorrowedStatus;
+import org.example.server.exception.ServerException;
+import org.example.server.repository.Repository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class BorrowedBookServiceTest {
+    private Repository repository;
+    private BookService service;
+    private BookStatusType type;
+    private Book book;
+    private LinkedList<Book> data;
+
+    @BeforeEach
+    @DisplayName("서비스 초기화 및 대여중인 책 생성")
+    void initService() {
+        repository = mock(Repository.class);
+        service = new BookService(repository);
+
+        type = new BorrowedStatus().getType();
+        book = new Book(1, "테스트책2", "이세희", 100, type.name(), "");
+
+        data = new LinkedList<>();
+        data.add(book);
+    }
+
+
+    @Test
+    @DisplayName("대여중일 때 전체 조회")
+    void readAllIfExist() {
+        when(repository.getAll()).thenReturn(data);
+
+        assertEquals(service.readAll().get(0).status, type.NAME_KOR);
+    }
+
+    @Test
+    @DisplayName("대여중일 때 이름으로 검색")
+    void searchByNameIfContain() {
+        when(repository.getByName("테스트책")).thenReturn(data);
+
+        assertEquals(service.searchByName("테스트책").get(0).status, type.NAME_KOR);
+    }
+
+    @Test
+    @DisplayName("대여중인데 대여하기: 실패")
+    void borrowIfBorrowed() {
+        when(repository.findById(1)).thenReturn(book);
+
+        assertThrows(ServerException.class, () -> {
+            service.borrow(1);
+        });
+    }
+
+    @Test
+    @DisplayName("대여중인데 반납: 성공")
+    void restoreIfBorrowed() {
+        when(repository.findById(1)).thenReturn(book);
+
+        service.restore(1);
+    }
+
+    @Test
+    @DisplayName("대여중인데 분실: 성공")
+    void lostIfBorrowed() {
+        when(repository.findById(1)).thenReturn(book);
+
+        service.lost(1);
+    }
+
+    @Test
+    @DisplayName("대여중인데 삭제: 성공")
+    void deleteIfBorrowed() {
+        when(repository.findById(1)).thenReturn(book);
+
+        service.delete(1);
+    }
+}
