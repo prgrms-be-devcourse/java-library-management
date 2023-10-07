325151f34641c2d4065747775f610c1ac74e3c87
ebcho <joeunbi514@naver.com>
2023-10-07 23:32:17 +0900

    fix: 싱글톤 구현 간소화

--- src/main/java/com/programmers/library/util/IdGenerator.java
+++ src/main/java/com/programmers/library/util/IdGenerator.java
@@ -3,23 +3,14 @@ package com.programmers.library.util;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class IdGenerator {
-	private static volatile IdGenerator instance;
+	private static final IdGenerator instance = new IdGenerator();
 	private final AtomicLong id = new AtomicLong(0L);
 
 	private IdGenerator() {
-		if (instance != null) {
-			throw new RuntimeException("싱글톤 객체이므로 new 키워드를 사용할 수 없습니다. getInstance() 메소드를 사용하세요.");
-		}
+		throw new RuntimeException("싱글톤 객체이므로 new 키워드를 사용할 수 없습니다. getInstance() 메소드를 사용하세요.");
 	}
 
 	public static IdGenerator getInstance() {
-		if (instance == null) {
-			synchronized (IdGenerator.class) {
-				if (instance == null) { // double checked locking
-					instance = new IdGenerator();
-				}
-			}
-		}
 		return instance;
 	}
 
