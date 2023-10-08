8d5a2fcd94f10859a120e0834ea1224f76f37361
수지니 <sssssjin99@naver.com>
2023-10-08 16:37:45 +0900

    [refactor]: Service layer 에서 동시요청을 고려해 리팩토링

--- src/main/java/app/library/management/core/service/BookService.java
+++ src/main/java/app/library/management/core/service/BookService.java
@@ -13,16 +13,19 @@ import app.library.management.core.service.response.dto.status.ResponseState;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
 public class BookService {
 
     private final BookRepository bookRepository;
     private final BookStatusManager bookStatusManager;
+    private final ConcurrentHashMap<Long, Object> locks;
 
     public BookService(BookRepository bookRepository, BookStatusManager bookStatusManager) {
         this.bookRepository = bookRepository;
         this.bookStatusManager = bookStatusManager;
+        locks = new ConcurrentHashMap<>();
     }
 
     /**
@@ -67,20 +70,23 @@ public class BookService {
      * @return BookServiceResponse
      */
     public BookServiceResponse rent(long id, LocalDateTime now) {
-        Optional<Book> optionalBook = bookRepository.findById(id);
-        if (optionalBook.isEmpty()) {
-            // 없는 자원에 대한 예외처리
-            return new BookServiceResponse(ResponseState.NOTFOUND_EXCEPTION, Stage.RENT, null);
-        }
+        locks.putIfAbsent(id, new Object());
+        synchronized (locks.get(id)) {
+            Optional<Book> optionalBook = bookRepository.findById(id);
+            if (optionalBook.isEmpty()) {
+                // 없는 자원에 대한 예외처리
+                return new BookServiceResponse(ResponseState.NOTFOUND_EXCEPTION, Stage.RENT, null);
+            }
 
-        Book book = optionalBook.get();
-        // [대여중, 정리중, 분실] 된 경우에 대한 예외처리
-        if (book.getStatus() != BookStatus.AVAILABLE) {
-            return new BookServiceResponse(ResponseState.VALIDATION_EXCEPTION, Stage.RENT, book.getStatus());
+            Book book = optionalBook.get();
+            // [대여중, 정리중, 분실] 된 경우에 대한 예외처리
+            if (book.getStatus() != BookStatus.AVAILABLE) {
+                return new BookServiceResponse(ResponseState.VALIDATION_EXCEPTION, Stage.RENT, book.getStatus());
+            }
+            book.rent(now);
+            bookRepository.update(book);
+            return new BookServiceResponse(ResponseState.SUCCESS, Stage.RENT, book.getStatus());
         }
-        book.rent(now);
-        bookRepository.update(book);
-        return new BookServiceResponse(ResponseState.SUCCESS, Stage.RENT, book.getStatus());
     }
 
     /**
@@ -90,20 +96,23 @@ public class BookService {
      * @return BookServiceResponse
      */
     public BookServiceResponse returnBook(long id, LocalDateTime now) {
-        Optional<Book> optionalBook = bookRepository.findById(id);
-        if (optionalBook.isEmpty()) {
-            // 없는 자원에 대한 예외처리
-            return new BookServiceResponse(ResponseState.NOTFOUND_EXCEPTION, Stage.RETURN, null);
+        locks.putIfAbsent(id, new Object());
+        synchronized (locks.get(id)) {
+            Optional<Book> optionalBook = bookRepository.findById(id);
+            if (optionalBook.isEmpty()) {
+                // 없는 자원에 대한 예외처리
+                return new BookServiceResponse(ResponseState.NOTFOUND_EXCEPTION, Stage.RETURN, null);
+            }
+            Book book = optionalBook.get();
+            if (!book.isBookReturnable()) {
+                // 이미 반납된 도서 [정리중, 대여중] 에 대한 예외처리
+                return new BookServiceResponse(ResponseState.VALIDATION_EXCEPTION, Stage.RETURN, book.getStatus());
+            }
+            book.returnBook(now);
+            bookRepository.update(book);
+            bookStatusManager.execute(book, now);
+            return new BookServiceResponse(ResponseState.SUCCESS, Stage.RETURN, book.getStatus());
         }
-        Book book = optionalBook.get();
-        if (!book.isBookReturnable()) {
-            // 이미 반납된 도서 [정리중, 대여중] 에 대한 예외처리
-            return new BookServiceResponse(ResponseState.VALIDATION_EXCEPTION, Stage.RETURN, book.getStatus());
-        }
-        book.returnBook(now);
-        bookRepository.update(book);
-        bookStatusManager.execute(book, now);
-        return new BookServiceResponse(ResponseState.SUCCESS, Stage.RETURN, book.getStatus());
     }
 
     /**
@@ -113,19 +122,22 @@ public class BookService {
      * @return BookServiceResponse
      */
     public BookServiceResponse reportLost(long id, LocalDateTime now) {
-        Optional<Book> optionalBook = bookRepository.findById(id);
-        if (optionalBook.isEmpty()) {
-            // 없는 자원에 대한 예외처리
-            return new BookServiceResponse(ResponseState.NOTFOUND_EXCEPTION, Stage.LOST, null);
+        locks.putIfAbsent(id, new Object());
+        synchronized (locks.get(id)) {
+            Optional<Book> optionalBook = bookRepository.findById(id);
+            if (optionalBook.isEmpty()) {
+                // 없는 자원에 대한 예외처리
+                return new BookServiceResponse(ResponseState.NOTFOUND_EXCEPTION, Stage.LOST, null);
+            }
+            Book book = optionalBook.get();
+            if (book.getStatus() == BookStatus.LOST) {
+                // 이미 [분실]된 도서에 대한 예외처리
+                return new BookServiceResponse(ResponseState.VALIDATION_EXCEPTION, Stage.LOST, book.getStatus());
+            }
+            book.lost(now);
+            bookRepository.update(book);
+            return new BookServiceResponse(ResponseState.SUCCESS, Stage.LOST, book.getStatus());
         }
-        Book book = optionalBook.get();
-        if (book.getStatus() == BookStatus.LOST) {
-            // 이미 [분실]된 도서에 대한 예외처리
-            return new BookServiceResponse(ResponseState.VALIDATION_EXCEPTION, Stage.LOST, book.getStatus());
-        }
-        book.lost(now);
-        bookRepository.update(book);
-        return new BookServiceResponse(ResponseState.SUCCESS, Stage.LOST, book.getStatus());
     }
 
     /**
@@ -135,12 +147,15 @@ public class BookService {
      * @return BookServiceResponse
      */
     public BookServiceResponse delete(long id) {
-        Optional<Book> optionalBook = bookRepository.findById(id);
-        if (optionalBook.isEmpty()) {
-            // 없는 자원에 대한 예외처리
-            return new BookServiceResponse(ResponseState.NOTFOUND_EXCEPTION, Stage.DELETE, null);
+        locks.putIfAbsent(id, new Object());
+        synchronized (locks.get(id)) {
+            Optional<Book> optionalBook = bookRepository.findById(id);
+            if (optionalBook.isEmpty()) {
+                // 없는 자원에 대한 예외처리
+                return new BookServiceResponse(ResponseState.NOTFOUND_EXCEPTION, Stage.DELETE, null);
+            }
+            bookRepository.delete(optionalBook.get());
+            return new BookServiceResponse(ResponseState.SUCCESS, Stage.DELETE, null);
         }
-        bookRepository.delete(optionalBook.get());
-        return new BookServiceResponse(ResponseState.SUCCESS, Stage.DELETE, null);
     }
 }
