a0d733dab40d3a4c54be21496bb650f209205e01
happyjamy <78072370+happyjamy@users.noreply.github.com>
2023-10-09 21:19:04 +0900

    [Comment] 도찌님 피드백 주석

--- src/main/java/com/programmers/domain/status/BookStatus.java
+++ src/main/java/com/programmers/domain/status/BookStatus.java
@@ -7,10 +7,12 @@ import java.util.Set;
 public interface BookStatus {
     BookStatusType getBookStatusName();
 
+    // 허용하는 액션을 들어내 캠슐화를 저해할 여지 있음.
     Set<Action> allowedActions();
 
     void disallowedExceptions(Action action);
 
+    // 이것보다 하나하나 만들어서 쓰면 나중에 컴파일 딴에서 액션이 추가 돼도 적지 않는 휴먼에러를 잡을 수 있음.
     default void validateAction(Action action) {
         if (!allowedActions().contains(action)) {
             disallowedExceptions(action);
