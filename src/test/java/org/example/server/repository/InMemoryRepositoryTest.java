9717dbeb6e4af4518be71c64ded03a9d6d19995a
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-10-09 15:20:50 +0900

    refactor: 레포지토리 테스트 코드 인메모리, 파일별 분리

---
+++ src/test/java/org/example/server/repository/InMemoryRepositoryTest.java
@@ -0,0 +1,24 @@
+package org.example.server.repository;
+
+import org.assertj.core.api.Assertions;
+import org.example.packet.requestPacket.BookRegisterDto;
+import org.example.server.entity.Book;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class InMemoryRepositoryTest {
+    private final Book newBook = new Book(new BookRegisterDto("새로 나온 책", "이세희", 100));
+    private Repository repository;
+    private int dataCount;
+
+    @Test
+    @DisplayName("인메모리 도서 등록 후 도서 추가 확인")
+    void inMemorySaveTest() {
+        repository = new InMemoryRepository();
+        dataCount = repository.findAll().size();
+
+        repository.save(newBook);
+
+        Assertions.assertThat(repository.findAll().size()).isEqualTo(dataCount + 1);
+    }
+}
