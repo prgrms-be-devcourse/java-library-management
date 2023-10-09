a13ea72adf262a9b8c9c00bc4ffd1c8a0210b53d
ebcho <joeunbi514@naver.com>
2023-10-09 13:41:57 +0900

    fix: 싱글톤 패턴 lazy initailization 으로 변경

--- src/main/java/com/programmers/library/util/IdGenerator.java
+++ src/main/java/com/programmers/library/util/IdGenerator.java
@@ -3,13 +3,20 @@ package com.programmers.library.util;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class IdGenerator {
-	private static final IdGenerator instance = new IdGenerator();
+	private static volatile IdGenerator instance;
 	private final AtomicLong id = new AtomicLong(0L);
 
 	private IdGenerator() {
 	}
 
 	public static IdGenerator getInstance() {
+		if (instance == null) {
+			synchronized (IdGenerator.class) {
+				if (instance == null) {
+					instance = new IdGenerator();
+				}
+			}
+		}
 		return instance;
 	}
 
